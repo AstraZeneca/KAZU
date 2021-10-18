c0c964d9a283d797afd6ec85603461f5761a0ea8
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-10-18 14:05:25 +0100

    deleted routes.py azner_runner.py and oold hydra config.yaml
    deleted test_azner_runner.py

--- azner/azner/runner/azner_runner.py
+++
@@ -1,9 +0,0 @@
-from omegaconf import DictConfig
-
-
-class AZNerRunner:
-    def __init__(self,cfg:DictConfig):
-        self.cfg = cfg
-
-    def ner(self,text:str)->str:
-        return "not implemented"
