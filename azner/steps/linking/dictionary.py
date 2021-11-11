c27d2f135dbe246c3c1932fa346114bd939cf77f
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-11-11 13:18:46 +0000

    docs for dictionary.py

--- azner/steps/linking/dictionary.py
+++ azner/steps/linking/dictionary.py
@@ -15,62 +15,29 @@ logger = logging.getLogger(__name__)
 
 
 class DictionaryEntityLinkingStep(BaseStep):
+    """
+    A simple dictionary lookup step, using RapidFuzz for string matching
+    """
+
     def __init__(
         self,
         depends_on: List[str],
-        ontology_path: str,
         entity_class_to_ontology_mappings: Dict[str, str],
         ontology_dictionary_index: Dict[str, DictionaryIndex],
         process_all_entities: bool = False,
         lookup_cache_size: int = 5000,
     ):
         """
-        This step wraps Sapbert: Self Alignment pretraining for biomedical entity representation.
 
-
-        We make use of two caches here:
-        1) Ontology embeddings
-            Since these are static and numerous, it makes sense to precompute them once and reload them each time
-            This is done automatically if no cache file is detected. A cache directory is generated with the prefix
-            'cache_' alongside the location of the original ontology file
-
-        2) Runtime LFUCache
-            Since certain entities will come up more frequently, we cache the result of the embedding check rather than
-            call bert repeatedly. This cache is maintained on the basis of the get_match_entity_class_hash(Entity)
-            function
-
-
-        Note, the ontology to link against is held in memory as an ndarray. For very large KB's we should use
-        faiss (see Nemo example via SapBert github reference)
-
-        Original paper
-        https://aclanthology.org/2021.naacl-main.334.pdf
-
-        :param model: path to HF SAPBERT model, config and tokenizer. A good pretrained default is available at
-                            https://huggingface.co/cambridgeltl/SapBERT-from-PubMedBERT-fulltext
-                            This is passed to HF Automodel.from_pretrained()
-        :param depends_on: namespaces of dependency stes
-        :param ontology_path: path to parquet of labels to map to. This should have three columns: 'source',
-                            'iri' and 'default_label'. The default_label will be used to create an embedding, and the
-                            source and iri will be used to create a Mapping for the entity.
-                            See SapBert paper for further info
-        :param batch_size: batch size for dataloader
-        :param dl_workers: number of workers for the dataloader
-        :param trainer: an instance of pytorch lightning trainer
-        :param embedding_index_factory: an instance of EmbeddingIndexFactory, used to instantiate embedding indexes
-        :param ontology_partition_size: number of rows to process before saving results, when building initial ontology cache
-        :param process_all_entities: bool flag. Since SapBert involves expensive bert calls, this flag controls whether
-                                        it should be used on all entities, or only entities that have no mappings (i.e.
-                                        entites that have already been linked via a less expensive method, such as
-                                        dictionary lookup). This flag check for the presence of at least one entry in
-                                        Entity.metadata.mappings
-        :param rebuild_ontology_cache: should the ontology embedding cache be rebuilt?
-        :param lookup_cache_size: this step maintains a cache of {hash(Entity.match,Entity.entity_class):Mapping}, to reduce bert calls. This dictates the size
+        :param depends_on:
+        :param entity_class_to_ontology_mappings: Dict mapping entity class to ontology names
+        :param ontology_dictionary_index: Dict of ontology name: DictionaryIndex
+        :param process_all_entities: if false, will ignore any entities with mappings already associated
+        :param lookup_cache_size: cache size to prevent repeated calls to the index
         """
         super().__init__(depends_on=depends_on)
         self.entity_class_to_ontology_mappings = entity_class_to_ontology_mappings
         self.process_all_entities = process_all_entities
-        self.ontology_path = ontology_path
         self.ontology_index_dict = ontology_dictionary_index
         self.lookup_cache = EntityLinkingLookupCache(lookup_cache_size)
 
