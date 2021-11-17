4dd42942b7be58f66368ed421ae45041d6003d93
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-11-17 16:51:24 +0000

    made server test more substantial

--- azner/tests/test_server.py
+++ azner/tests/test_server.py
@@ -5,9 +5,12 @@ import requests
 from hydra import compose, initialize_config_dir
 
 from azner.data.data import Document
-from azner.tests.utils import get_TransformersModelForTokenClassificationNerStep_model_path, SKIP_MESSAGE
+from azner.tests.utils import (
+    SKIP_MESSAGE,
+)
 from azner.web.routes import AZNER
 from azner.web.server import start
+
 
 @pytest.mark.skipif(os.environ.get("KAZU_TEST_CONFIG_DIR") is None, reason=SKIP_MESSAGE)
 def test_api():
@@ -21,12 +24,11 @@ def test_api():
         )
         start(cfg)
         response = requests.post(
-            f"http://127.0.0.1:{cfg.ray.serve.port}/api/{AZNER}/", json={"text": "Why do we always test EGFR in "
-                                                                                 "these applications?"}
+            f"http://127.0.0.1:{cfg.ray.serve.port}/api/{AZNER}/",
+            json={"text": "Why do we always test EGFR in " "these applications?"},
         ).json()
         result = Document(**response)
         assert isinstance(result, Document)
         section = result.sections[0]
-        assert len(section.entities) >0
-        assert section.entities[0].match == 'EGFR'
-
+        assert len(section.entities) > 0
+        assert section.entities[0].match == "EGFR"
