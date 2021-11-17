a63d5945ed77e9c1affa20fe379c8dc7cb4284ce
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-11-17 21:22:16 +0000

    fixed start.sh

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -2,13 +2,15 @@
 
 #download the model pack using Azure SAS
 echo "downloading model pack"
-wget $MODEL_PACK .
+wget "$MODEL_PACK" -O model_pack.zip
 #unpack the models
 echo "unzipping models"
-tar -xf model_pack.tar.gz
+unzip  model_pack.zip
 echo "running ray deployment"
 python -m azner.web.server \
 SapBertForEntityLinkingStep=docker \
 ray=cluster \
-TransformersModelForTokenClassificationNerStep=docker
+TransformersModelForTokenClassificationNerStep=docker \
+DictionaryEntityLinkingStep=docker \
+hydra.run.dir="."
 echo "ray deployment complete"
