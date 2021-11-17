fda04752918cf3e437b8826a1bbb913a096cb591
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-11-17 13:15:24 +0000

    initial version of abbreviation expansion added
    first implementation of abbreviation expansion step
    
    initial tests for abbreviation expansion
    
    added test for abbreviation expansion
    
    added test for destructive abbreviation expansion
    
    updated config to allow destructive expansion or not
    
    updated defautl config for abbreviation expansion
    
    added SciSpacyAbbreviationExpansionStep to steps init
    
    changed abbreviation expansion so that it returns a hashable type (and update test)
    
    REBASE ME
    
    REBASE ME
    
    REBASE ME
    
    reworked String preprocessor
    
    reworked String preprocessor
    
    updated init

--- azner/steps/__init__.py
+++ azner/steps/__init__.py
@@ -1,4 +1,7 @@
-from .base.step import BaseStep
+from .base.step import BaseStep, StringPreprocessorStep
 from .ner.hf_token_classification import TransformersModelForTokenClassificationNerStep
 from .linking.sapbert import SapBertForEntityLinkingStep
 from .linking.dictionary import DictionaryEntityLinkingStep
+from .abbreviation_expansion.scispacy_abbreviation_expansion import (
+    SciSpacyAbbreviationExpansionStep,
+)
---
+++ azner/tests/test_string_preprocessor_step.py
@@ -0,0 +1,15 @@
+from typing import List, Tuple
+
+from data.data import Document
+from steps import StringPreprocessorStep
+
+
+# class AddSomeCharsStep(StringPreprocessorStep):
+#
+#     def _run(self, docs: List[Document]) -> Tuple[List[Document], List[Document]]:
+#
+#
+#
+#
+# def test_multiple_string_preprocessing_steps():
+#
