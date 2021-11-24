7e5f61c082f8a9befbc134d6334ad0b77dba142f
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-11-24 18:11:20 +0000

    removed preprocessing rules from dictionary_index.py, as they belong elsewhere
    exact match test
    
    added test for similarity

--- azner/utils/dictionary_index.py
+++ azner/utils/dictionary_index.py
@@ -1,5 +1,3 @@
-from typing import Tuple, List
-
 import pandas as pd
 from rapidfuzz import process, fuzz
 
@@ -26,43 +24,6 @@ class DictionaryIndex:
         self.name = name
         self.df = pd.read_parquet(path)
         self.df["syn"] = self.df["syn"].str.lower()
-        self.lengths = self.df["syn"].apply(len)
-
-    def slice_df_for_potential_syns(self, query: str) -> pd.DataFrame:
-        """
-        dont attempt string match on any syns that are shorter than the actual query
-        returns a slice of self.df for equal or longer syns
-        :param query:
-        :return:
-        """
-        query_len = len(query)
-        target_indices = self.lengths >= query_len
-        df_slice = self.df[target_indices]
-        return df_slice
-
-    def post_process_hits(self, query: str, hits: List[Tuple[str, float, int]]):
-        """
-        prefer exact matching hits, then containing hits
-        :param query:
-        :param hits:
-        :return:
-        """
-
-        hit_strings = {x[0]: x for x in hits}
-        exact_hits = {x: i for i, x in enumerate(hit_strings) if query == x}
-        if len(exact_hits) > 0:
-            return [hits[i] for i in exact_hits.values()]
-        # ignore any queries shorter than 5 chars as likely to be noisy
-        query_length = len(query)
-        containing_hits = {
-            x: i for i, x in enumerate(hit_strings) if (query_length >= 5 and query in x)
-        }
-        if len(containing_hits) > 0:
-            containing_tups = [hits[i] for i in containing_hits.values()]
-            return sorted(containing_tups, key=lambda x: x[1], reverse=True)
-        else:
-            # fail if no matches found
-            return []
 
     def search(self, query: str) -> pd.DataFrame:
         """
@@ -74,16 +35,14 @@ class DictionaryIndex:
         if not self.fuzzy:
             return self.df[self.df["syn"] == query]
         else:
-            targets = self.slice_df_for_potential_syns(query)
             hits = process.extract(
                 query,
-                targets["syn"].tolist(),
+                self.df["syn"].tolist(),
                 scorer=fuzz.WRatio,
-                limit=10,
+                limit=20,
                 score_cutoff=self.score_cutoff,
             )
-            hits = self.post_process_hits(query, hits)
             locs = [x[2] for x in hits]
-            hit_df = targets.iloc[locs].copy()
+            hit_df = self.df.iloc[locs].copy()
             hit_df[LINK_SCORE] = [x[1] for x in hits]
             return hit_df
