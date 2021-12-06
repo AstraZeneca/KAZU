3c023f73fb5452af2dbfea4b5f823c10902fa6cb
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-12-06 19:50:00 +0000

    added caching abstractions and new CachedIndexGroup class

--- azner/utils/caching.py
+++ azner/utils/caching.py
@@ -1,9 +1,55 @@
-from typing import List
+import logging
+from pathlib import Path
+from typing import Type, Dict, Any
 
 from cachetools import LFUCache
 
-from azner.data.data import Entity, Mapping
+from azner.data.data import Entity, LINK_SCORE, NAMESPACE
+from azner.modelling.ontology_preprocessing.base import OntologyParser, IDX, MAPPING_TYPE
+from azner.utils.link_index import (
+    Index,
+    MatMulTensorEmbeddingIndex,
+    FaissEmbeddingIndex,
+    CDistTensorEmbeddingIndex,
+    DictionaryIndex,
+)
 from azner.utils.utils import get_match_entity_class_hash
+
+
+import os
+import shutil
+from typing import List, Tuple
+
+import pandas as pd
+import torch
+from pytorch_lightning import Trainer
+
+from azner.data.data import Mapping
+from azner.modelling.linking.sapbert.train import (
+    PLSapbertModel,
+)
+from azner.modelling.ontology_preprocessing.base import (
+    DEFAULT_LABEL,
+    SOURCE,
+)
+from azner.utils.utils import (
+    get_cache_dir,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def select_index_type(embedding_index_class_name) -> Type:
+    if embedding_index_class_name == MatMulTensorEmbeddingIndex.__name__:
+        return MatMulTensorEmbeddingIndex
+    elif embedding_index_class_name == FaissEmbeddingIndex.__name__:
+        return FaissEmbeddingIndex
+    elif embedding_index_class_name == CDistTensorEmbeddingIndex.__name__:
+        return CDistTensorEmbeddingIndex
+    elif embedding_index_class_name == DictionaryIndex.__name__:
+        return DictionaryIndex
+    else:
+        raise NotImplementedError(f"{embedding_index_class_name} not implemented in factory")
 
 
 class EntityLinkingLookupCache:
@@ -40,3 +86,183 @@ class EntityLinkingLookupCache:
                 for mapping in maybe_mappings:
                     ent.add_mapping(mapping)
         return cache_misses
+
+
+class OntologyCacheManager:
+    def __init__(self, rebuild_cache: bool = False):
+        self.rebuild_cache = rebuild_cache
+
+    def get_or_create_ontology_index(self, parser: OntologyParser) -> Index:
+        """
+        populate self.ontology_ids and self.ontology_index_dict either by calculating them afresh or loading from a
+        cached version on disk
+        """
+        cache_dir = get_cache_dir(parser.in_path, create_if_not_exist=False)
+        if self.rebuild_cache:
+            logger.info("forcing a rebuild of the ontology cache")
+            if os.path.exists(cache_dir):
+                shutil.rmtree(cache_dir)
+            get_cache_dir(parser.in_path, create_if_not_exist=True)
+            return self.build_ontology_cache(cache_dir, parser)
+        elif os.path.exists(cache_dir):
+            logger.info(f"loading cached ontology file from {cache_dir}")
+            return self.load_ontology_from_cache(cache_dir, parser)
+        else:
+            logger.info("No ontology cache file found. Building a new one")
+            get_cache_dir(parser.in_path, create_if_not_exist=True)
+            return self.build_ontology_cache(cache_dir, parser)
+
+    def build_ontology_cache(self, cache_dir: Path, parser: OntologyParser) -> Index:
+        raise NotImplementedError()
+
+    def load_ontology_from_cache(self, cache_dir: Path, parser: OntologyParser) -> Index:
+        return Index.load(str(cache_dir), parser.name)
+
+
+class DictionaryOntologyCacheManager(OntologyCacheManager):
+    def __init__(self, rebuild_cache: bool = False):
+
+        super().__init__(rebuild_cache)
+
+    def build_ontology_cache(self, cache_dir: Path, parser: OntologyParser) -> Index:
+        logger.info(f"creating index for {parser.in_path}")
+
+        index = DictionaryIndex(name=parser.name)
+
+        ontology_df = parser.format_default_labels()
+        synonym_df = parser.synonym_table
+        index.add(synonym_df, ontology_df)
+        index_cache_dir = get_cache_dir(parser.in_path, create_if_not_exist=True)
+        index_path = index.save(str(index_cache_dir))
+        logger.info(f"saved {index.name} index to {index_path.absolute()}")
+        logger.info(f"final index size for {index.name} is {len(index)}")
+        return index
+
+
+class EmbeddingOntologyCacheManager(OntologyCacheManager):
+    def __init__(
+        self,
+        model: PLSapbertModel,
+        batch_size: int,
+        trainer: Trainer,
+        dl_workers: int,
+        ontology_partition_size: int,
+        index_type: str,
+        rebuild_cache: bool = False,
+    ):
+
+        super().__init__(rebuild_cache)
+        self.index_type = select_index_type(index_type)
+        self.ontology_partition_size = ontology_partition_size
+        self.dl_workers = dl_workers
+        self.batch_size = batch_size
+        self.model = model
+        self.trainer = trainer
+
+    def build_ontology_cache(self, cache_dir: Path, parser: OntologyParser) -> Index:
+
+        logger.info(f"creating index for {parser.in_path}")
+
+        index = self.index_type(parser.name)
+        ontology_df = parser.format_default_labels()
+        for (
+            partition_number,
+            metadata_df,
+            ontology_embeddings,
+        ) in self.predict_ontology_embeddings(ontology_dataframe=ontology_df):
+            logger.info(f"processing partition {partition_number} ")
+            index.add(ontology_embeddings, metadata_df)
+            logger.info(f"index size is now {len(index)}")
+
+        index_cache_dir = get_cache_dir(parser.in_path, create_if_not_exist=True)
+        index_path = index.save(index_cache_dir)
+        logger.info(f"saved {parser.name} index to {index_path.absolute()}")
+        logger.info(f"final index size for {parser.name} is {len(index)}")
+        return index
+
+    def split_dataframe(self, df: pd.DataFrame, chunk_size: int = 100000):
+        """
+        generator to split up a dataframe into partitions
+        :param df:
+        :param chunk_size: size of partittions to create
+        :return:
+        """
+        num_chunks = len(df) // chunk_size + 1
+        for i in range(num_chunks):
+            yield df[i * chunk_size : (i + 1) * chunk_size]
+
+    def predict_ontology_embeddings(
+        self, ontology_dataframe: pd.DataFrame
+    ) -> Tuple[List[str], pd.DataFrame, torch.Tensor]:
+        """
+        based on the value of self.ontology_path, this returns a Tuple[List[str],np.ndarray]. The strings are the
+        iri's, and the torch.Tensor are the embeddings to be queried against
+        :return: partition number, dataframe for metadata, 2d tensor of embeddings
+        """
+
+        for partition_number, df in enumerate(
+            self.split_dataframe(ontology_dataframe, self.ontology_partition_size)
+        ):
+            df = df.copy()
+            if df.shape[0] == 0:
+                return
+            logger.info(f"creating partitions for partition {partition_number}")
+            logger.info(f"read {df.shape[0]} rows from ontology")
+            default_labels = df[DEFAULT_LABEL].tolist()
+            logger.info(f"predicting embeddings for default_labels. Examples: {default_labels[:3]}")
+            results = self.model.get_embeddings_for_strings(default_labels)
+            yield partition_number, df, results
+
+
+class CachedIndexGroup:
+    """
+    Convenience class for building and managing a group of indexes, and querying against them
+    """
+
+    def __init__(
+        self,
+        entity_class_to_ontology_mappings: Dict[str, List[str]],
+        parsers: List[OntologyParser],
+        cache_manager: OntologyCacheManager,
+    ):
+        self.cache_manager = cache_manager
+        self.parsers = parsers
+        self.entity_class_to_ontology_mappings = entity_class_to_ontology_mappings
+        self.ontology_index_dict: Dict[str, Index] = {}
+
+    def search(self, query: Any, entity_class: str, namespace: str, **kwargs) -> List[Mapping]:
+        ontologies_to_search = self.entity_class_to_ontology_mappings.get(entity_class, [])
+        results = []
+        for ontology_name in ontologies_to_search:
+            index = self.ontology_index_dict.get(ontology_name)
+            if index is None:
+                logger.warning(f"tried to search indices for {ontology_name}, but none were found")
+            else:
+                index_results = index.search(query=query, **kwargs)
+                index_results[SOURCE] = index.name
+                results.append(index_results)
+        results = pd.concat(results).sort_values(by=LINK_SCORE)
+        mappings = []
+        for i, row in results.iterrows():
+            row_dict = row.to_dict()
+            ontology_id = row_dict.pop(IDX)
+            mapping_type = row_dict.pop(MAPPING_TYPE)
+            source = row_dict.pop(SOURCE)
+            row_dict[NAMESPACE] = namespace
+            new_mapping = Mapping(
+                source=source,
+                idx=ontology_id,
+                mapping_type=mapping_type,
+                metadata=row_dict,
+            )
+            mappings.append(new_mapping)
+        return mappings
+
+    def load(self):
+        """
+        loads the cached version of the embedding indices from disk
+        :return: ontology:Index dict
+        """
+        for parser in self.parsers:
+            index = self.cache_manager.get_or_create_ontology_index(parser)
+            self.ontology_index_dict[index.name] = index
--- azner/utils/link_index.py
+++ azner/utils/link_index.py
@@ -1,22 +1,20 @@
 import abc
-import json
+import logging
 import os
+import pickle
 import shutil
 from pathlib import Path
 from typing import Tuple, Any
+
 import numpy as np
 import pandas as pd
 import torch
 from rapidfuzz import process, fuzz
 
 from azner.data.data import LINK_SCORE
+from azner.modelling.ontology_preprocessing.base import SYN, IDX
 
-# dataframe column keys
-DEFAULT_LABEL = "default_label"
-IDX = "idx"
-SYN = "syn"
-MAPPING_TYPE = "mapping_type"
-SOURCE = "source"
+logger = logging.getLogger(__name__)
 
 
 class Index(abc.ABC):
@@ -24,29 +22,9 @@ class Index(abc.ABC):
     base class for all indices.
     """
 
-    def search(self, *args, **kwargs) -> pd.DataFrame:
-        """
-        calls to search should return a :py:class:`<pd.DataFrame>`
-
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        raise NotImplementedError()
-
-
-class DictionaryIndex(Index):
-    """
-    a simple dictionary index for linking
-    """
-
     def __init__(
         self,
-        path: str,
         name: str = "unnamed_index",
-        fuzzy: bool = False,
-        score_cutoff: int = 99.0,
-        top_n: int = 20,
     ):
         """
 
@@ -56,14 +34,133 @@ class DictionaryIndex(Index):
         :param score_cutoff: minimum score for fuzzy matching
         :param top_n: number of hits to return
         """
-        self.top_n = top_n
-        self.score_cutoff = score_cutoff
-        self.fuzzy = fuzzy
         self.name = name
-        self.df = pd.read_parquet(path)
-        self.df[SYN] = self.df[SYN].str.lower()
+        self.metadata = None
 
-    def search(self, query: str) -> pd.DataFrame:
+    def _search(
+        self, query: Any, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        calls to search should return a :py:class:`<pd.DataFrame>`
+
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        raise NotImplementedError()
+
+    def search(
+        self, query: Any, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> pd.DataFrame:
+        """
+        search the index
+
+        :param query: a string to query against
+        :return: a df of hits
+        """
+        locs, scores = self._search(query, score_cutoff=score_cutoff, top_n=top_n, **kwargs)
+        hit_df = self.metadata.iloc[locs].copy()
+        hit_df[LINK_SCORE] = scores
+        return hit_df
+
+    def save(self, path: str) -> Path:
+        """
+        save to disk. Makes a directory at the path location with all the index assets
+
+        :param path:
+        :return: a Path to where the index was saved
+        """
+        directory = Path(path).joinpath(self.name)
+        if directory.exists():
+            shutil.rmtree(directory)
+        os.makedirs(directory)
+        with open(self.get_index_data_path(directory), "wb") as f:
+            pickle.dump(self, f)
+        self.metadata.to_parquet(self.get_dataframe_path(directory), index=None)
+
+        self._save(str(self.get_index_path(directory)))
+        return directory
+
+    @staticmethod
+    def load(path: str, name: str):
+        """
+        load from disk
+
+        :param path:
+        :return:
+        """
+        path = Path(path).joinpath(name)
+        with open(Index.get_index_path(path), "rb") as f:
+            index = pickle.load(f)
+        index.metadata = pd.read_parquet(Index.get_dataframe_path(path))
+        index._load(Index.get_index_data_path(path))
+        return index
+
+    @staticmethod
+    def get_path_for_ancillory_file(directory: Path, filename: str) -> Path:
+        path = Path(directory).joinpath(filename)
+        return path
+
+    @staticmethod
+    def get_dataframe_path(path: Path) -> Path:
+        return Index.get_path_for_ancillory_file(path, "ontology_metadata.parquet")
+
+    @staticmethod
+    def get_index_path(path: Path) -> Path:
+        return Index.get_path_for_ancillory_file(path, "index.pkl")
+
+    @staticmethod
+    def get_index_data_path(path: Path) -> Path:
+        return Index.get_path_for_ancillory_file(path, "index.data")
+
+    def _save(self, path: str):
+        """
+        concrete implementations should implement this to save an index
+
+        :param path:
+        :return:
+        """
+        raise NotImplementedError()
+
+    def __getstate__(self):
+        return self.name
+
+    def __setstate__(self, state):
+        self.name = state
+
+    def _load(self, path: str) -> Any:
+        """
+        concrete implementations should implement this to load an index
+
+        :param path:
+        :return:
+        """
+        raise NotImplementedError()
+
+    def add(self, *args, **kwargs):
+        raise NotImplementedError()
+
+
+class DictionaryIndex(Index):
+    """
+    a simple dictionary index for linking
+    """
+
+    def __init__(self, name: str = "unnamed_index"):
+        """
+
+        :param path: path to parquet file of synonyms
+        :param name: a name for this index
+        :param fuzzy: use fuzzy matching
+        :param score_cutoff: minimum score for fuzzy matching
+        :param top_n: number of hits to return
+        """
+        super().__init__(name)
+        self.synonym_df = None
+
+    def _search(
+        self, query: Any, score_cutoff: int = 99.0, top_n: int = 20, fuzzy: bool = True, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
         """
         search the index
 
@@ -71,20 +168,39 @@ class DictionaryIndex(Index):
         :return: a df of hits
         """
         query = query.lower()
-        if not self.fuzzy:
-            return self.df[self.df[SYN] == query]
+        if not fuzzy:
+            idx_list = self.synonym_df[self.synonym_df[SYN] == query][IDX]
+            # score 100 for exact matches
+            scores = [100.0 for _ in range(len(idx_list))]
         else:
             hits = process.extract(
                 query,
-                self.df[SYN].tolist(),
+                self.synonym_df[SYN].tolist(),
                 scorer=fuzz.WRatio,
-                limit=self.top_n,
-                score_cutoff=self.score_cutoff,
+                limit=top_n,
+                score_cutoff=score_cutoff,
             )
             locs = [x[2] for x in hits]
-            hit_df = self.df.iloc[locs].copy()
-            hit_df[LINK_SCORE] = [x[1] for x in hits]
-            return hit_df
+            idx_list = self.synonym_df.iloc[locs][IDX]
+            scores = [x[1] for x in hits]
+        return self.metadata[self.metadata[IDX].isin(idx_list)].index, np.array(scores)
+
+    def _load(self, path: str) -> Any:
+        self.synonym_df = pd.read_parquet(path)
+
+    def _save(self, path: str):
+        self.synonym_df.to_parquet(path, index=None)
+
+    def add(self, synonym_df: pd.DataFrame, metadata_df: pd.DataFrame):
+        if self.synonym_df is None and self.metadata is None:
+            self.synonym_df = synonym_df
+            self.metadata = metadata_df
+        else:
+            self.synonym_df = pd.concat([self.synonym_df, synonym_df])
+            self.metadata = pd.concat([self.metadata, metadata_df])
+
+    def __len__(self):
+        return self.metadata.shape[0]
 
 
 class EmbeddingIndex(Index):
@@ -93,10 +209,7 @@ class EmbeddingIndex(Index):
     """
 
     def __init__(self, name: str = "unnamed_index"):
-        """
-        :param name: a name for this index
-        """
-        self.name = name
+        super().__init__(name)
 
     def add(self, embeddings: torch.Tensor, metadata: pd.DataFrame):
         """
@@ -134,87 +247,14 @@ class EmbeddingIndex(Index):
         """
         raise NotImplementedError()
 
-    def search(self, embedding: torch.Tensor) -> pd.DataFrame:
-        """
-        search the index
-
-        :param embedding: a 2d tensor to query the index with
-        :return: a tuple of 2d numpy arrays: distances, nearest neighbours, and a pd.DataFrame of metadata
-        """
-        distances, neighbors = self._search(embedding)
-        hit_df = self.metadata.iloc[neighbors].copy()
-
-        hit_df[LINK_SCORE] = distances.tolist()
-        return hit_df
-
-    def _search(self, embedding: torch.Tensor) -> Tuple[np.ndarray, np.ndarray]:
+    def _search(
+        self, query: Any, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
         """
         concrete implementations should implement this to search  an index
 
         :param embedding: a 2d tensor to query the index with
         :return: a tuple of 2d numpy arrays: distances, nearest neighbours
-        """
-        raise NotImplementedError()
-
-    def get_path_for_ancillory_file(self, directory: str, filename: str) -> Path:
-        path = Path(directory).joinpath(filename)
-        return path
-
-    def get_dataframe_path(self, path: str) -> Path:
-        return self.get_path_for_ancillory_file(path, "metadata.parquet")
-
-    def get_index_path(self, path: Path) -> Path:
-        return self.get_path_for_ancillory_file(path, "index.sapbert")
-
-    def get_index_metadata_path(self, path: Path) -> Path:
-        return self.get_path_for_ancillory_file(path, "index_meta.sapbert")
-
-    def save(self, path: str) -> Path:
-        """
-        save to disk. Makes a directory at the path location with all the index assets
-
-        :param path:
-        :return: a Path to where the index was saved
-        """
-        directory = Path(path).joinpath(self.name)
-        if directory.exists():
-            shutil.rmtree(directory)
-        os.makedirs(directory)
-
-        self.metadata.to_parquet(self.get_dataframe_path(directory), index=None)
-        with open(self.get_index_metadata_path(directory), "w") as f:
-            json.dump({"name": self.name}, f)
-        self._save(str(self.get_index_path(directory)))
-        return directory
-
-    def _save(self, path: str):
-        """
-        concrete implementations should implement this to save an index
-
-        :param path:
-        :return:
-        """
-        raise NotImplementedError()
-
-    def load(self, path: str):
-        """
-        load from disk
-
-        :param path:
-        :return:
-        """
-        self.metadata = pd.read_parquet(self.get_dataframe_path(path))
-        with open(self.get_index_metadata_path(path), "r") as f:
-            dict = json.load(f)
-            self.name = dict["name"]
-        self.index = self._load(str(self.get_index_path(path)))
-
-    def _load(self, path: str) -> Any:
-        """
-        concrete implementations should implement this to load an index
-
-        :param path:
-        :return:
         """
         raise NotImplementedError()
 
@@ -232,11 +272,8 @@ class FaissEmbeddingIndex(EmbeddingIndex):
     an embedding index that uses faiss.IndexFlatL2
     """
 
-    def __init__(self, return_n_nearest_neighbours: int, *args, **kwargs):
-        """
-        :param dims: the dimensions of the embeddings
-        :param return_n_nearest_neighbours: when a call to .search is made, this may neightbours will be returned
-        """
+    def __init__(self, name: str = "unnamed_index"):
+        super().__init__(name)
         try:
             import faiss
 
@@ -244,8 +281,6 @@ class FaissEmbeddingIndex(EmbeddingIndex):
         except ImportError:
             raise RuntimeError(f"faiss is not installed. Cannot use {self.__class__.__name__}")
 
-        super().__init__(*args, **kwargs)
-        self.return_n_nearest_neighbours = return_n_nearest_neighbours
         self.index = None
 
     def _load(self, path: str):
@@ -254,11 +289,11 @@ class FaissEmbeddingIndex(EmbeddingIndex):
     def _save(self, path: str):
         self.faiss.write_index(self.index, path)
 
-    def _search(self, embedding: torch.Tensor):
-        distances, neighbors = self.index.search(
-            embedding.numpy(), self.return_n_nearest_neighbours
-        )
-        return np.squeeze(distances), np.squeeze(neighbors)
+    def _search(
+        self, query: torch.Tensor, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        distances, neighbors = self.index.search(query.numpy(), top_n)
+        return np.squeeze(neighbors), np.squeeze(distances)
 
     def _create_index(self, embeddings: torch.Tensor):
         index = self.faiss.IndexFlatL2(embeddings.shape[1])
@@ -278,9 +313,9 @@ class TensorEmbeddingIndex(EmbeddingIndex):
     a simple index of torch tensors, that is queried with torch.matmul
     """
 
-    def __init__(self, return_n_nearest_neighbours: int, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.return_n_nearest_neighbours = return_n_nearest_neighbours
+    def __init__(self, name: str):
+
+        super().__init__(name)
         self.index = None
 
     def _load(self, path: str):
@@ -289,7 +324,9 @@ class TensorEmbeddingIndex(EmbeddingIndex):
     def _save(self, path: str):
         torch.save(self.index, path)
 
-    def _search(self, embedding: torch.Tensor) -> Tuple[np.ndarray, np.ndarray]:
+    def _search(
+        self, query: torch.Tensor, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
         raise NotImplementedError()
 
     def _add(self, embeddings: torch.Tensor):
@@ -307,15 +344,18 @@ class CDistTensorEmbeddingIndex(TensorEmbeddingIndex):
     Calculate embedding based on cosine distance
     """
 
-    def _search(self, embedding: torch.Tensor) -> Tuple[np.ndarray, np.ndarray]:
-        score_matrix = torch.cdist(embedding, self.index)
+    def _search(
+        self, query: torch.Tensor, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        score_matrix = torch.cdist(query, self.index)
 
         score_matrix = torch.squeeze(score_matrix)
-        neighbours = torch.argsort(score_matrix, descending=False)[
-            : self.return_n_nearest_neighbours
-        ]
+        neighbours = torch.argsort(score_matrix, descending=False)[:top_n]
         distances = score_matrix[neighbours]
-        return distances.cpu().numpy(), neighbours.cpu().numpy()
+        return (
+            neighbours.cpu().numpy(),
+            distances.cpu().numpy(),
+        )
 
 
 class MatMulTensorEmbeddingIndex(TensorEmbeddingIndex):
@@ -323,41 +363,13 @@ class MatMulTensorEmbeddingIndex(TensorEmbeddingIndex):
     calculate embedding based on MatMul
     """
 
-    def _search(self, embedding: torch.Tensor) -> Tuple[np.ndarray, np.ndarray]:
-        score_matrix = torch.matmul(embedding, self.index.T)
+    def _search(
+        self, query: torch.Tensor, score_cutoff: int = 99.0, top_n: int = 20, **kwargs
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        score_matrix = torch.matmul(query, self.index.T)
 
         score_matrix = torch.squeeze(score_matrix)
-        neighbours = torch.argsort(score_matrix, descending=True)[
-            : self.return_n_nearest_neighbours
-        ]
+        neighbours = torch.argsort(score_matrix, descending=True)[:top_n]
         distances = score_matrix[neighbours]
         distances = 100 - (1 / distances)
-        return distances.cpu().numpy(), neighbours.cpu().numpy()
-
-
-class EmbeddingIndexFactory:
-    """
-    since we want to make multiple indices (one per ontology), this factory class helps us build them consistently
-    """
-
-    def __init__(self, embedding_index_class_name, return_n_nearest_neighbours):
-        self.return_n_nearest_neighbours = return_n_nearest_neighbours
-        self.embedding_index_class_name = embedding_index_class_name
-
-    def create_index(self, name: str = "unnamed_index"):
-        if self.embedding_index_class_name == MatMulTensorEmbeddingIndex.__name__:
-            return MatMulTensorEmbeddingIndex(
-                name=name, return_n_nearest_neighbours=self.return_n_nearest_neighbours
-            )
-        elif self.embedding_index_class_name == FaissEmbeddingIndex.__name__:
-            return FaissEmbeddingIndex(
-                name=name, return_n_nearest_neighbours=self.return_n_nearest_neighbours
-            )
-        elif self.embedding_index_class_name == CDistTensorEmbeddingIndex.__name__:
-            return CDistTensorEmbeddingIndex(
-                name=name, return_n_nearest_neighbours=self.return_n_nearest_neighbours
-            )
-        else:
-            raise NotImplementedError(
-                f"{self.embedding_index_class_name} not implemented in factory"
-            )
+        return neighbours.cpu().numpy(), distances.cpu().numpy()
