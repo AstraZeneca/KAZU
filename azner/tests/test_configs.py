9bc2e9329297021ba60f34ec4567cfb587b4acc3
Richard Jackson <richard.jackson4@astrazeneca.com>
2021-12-13 14:36:28 +0000

    added tests for configs

---
+++ azner/tests/test_configs.py
@@ -0,0 +1,27 @@
+import pytest
+from hydra import initialize_config_dir, compose
+from hydra.utils import instantiate
+
+from azner.pipeline.pipeline import Pipeline, load_steps
+from azner.tests.utils import CONFIG_DIR
+
+
+def test_docker_configs():
+    with initialize_config_dir(config_dir=str(CONFIG_DIR)):
+        cfg = compose(
+            config_name="config",
+            overrides=[
+                "DictionaryEntityLinkingStep=docker",
+                "SapBertForEntityLinkingStep=docker",
+                "TransformersModelForTokenClassificationNerStep=docker"
+            ],
+        )
+        # should raise OSErrors from missing files
+        with pytest.raises(OSError):
+            instantiate(cfg.DictionaryEntityLinkingStep)
+        with pytest.raises(OSError):
+            instantiate(cfg.SapBertForEntityLinkingStep)
+        with pytest.raises(OSError):
+            instantiate(cfg.TransformersModelForTokenClassificationNerStep)
+        with pytest.raises(OSError):
+            Pipeline(steps=load_steps(cfg))
