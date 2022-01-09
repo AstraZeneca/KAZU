cea4261120e30183f11431f12517a125218b4f75
Elliot Ford <elliot.ford@astrazeneca.com>
2022-01-09 21:43:04 +0000

    use kazu_test_config fixture everywhere possible

--- azner/tests/test_distillation_training.py
+++ azner/tests/test_distillation_training.py
@@ -1,7 +1,5 @@
 import tempfile
 
-from hydra import initialize_config_module, compose
-
 from azner.tests.utils import TEST_ASSETS_PATH
 from azner.modelling.distillation.train import start
 
@@ -9,18 +7,16 @@ BERT_TEST_MODEL_PATH = TEST_ASSETS_PATH.joinpath("bert_test_model")
 DATA_DIR = TEST_ASSETS_PATH.joinpath("tinybern")
 
 
-def test_stage_2_tinybert_distillation():
+def test_stage_2_tinybert_distillation(override_kazu_test_config):
     with tempfile.TemporaryDirectory() as f:
-        with initialize_config_module(config_module="azner.conf"):
-            cfg = compose(
-                config_name="config",
-                overrides=[
-                    f"DistillationTraining.model.student_model_path={BERT_TEST_MODEL_PATH}",
-                    f"DistillationTraining.model.teacher_model_path={BERT_TEST_MODEL_PATH}",
-                    f"DistillationTraining.model.data_dir={DATA_DIR}",
-                    f"DistillationTraining.save_dir={f}",
-                    "DistillationTraining.training_params.max_epochs=2",
-                ],
-            )
+        cfg = override_kazu_test_config(
+            overrides=[
+                f"DistillationTraining.model.student_model_path={BERT_TEST_MODEL_PATH}",
+                f"DistillationTraining.model.teacher_model_path={BERT_TEST_MODEL_PATH}",
+                f"DistillationTraining.model.data_dir={DATA_DIR}",
+                f"DistillationTraining.save_dir={f}",
+                "DistillationTraining.training_params.max_epochs=2",
+            ],
+        )
 
-            start(cfg)
+        start(cfg)
