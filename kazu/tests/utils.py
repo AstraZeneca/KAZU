9de0a1811938e5d5527a2e9f3e9fea4a2e71128f
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-01-25 15:36:36 +0000

    helper function in tests

--- kazu/tests/utils.py
+++ kazu/tests/utils.py
@@ -126,241 +126,125 @@ def entity_linking_hard_cases() -> Tuple[List[Document], List[str], List[str]]:
     docs, iris, sources = [], [], []
 
     doc = SimpleDocument("Lioresal")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="drug",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"drug")
     docs.append(doc)
     iris.append("CHEMBL701")
     sources.append("CHEMBL")
 
     doc = SimpleDocument("Tagrisso")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="drug",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"drug")
     docs.append(doc)
     iris.append("CHEMBL3353410")
     sources.append("CHEMBL")
 
     doc = SimpleDocument("Osimertinib")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="drug",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"drug")
     docs.append(doc)
     iris.append("CHEMBL3353410")
     sources.append("CHEMBL")
 
     doc = SimpleDocument("Osimertinib Mesylate")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="drug",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"drug")
     docs.append(doc)
     iris.append("CHEMBL3545063")
     sources.append("CHEMBL")
 
     doc = SimpleDocument("pain in the head")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="disease",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"disease")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/MONDO_0021146")
     sources.append("MONDO")
 
     doc = SimpleDocument("triple-negative breast cancer")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="disease",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"disease")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/MONDO_0005494")
     sources.append("MONDO")
 
     doc = SimpleDocument("triple negative cancer of the breast")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="disease",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"disease")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/MONDO_0005494")
     sources.append("MONDO")
 
     doc = SimpleDocument("HER2 negative breast cancer")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="disease",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"disease")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/MONDO_0000618")
     sources.append("MONDO")
 
     doc = SimpleDocument("HER2 negative cancer")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="disease",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"disease")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/MONDO_0000618")
     sources.append("MONDO")
 
     doc = SimpleDocument("bony part of hard palate")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="anatomy",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"anatomy")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/UBERON_0012074")
     sources.append("UBERON")
 
     doc = SimpleDocument("liver")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="anatomy",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"anatomy")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/UBERON_0002107")
     sources.append("UBERON")
 
     doc = SimpleDocument("stomach primordium")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="anatomy",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"anatomy")
     docs.append(doc)
     iris.append("http://purl.obolibrary.org/obo/UBERON_0012172")
     sources.append("UBERON")
 
     doc = SimpleDocument("EGFR")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="gene",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"gene")
     docs.append(doc)
     iris.append("ENSG00000146648")
     sources.append("ENSEMBL")
 
     doc = SimpleDocument("epidermal growth factor receptor")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="gene",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"gene")
     docs.append(doc)
     iris.append("ENSG00000146648")
     sources.append("ENSEMBL")
 
     doc = SimpleDocument("ERBB1")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="gene",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"gene")
     docs.append(doc)
     iris.append("ENSG00000146648")
     sources.append("ENSEMBL")
 
     doc = SimpleDocument("MAPK10")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="gene",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"gene")
     docs.append(doc)
     iris.append("ENSG00000109339")
     sources.append("ENSEMBL")
 
     doc = SimpleDocument("mitogen-activated protein kinase 10")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="gene",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"gene")
     docs.append(doc)
     iris.append("ENSG00000109339")
     sources.append("ENSEMBL")
 
     doc = SimpleDocument("JNK3")
-    doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match=doc.sections[0].get_text(),
-            entity_class="gene",
-            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
-        )
-    ]
+    add_whole_document_entity(doc,"gene")
     docs.append(doc)
     iris.append("ENSG00000109339")
     sources.append("ENSEMBL")
 
     return docs, iris, sources
 
+
+def add_whole_document_entity(doc: SimpleDocument, entity_class: str):
+    doc.sections[0].entities = [
+        Entity(
+            namespace="test",
+            match=doc.sections[0].get_text(),
+            entity_class=entity_class,
+            spans=frozenset([CharSpan(start=0, end=len(doc.sections[0].get_text()))]),
+            )
+        ]
 
 def get_TransformersModelForTokenClassificationNerStep_model_path():
     return os.getenv("TransformersModelForTokenClassificationPath")
