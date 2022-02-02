181936a94da2357994a6493d4b24b3a2597bb24e
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-02-02 20:31:14 +0000

    first impl of SethStep

--- kazu/steps/ner/seth.py
+++ kazu/steps/ner/seth.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import traceback
 from typing import Optional, List, Tuple
 
@@ -11,35 +12,43 @@ logger = logging.getLogger(__name__)
 
 
 class SethStep(BaseStep):
-    entity_class = "GENERAL_VARIANT"
-
-    def __init__(self, depends_on: Optional[List[str]]):
+    def __init__(self, depends_on: Optional[List[str]], entity_class: str, seth_fatjar_path: str):
         super().__init__(depends_on)
-        self.gateway = JavaGateway()
-        self.seth = self.gateway.entry_point
+        if not os.path.exists(seth_fatjar_path):
+            raise RuntimeError(f"required jar: {seth_fatjar_path} not found")
+
+        self.gateway = JavaGateway.launch_gateway(classpath=seth_fatjar_path, die_on_exit=True)
+        self.seth = self.gateway.jvm.com.astrazeneca.kazu.SethRunner()
+        self.entity_class = entity_class
 
     def _run(self, docs: List[Document]) -> Tuple[List[Document], List[Document]]:
         failed_docs = []
         for doc in docs:
             for section in doc.sections:
+
                 try:
                     mutation_lst = self.seth.findMutations(section.get_text())
                     entities = []
                     for mutation_dict in mutation_lst:
                         entities.append(
                             Entity.from_spans(
-                                start=mutation_dict["start"],
-                                end=mutation_dict["end"],
-                                match=mutation_dict["match"],
-                                entity_class=SethStep.entity_class,
+                                spans=[
+                                    (
+                                        int(mutation_dict["start"]),
+                                        int(mutation_dict["end"]),
+                                    )
+                                ],
+                                text=section.get_text(),
+                                entity_class=self.entity_class,
+                                namespace=self.namespace,
                                 metadata={
-                                        "found_with": mutation_dict.get("found_with"),
-                                        "protein_mutation": mutation_dict.get("protein_mutation"),
-                                        "hgvs": mutation_dict["hgvs"],
-                                    }
-                                ),
-                            )
-
+                                    "found_with": mutation_dict.get("found_with"),
+                                    "protein_mutation": mutation_dict.get("protein_mutation"),
+                                    "hgvs": mutation_dict["hgvs"],
+                                },
+                            ),
+                        )
+                    section.entities.extend(entities)
                 except Exception:
                     doc.metadata[PROCESSING_EXCEPTION] = traceback.format_exc()
                     failed_docs.append(doc)
