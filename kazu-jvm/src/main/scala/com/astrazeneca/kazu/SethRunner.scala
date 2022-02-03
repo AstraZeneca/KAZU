0c30d570219c59cb580839c0df492bc1814d4ed0
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-02-03 12:04:11 +0000

    scala docs

--- kazu-jvm/src/main/scala/com/astrazeneca/kazu/SethRunner.scala
+++ kazu-jvm/src/main/scala/com/astrazeneca/kazu/SethRunner.scala
@@ -7,6 +7,11 @@ import java.util
 import scala.collection.JavaConverters._
 
 class SethRunner {
+  /**
+   * wrapper for SETH, returning a json like Map object. This reduces cross process communication, as py4j only
+   * needs to make one call per text string. JavaConverters are needed so that Py4J correctly assigns Py4J types/
+   * unboxing is handled correctly
+   */
   val annotator = new SETH()
 
   def findMutations(text: String): util.List[util.Map[String, Any]] = {
