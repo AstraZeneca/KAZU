6210e30b7d4253728fb63a8cd73d54e000657f2a
Elliot Ford <elliot.ford@astrazeneca.com>
2022-02-07 15:45:58 +0000

    return pipeline from func that assembles it
    This saves us having to load it from file when we've just created
    the object we need to load anyway

--- kazu/modelling/ontology_matching/assemble_pipeline.py
+++ kazu/modelling/ontology_matching/assemble_pipeline.py
@@ -4,7 +4,7 @@ import spacy
 from modelling.ontology_matching.ontology_matcher import OntologyMatcher
 
 
-def main(parquet_dir: str, labels: str, span_key: str, output_dir: str):
+def main(parquet_dir: str, labels: str, span_key: str, output_dir: str) -> spacy.language.Language:
     nlp = spacy.blank("en")
     nlp.add_pipe("sentencizer")
     config = {"span_key": span_key}
@@ -13,6 +13,7 @@ def main(parquet_dir: str, labels: str, span_key: str, output_dir: str):
     ontology_matcher.set_labels(labels.split(","))
     ontology_matcher.set_ontologies(parquet_dir)
     nlp.to_disk(output_dir)
+    return nlp
 
 
 if __name__ == "__main__":
