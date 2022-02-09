4afda74cc66ae32059ebfd88e889dcd228546a44
Elliot Ford <elliot.ford@astrazeneca.com>
2022-02-09 10:57:10 +0000

    accept Iterable[str] for labels in assemble_pipeline
    This is how we want to accept it from kazu config since we
    can specify lists in yaml(/OmegaConfig/hydra). taking a string
    only and splitting it seems to be an artifact of the format of
    the spacy 3.0 project config.

--- kazu/modelling/ontology_matching/assemble_pipeline.py
+++ kazu/modelling/ontology_matching/assemble_pipeline.py
@@ -1,3 +1,5 @@
+from typing import Iterable, Union
+
 import typer
 import spacy
 
@@ -6,14 +8,19 @@ from kazu.utils.utils import PathLike, SinglePathLikeOrIterable
 
 
 def main(
-    parquet_files: SinglePathLikeOrIterable, labels: str, span_key: str, output_dir: PathLike
+    parquet_files: SinglePathLikeOrIterable,
+    labels: Union[Iterable[str], str],
+    span_key: str,
+    output_dir: PathLike,
 ) -> spacy.language.Language:
     nlp = spacy.blank("en")
     nlp.add_pipe("sentencizer")
     config = {"span_key": span_key}
     ontology_matcher = nlp.add_pipe("ontology_matcher", config=config)
     assert isinstance(ontology_matcher, OntologyMatcher)
-    ontology_matcher.set_labels(labels.split(","))
+    if isinstance(labels, str):
+        labels = labels.split(",")
+    ontology_matcher.set_labels(labels)
     ontology_matcher.set_ontologies(parquet_files)
     nlp.to_disk(output_dir)
     return nlp
