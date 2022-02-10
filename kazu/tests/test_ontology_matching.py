12f29380194e56e8580c53e440faaf61a0fdbcb0
Elliot Ford <elliot.ford@astrazeneca.com>
2022-02-10 15:45:29 +0000

    explosion port: test files
    As-is from explosion code so changes are clear (apart from changing
    line endings from CRLF to LR).

---
+++ kazu/tests/test_ontology_matching.py
@@ -0,0 +1,146 @@
+import pytest
+from pathlib import Path
+import pandas as pd
+
+import spacy
+from spacy.lang.en import English
+from spacy.tests.util import make_tempdir
+
+from ...az_ner.modelling.ontology_matching.ontology_matcher import OntologyMatcher
+from ...az_ner.modelling.ontology_matching.ontology_matcher import DISEASE, ANATOMY
+
+
+@pytest.fixture(scope="session")
+def example_text():
+    return "There is a Q42_ID and Q42_syn in this sentence, as well as Q42_syn & Q8_syn synonyms and label Q8_label."
+
+
+def _create_parquet_file(file_loc):
+    df = pd.DataFrame.from_dict(
+        {
+            "iri": ["UBERON_042", "MONDO_08"],
+            "default_label": ["Q42_label", "Q8_label"],
+            "syn": ["Q42_syn", "Q8_syn"],
+        }
+    )
+    df.to_parquet(file_loc)
+    return file_loc
+
+
+def test_constructor():
+    with make_tempdir() as d:
+        nlp = English()
+        default_config = {
+            "span_key": "my_results",
+            "entry_filter": {"@misc": "arizona.entry_filter_blacklist.v1"},
+            "variant_generator": {"@misc": "arizona.variant_generator.v1"},
+        }
+        ontology_matcher = OntologyMatcher(nlp, d, **default_config)
+        assert ontology_matcher.span_key == "my_results"
+        assert ontology_matcher.nr_strict_rules == 0
+        assert ontology_matcher.nr_lowercase_rules == 0
+        assert ontology_matcher.labels == []
+        assert ontology_matcher.ontologies == []
+
+
+def test_initialize():
+    with make_tempdir() as d:
+        nlp = English()
+        ontology_matcher = nlp.add_pipe("ontology_matcher")
+        assert isinstance(ontology_matcher, OntologyMatcher)
+        # no matcher rules are defined
+        nlp.initialize()
+        assert ontology_matcher.nr_strict_rules == 0
+        assert ontology_matcher.nr_lowercase_rules == 0
+        # non-existing dir does not work
+        with pytest.raises(ValueError):
+            ontology_matcher.set_ontologies(d / "non_existing")
+        # non-existing file does not work
+        with pytest.raises(ValueError):
+            ontology_matcher.set_ontologies(d / "single_file.parquet")
+        # existing parquet file
+        parquet_loc = _create_parquet_file(d / "single_file.parquet")
+        ontology_matcher.set_ontologies(parquet_loc)
+        assert ontology_matcher.ontologies == ["single_file.parquet"]
+        assert ontology_matcher.nr_strict_rules > 0
+        assert ontology_matcher.nr_lowercase_rules > 0
+        # single json file does not work
+        json_loc = d / "single_file.json"
+        Path(json_loc).touch()
+        with pytest.raises(ValueError):
+            ontology_matcher.set_ontologies(json_loc)
+        # subdir of files: ignore non-parquet files
+        sub_dir = d / "subdir"
+        sub_dir.mkdir()
+        _create_parquet_file(sub_dir / "file1.parquet")
+        Path(sub_dir / "file2.json").touch()
+        _create_parquet_file(sub_dir / "file3.parquet")
+        ontology_matcher.set_ontologies(sub_dir)
+        assert ontology_matcher.ontologies == ["file1.parquet", "file3.parquet"]
+
+
+def test_apply(example_text):
+    with make_tempdir() as d:
+        nlp = English()
+        parquet_loc = d / "onto_synonyms.parquet"
+        parquet_file = _create_parquet_file(parquet_loc)
+        config = {"span_key": "my_hits"}
+        ontology_matcher = nlp.add_pipe("ontology_matcher", config=config)
+        assert isinstance(ontology_matcher, OntologyMatcher)
+        ontology_matcher.set_labels([ANATOMY, DISEASE])
+        ontology_matcher.set_ontologies(parquet_file)
+        doc = nlp(example_text)
+        matches = doc.spans["my_hits"]
+        assert len(matches) == 3
+        assert set([m.text for m in matches]) == {"Q42_syn", "Q8_syn"}
+        assert set([m.label_ for m in matches]) == {"Anatomy", "Disease"}
+        assert set([m.kb_id_ for m in matches]) == {"UBERON_042", "MONDO_08"}
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    "labels,results",
+    [
+        ([], 0),
+        ([ANATOMY], 2),
+        ([DISEASE], 1),
+        ([ANATOMY, DISEASE], 3),
+        (["RANDOM LABEL", ANATOMY, DISEASE], 3),
+    ]
+)
+# fmt: on
+def test_labels(example_text, labels, results):
+    with make_tempdir() as d:
+        nlp = English()
+        parquet_loc = d / "onto_synonyms.parquet"
+        parquet_file = _create_parquet_file(parquet_loc)
+        config = {"span_key": "my_hits"}
+        ontology_matcher = nlp.add_pipe("ontology_matcher", config=config)
+        assert isinstance(ontology_matcher, OntologyMatcher)
+        ontology_matcher.set_labels(labels)
+        ontology_matcher.set_ontologies(parquet_file)
+        doc = nlp(example_text)
+        matches = doc.spans["my_hits"]
+        assert len(matches) == results
+
+
+def test_serialization(example_text):
+    with make_tempdir() as d:
+        nlp1 = English()
+        parquet_loc = d / "onto_synonyms.parquet"
+        parquet_file = _create_parquet_file(parquet_loc)
+        config = {"span_key": "my_hits"}
+        ontology_matcher = nlp1.add_pipe("ontology_matcher", config=config)
+        assert isinstance(ontology_matcher, OntologyMatcher)
+        ontology_matcher.set_labels([ANATOMY, DISEASE])
+        ontology_matcher.set_ontologies(parquet_file)
+        doc1 = nlp1(example_text)
+        spans1 = [(s.start, s.end, s.text) for s in doc1.spans["my_hits"]]
+        nlp_loc = d / "ontology_pipeline"
+        nlp1.to_disk(nlp_loc)
+        nlp2 = spacy.load(nlp_loc)
+        doc2 = nlp2(example_text)
+        spans2 = [(s.start, s.end, s.text) for s in doc2.spans["my_hits"]]
+        assert len(spans1) == 3
+        assert len(spans2) == 3
+        assert spans1 == spans2
---
+++ kazu/tests/test_spacy_ner_and_el_pipeline.py
@@ -0,0 +1,80 @@
+import spacy
+import pytest
+from pathlib import Path
+
+from ...az_ner.modelling.ontology_matching.ontology_matcher import OntologyMatcher
+
+ONTOLOGIES_DIR = Path(__file__).parent.parent.parent.parent / "ontologies_processed/"
+DEFAULT_KEY = "test_spans"
+LABELS = "GGP,Chemical,Anatomy,Disease,Cell_line"
+
+
+@pytest.fixture(scope="module")
+def nlp():
+    nlp = spacy.blank("en")
+    nlp.add_pipe("sentencizer")
+    config = {"span_key": DEFAULT_KEY}
+    ontology_matcher = nlp.add_pipe("ontology_matcher", config=config)
+    assert isinstance(ontology_matcher, OntologyMatcher)
+    ontology_matcher.set_labels(LABELS.split(","))
+    ontology_matcher.set_ontologies(ONTOLOGIES_DIR)
+    return nlp
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    "sentence,entities",
+    [
+        ("The mean (SD) HAVOC score was 2.6.", []),
+        ("Diseases associated with WAS include Wiskott Syndrome and Thrombocytopenia 1.", ["WAS", "Wiskott Syndrome", "Thrombocytopenia 1"]),
+        ("MEDI8897 is a recombinant human RSV monoclonal antibody", ["MEDI8897"]),
+        ("We aimed to confirm these findings in patients with a BRCA1 or BRCA2 mutation", ["BRCA1", "BRCA2"]),
+        ("Gastrointestinal AEs were typically low-grade.", []),
+        ("Few clinical trials in asthma have focused on Hispanic populations.", ["asthma"]),
+        ("These patients were treated with abemaciclib.", ["abemaciclib"]),
+        ("Blood was sampled pre- and post-dose on Day 32.", ["Blood"]),
+        ("TIME cells express readily detectable telomerase activity. There is TIME !", ["TIME"]),
+        ("Subjects with prevalent kidney disease were randomized to linagliptin or placebo added to usual care.", ["kidney disease", "linagliptin"]),
+        ("The increase in lifespan is matched by time free from incident cardiovascular disease.", ["cardiovascular disease", "lifespan"]),
+        ("The necuparanib arm had a higher incidence of haematologic toxicity.", ["necuparanib"]),
+        ("This was a single-arm trial. My arm hurts.", ["arm"]),
+        ("We value life more than anything.", ["life"]),
+        ("The main endpoint is quality of life.", []),
+        ("The main endpoint is quality-of-life.", []),
+        ("IVF, with or without ICSI, was received in all 500 patients.", []),
+        ("All three decontamination processes reduced bacteria counts similarly.", []),
+        ("The primary endpoint was MFS.", []),
+        ("Mean glycated haemoglobin concentration was 66 mmol/mol (8.2%).", []),
+        ("Studying pembrolizumab plus neoadjuvant chemotherapy in early-stage breast cancer.", ["pembrolizumab", "breast cancer"]),
+        ("Vandetanib plus docetaxel led to a significant improvement in PFS versus placebo plus docetaxel.", ["Vandetanib", "docetaxel", "docetaxel"]),
+        ("Anifrolumab dose-dependently suppressed the IFN gene signature.", ["IFN", "Anifrolumab"]),
+        ("Antiplatelet effects of citalopram in patients with ischemic stroke", ["citalopram", "ischemic stroke"]),
+        ("We reviewed 19 patients with the Dandy-Walker syndrome", ["Dandy-Walker syndrome"]),
+        ("We reviewed 19 patients with the Dandy Walker syndrome", ["Dandy Walker syndrome"]),
+    ]
+)
+# fmt: on
+def test_ner_results(nlp, sentence, entities):
+    doc = nlp(sentence)
+    pred_spans = list(doc.spans[DEFAULT_KEY])
+    assert set([s.text for s in pred_spans]) == set(entities)
+    assert len(pred_spans) == len(entities)
+
+
+# fmt: off
+@pytest.mark.parametrize(
+    "sentence,entities",
+    [
+        ("We aimed to confirm these findings in patients with a BRCA1 or BRCA2 mutation", ["ENSG00000012048", "ENSG00000139618"]),
+        ("These patients were treated with abemaciclib.", ["CHEMBL3301610"]),
+        ("Blood was sampled pre- and post-dose on Day 32.", ["UBERON_0000178"]),
+        ("TIME cells express readily detectable telomerase activity", ["CVCL_0047"]),
+        ("Studying pembrolizumab plus neoadjuvant chemotherapy in early-stage breast cancer.", ["CHEMBL3137343", "MONDO_0007254"]),
+    ]
+)
+# fmt: on
+def test_nel_results(nlp, sentence, entities):
+    doc = nlp(sentence)
+    pred_spans = list(doc.spans[DEFAULT_KEY])
+    assert set([s.kb_id_ for s in pred_spans]) == set(entities)
+    assert len(pred_spans) == len(entities)
