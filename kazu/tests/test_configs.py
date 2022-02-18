d4f865653324515c065d9623321f5c08ba1d686f
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-02-18 11:13:38 +0000

    removed old test_configs.py

--- kazu/tests/test_configs.py
+++
@@ -1,8 +0,0 @@
-from kazu.pipeline import Pipeline, load_steps
-from kazu.tests.utils import requires_model_pack
-
-
-@requires_model_pack
-def test_configs(kazu_test_config):
-    # test the default pipeline can load/configs are all correct
-    Pipeline(steps=load_steps(kazu_test_config))
