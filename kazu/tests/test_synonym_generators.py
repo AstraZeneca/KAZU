c59fc938d25ef9835d6fe3b8c9be046f003ce010
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-03-24 16:18:03 +0000

    CombinatorialSynonymGenerator test

--- kazu/tests/test_synonym_generators.py
+++ kazu/tests/test_synonym_generators.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Union
 
 from hydra.utils import instantiate
 
@@ -10,14 +10,17 @@ from kazu.modelling.ontology_preprocessing.synonym_generation import (
     StopWordRemover,
     StringReplacement,
     GreekSymbolSubstitution,
+    CombinatorialSynonymGenerator,
 )
 from kazu.tests.utils import requires_model_pack
 
 
 def check_generator_result(
-    expected_syns: List[str], generator: SynonymGenerator, input: Dict[str, List[SynonymData]]
+    expected_syns: List[str],
+    generator: Union[CombinatorialSynonymGenerator, SynonymGenerator],
+    data: Dict[str, List[SynonymData]],
 ):
-    result = generator(input)
+    result = generator(data)
     new_syns = list(result.keys())
     assert len(new_syns) == len(expected_syns)
     assert all([x in new_syns for x in expected_syns])
@@ -27,42 +30,64 @@ def check_generator_result(
 def test_SeparatorExpansion(kazu_test_config):
     spacy_pipeline = instantiate(kazu_test_config.SpacyPipeline)
     generator = SeparatorExpansion(spacy_pipeline)
-    input = {"ABAC (ABAC1/ABAC2)": [SynonymData(idx="text", mapping_type=[])]}
+    data = {"ABAC (ABAC1/ABAC2)": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = ["ABAC", "ABAC1", "ABAC2", "ABAC1/ABAC2", "ABAC ABAC1/ABAC2"]
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
 
 
 def test_CaseModifier():
     generator = CaseModifier(lower=True)
-    input = {"ABAC": [SynonymData(idx="text", mapping_type=[])]}
+    data = {"ABAC": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = ["abac"]
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
 
 
 @requires_model_pack
 def test_StopWordRemover(kazu_test_config):
     spacy_pipeline = instantiate(kazu_test_config.SpacyPipeline)
     generator = StopWordRemover(spacy_pipeline=spacy_pipeline)
-    input = {"The cat sat on the mat": [SynonymData(idx="text", mapping_type=[])]}
+    data = {"The cat sat on the mat": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = ["cat sat mat"]
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
 
 
 def test_StringReplacement():
     generator = StringReplacement(replacement_dict={"cat": ["dog", "chicken"]})
-    input = {"The cat sat on the mat": [SynonymData(idx="text", mapping_type=[])]}
+    data = {"The cat sat on the mat": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = ["The dog sat on the mat", "The chicken sat on the mat"]
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
 
 
 def test_GreekSymbolSubstitution():
     generator = GreekSymbolSubstitution()
-    input = {"alpha-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
+    data = {"alpha-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = ["α-thalassaemia"]
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
-    input = {"α-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
+    data = {"α-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = ["alpha-thalassaemia", "a-thalassaemia"]
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
-    input = {"A-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
+    data = {"A-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
     expected_syns = []
-    check_generator_result(expected_syns=expected_syns, generator=generator, input=input)
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
+
+
+def test_CombinatorialSynonymGenerator():
+    generator = CombinatorialSynonymGenerator(
+        [
+            GreekSymbolSubstitution(),
+            CaseModifier(lower=True),
+            StringReplacement(replacement_dict={"-": [" ", "_"]}),
+        ]
+    )
+    data = {"Alpha-thalassaemia": [SynonymData(idx="text", mapping_type=[])]}
+    expected_syns = [
+        "alpha-thalassaemia",
+        "Alpha thalassaemia",
+        "Alpha_thalassaemia",
+        "α-thalassaemia",
+        "alpha thalassaemia",
+        "alpha_thalassaemia",
+        "α thalassaemia",
+        "α_thalassaemia",
+    ]
+    check_generator_result(expected_syns=expected_syns, generator=generator, data=data)
