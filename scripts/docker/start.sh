cb31c87fe6fe36be756566a92a263b280c74aa2b
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-05-16 12:13:38 +0100

    updated conf for web

--- scripts/docker/start.sh
+++ scripts/docker/start.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
 #download the model pack using Azure SAS
-echo "downloading model pack"
-mkdir -p model_pack
-wget --progress=bar:force:noscroll "$MODEL_PACK" -O model_pack/model_pack.zip
-#unpack the models
-echo "unzipping models"
-cd model_pack
-unzip  model_pack.zip
-mv model_pack/* .
+#echo "downloading model pack"
+#mkdir -p model_pack
+#wget --progress=bar:force:noscroll "$MODEL_PACK" -O model_pack/model_pack.zip
+##unpack the models
+#echo "unzipping models"
+#cd model_pack
+#unzip  model_pack.zip
+#mv model_pack/* .
 export KAZU_MODEL_PACK=/home/ray/model_pack/
-cd ..
 echo "running ray deployment"
-RAY_SERVE_CONFIG="${RAY_SERVE_CONFIG:-local}"
-echo "ray config set to ${RAY_SERVE_CONFIG}"
+#RAY_SERVE_CONFIG="${RAY_SERVE_CONFIG:-local}"
+#echo "ray config set to ${RAY_SERVE_CONFIG}"
 export TOKENIZERS_PARALLELISM=false
-python -m kazu.web.server \
-ray=${RAY_SERVE_CONFIG} \
-hydra.run.dir="."
+python -m kazu.web.server --config-path "/home/ray/kazu/conf" hydra.run.dir="." pipeline=web
 echo "ray deployment complete"
