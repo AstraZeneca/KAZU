485c65b07d2ba84416d566897240a9c9023877e2
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-05-24 22:53:39 +0100

    arrow 8.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "pytorch-lightning==1.5.8",
         "pydash==5.1.0",
         "pandas==1.3.4",
-        "pyarrow==5.0.0",
+        "pyarrow==8.0.0",
         "pytorch-metric-learning==0.9.99",
         "rapidfuzz==1.8.2",
         "seqeval==1.2.2",
