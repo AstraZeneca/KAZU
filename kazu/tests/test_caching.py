c1fdb07ba3d7fde08137f49db0545f557e37e5f6
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-05-30 14:57:33 +0100

    fixing tests

--- kazu/tests/test_caching.py
+++ kazu/tests/test_caching.py
@@ -14,7 +14,7 @@ from kazu.modelling.ontology_preprocessing.base import (
     SYN,
     MAPPING_TYPE,
 )
-from kazu.tests.utils import BERT_TEST_MODEL_PATH
+from kazu.tests.utils import BERT_TEST_MODEL_PATH, DummyParser
 from kazu.utils.caching import (
     EmbeddingIndexCacheManager,
     DictionaryIndexCacheManager,
@@ -22,35 +22,13 @@ from kazu.utils.caching import (
 )
 from kazu.utils.link_index import Index
 
-DUMMY_SOURCE = "test_parser"
-DUMMY_DATA = {
-    IDX: ["first", "first", "second", "second", "third", "alpha"],
-    DEFAULT_LABEL: ["1", "1", "2", "2", "3", "4"],
-    SYN: ["1", "one", "2", "two", "3", "1"],
-    MAPPING_TYPE: ["int", "text", "int", "text", "int", "text"],
-}
-
-
-def dummy_df() -> pd.DataFrame:
-    return pd.DataFrame.from_dict(DUMMY_DATA)
-
-
-class DummyParser(OntologyParser):
-    name = DUMMY_SOURCE
-
-    def find_kb(self, string: str) -> str:
-        return DUMMY_SOURCE
-
-    def parse_to_dataframe(self) -> pd.DataFrame:
-
-        return dummy_df()
 
 
 def test_enumerate_dataframe_chunks():
     # + 1 to check we can handle chunk sizes larger than the df
     parser = DummyParser("")
     parser.populate_metadata_database()
-    for chunk_size in range(1, len(DUMMY_DATA[IDX]) + 1):
+    for chunk_size in range(1, len(DummyParser.DUMMY_DATA[IDX]) + 1):
         print(f"chunk size: {chunk_size}")
         res = list(EmbeddingIndexCacheManager.enumerate_database_chunks(parser.name, chunk_size))
         for _, split_df in res[:-1]:
@@ -60,7 +38,7 @@ def test_enumerate_dataframe_chunks():
 
 @pytest.mark.parametrize("index_type", ["MatMulTensorEmbeddingIndex", "CDistTensorEmbeddingIndex"])
 def test_embedding_cache_manager(tmp_path: Path, index_type: str):
-    in_path = tmp_path.joinpath(DUMMY_SOURCE)
+    in_path = tmp_path.joinpath(DummyParser.DUMMY_SOURCE)
     in_path.mkdir()
     model = PLSapbertModel(model_name_or_path=str(BERT_TEST_MODEL_PATH))
     trainer = Trainer(logger=False)
@@ -78,7 +56,7 @@ def test_embedding_cache_manager(tmp_path: Path, index_type: str):
 
     index = manager.get_or_create_ontology_indices()[0]
     assert isinstance(index, Index)
-    assert len(index) == len(set(DUMMY_DATA[IDX]))
+    assert len(index) == len(set(DummyParser.DUMMY_DATA[IDX]))
     # there should now be a cached file at the parent of the target in_path
     cache_dir_item = [x for x in os.listdir(tmp_path) if x.startswith("cached")][0]
     cache_dir = tmp_path.joinpath(cache_dir_item)
@@ -102,7 +80,7 @@ def test_embedding_cache_manager(tmp_path: Path, index_type: str):
 
 
 def test_dictionary_cache_manager(tmp_path: Path):
-    in_path = tmp_path.joinpath(DUMMY_SOURCE)
+    in_path = tmp_path.joinpath(DummyParser.DUMMY_SOURCE)
     in_path.mkdir()
     parser = DummyParser(in_path=str(in_path))
     manager = DictionaryIndexCacheManager(
@@ -110,7 +88,7 @@ def test_dictionary_cache_manager(tmp_path: Path):
     )
     index = manager.get_or_create_ontology_indices()[0]
     assert isinstance(index, Index)
-    assert len(index) == len(set(DUMMY_DATA[IDX]))
+    assert len(index) == len(set(DummyParser.DUMMY_DATA[IDX]))
     # there should now be a cached file at the parent of the target in_path
     cache_dir_item = [x for x in os.listdir(tmp_path) if x.startswith("cached")][0]
     cache_dir = tmp_path.joinpath(cache_dir_item)
@@ -127,12 +105,12 @@ def test_dictionary_cache_manager(tmp_path: Path):
 
 
 def test_cached_index_group(tmp_path: Path):
-    in_path1 = tmp_path.joinpath(DUMMY_SOURCE + "1")
+    in_path1 = tmp_path.joinpath(DummyParser.DUMMY_SOURCE + "1")
     in_path1.mkdir()
 
     parser1 = DummyParser(in_path=str(in_path1))
     parser1.name = "ontology_1"
-    in_path2 = tmp_path.joinpath(DUMMY_SOURCE + "2")
+    in_path2 = tmp_path.joinpath(DummyParser.DUMMY_SOURCE + "2")
     in_path2.mkdir()
     parser2 = DummyParser(in_path=str(in_path2))
     parser2.name = "ontology_2"
