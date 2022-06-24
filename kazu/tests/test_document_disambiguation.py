fb1bf0c9d06ea44a2d86293750371b3471d3cb10
Elliot Ford <elliot.ford@astrazeneca.com>
2022-06-24 10:23:07 +0100

    make step common fixture to tests
    instead of instantiating the same step once per test

--- kazu/tests/test_document_disambiguation.py
+++ kazu/tests/test_document_disambiguation.py
@@ -1,4 +1,6 @@
+import pytest
 from hydra.utils import instantiate
+
 from kazu.data.data import (
     Document,
     Entity,
@@ -10,6 +12,7 @@ from kazu.data.data import (
 from kazu.data.data import LinkRanks, EquivalentIdAggregationStrategy
 from kazu.modelling.ontology_preprocessing.base import MetadataDatabase
 from kazu.steps.other.document_disambiguationv2 import (
+    DocumentLevelDisambiguationStep,
     DISAMBIGUATED_BY,
     DISAMBIGUATED_BY_DEFINED_ELSEWHERE,
     DISAMBIGUATED_BY_REACTOME,
@@ -18,8 +21,13 @@ from kazu.tests.utils import requires_model_pack
 from kazu.utils.link_index import Hit, DICTIONARY_HITS
 
 
+@pytest.fixture
+def doc_level_disamb_step(kazu_test_config) -> DocumentLevelDisambiguationStep:
+    return instantiate(kazu_test_config.DocumentLevelDisambiguationStep)
+
+
 @requires_model_pack
-def test_kb_disambiguation(kazu_test_config):
+def test_kb_disambiguation(doc_level_disamb_step):
     metadata_db = MetadataDatabase()
     EXPECTED_IDX = "ENSG00000111276"
     PATHWAY_LINKED_IDX = "ENSG00000110092"
@@ -27,8 +35,6 @@ def test_kb_disambiguation(kazu_test_config):
     metadata_db.add(
         "ENSEMBL", {EXPECTED_IDX: {"default_label": "cyclin dependent kinase inhibitor 1B"}}
     )
-
-    step = instantiate(kazu_test_config.DocumentLevelDisambiguationStep)
     text = "p27 can be many things, but it often co-occurs with cyclin D1"
     doc = Document.create_simple_document(text)
 
@@ -85,7 +91,7 @@ def test_kb_disambiguation(kazu_test_config):
         )
     ]
     doc.sections[0].entities = [ambig_ent, unambig_ent]
-    step([doc])
+    doc_level_disamb_step([doc])
     result_entities = doc.get_entities()
     assert len(result_entities) == 2
     assert len(result_entities[0].mappings) == 1
@@ -99,9 +105,8 @@ def test_kb_disambiguation(kazu_test_config):
 
 
 @requires_model_pack
-def test_document_disambiguation_s1(kazu_test_config):
+def test_document_disambiguation_s1(doc_level_disamb_step):
     # scenario 1. Two entities, one entity with one ambiguous mapping, one with one unambiguous mapping
-    step = instantiate(kazu_test_config.DocumentLevelDisambiguationStep)
     EXPECTED_IDX = "I'm right"
     text = "p27 can be many things, but in this case it's CDKN1B"
     doc = Document.create_simple_document(text)
@@ -156,7 +161,7 @@ def test_document_disambiguation_s1(kazu_test_config):
         )
     ]
     doc.sections[0].entities = [ambig_ent, unambig_ent]
-    step([doc])
+    doc_level_disamb_step([doc])
     result_entities = doc.get_entities()
     assert len(result_entities) == 2
     assert len(result_entities[0].mappings) == 1
@@ -166,9 +171,8 @@ def test_document_disambiguation_s1(kazu_test_config):
 
 
 @requires_model_pack
-def test_document_disambiguation_s2(kazu_test_config):
+def test_document_disambiguation_s2(doc_level_disamb_step):
     # scenario 2. Three entities, two ambiguous entities with one mapping, one unambiguous with one mapping
-    step = instantiate(kazu_test_config.DocumentLevelDisambiguationStep)
     EXPECTED_IDX = "I'm right"
 
     text = "p27 can be many things, but in this case it's CDKN1B"
@@ -239,7 +243,7 @@ def test_document_disambiguation_s2(kazu_test_config):
         )
     ]
     doc.sections[0].entities = [ambig_ent, ambig_ent, unambig_ent]
-    step([doc])
+    doc_level_disamb_step([doc])
     result_entities = doc.get_entities()
     assert len(result_entities) == 3
     assert len(result_entities[0].mappings) == 1
@@ -259,9 +263,8 @@ def test_document_disambiguation_s2(kazu_test_config):
 
 
 @requires_model_pack
-def test_document_disambiguation_s3(kazu_test_config):
+def test_document_disambiguation_s3(doc_level_disamb_step):
     # scenario 3. Two entities, one entity with one ambiguous mapping and one unabiguous mapping, one with one unambiguous mapping
-    step = instantiate(kazu_test_config.DocumentLevelDisambiguationStep)
     EXPECTED_IDX = "I'm right"
     text = "p27 can be many things, but in this case it's CDKN1B"
     doc = Document.create_simple_document(text)
@@ -327,7 +330,7 @@ def test_document_disambiguation_s3(kazu_test_config):
         )
     ]
     doc.sections[0].entities = [ambig_ent, unambig_ent]
-    step([doc])
+    doc_level_disamb_step([doc])
     result_entities = doc.get_entities()
     assert len(result_entities) == 2
     assert len(result_entities[0].mappings) == 2
