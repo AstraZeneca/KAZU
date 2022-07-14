370d0962d9cd90640eb78c1703f2504750d70d92
Elliot Ford <elliot.ford@astrazeneca.com>
2022-07-14 17:56:39 +0100

    add sphinx doctest extension

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = "Korea University, AstraZeneca"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "myst_parser"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.doctest", "myst_parser"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
