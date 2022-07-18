3c7922be1a666f2bfb4fbeae196811ea45740e77
Elliot Ford <elliot.ford@astrazeneca.com>
2022-07-18 17:18:12 +0100

    add comments explaining weird typing usage

--- kazu/utils/grouping.py
+++ kazu/utils/grouping.py
@@ -1,10 +1,15 @@
 from itertools import groupby
 from typing import Callable, Iterable, Tuple, TypeVar, TYPE_CHECKING
 
+# see https://github.com/python/typeshed/tree/master/stdlib/_typeshed
+# In short, this is needed as
+# the _typeshed package and its types do not exist at runtime
 if TYPE_CHECKING:
     from _typeshed import SupportsRichComparison
 
 Item = TypeVar("Item")
+# we need to use Supports RichComparison here otherwise mypy complains
+# about the 'key' argument to sorted
 Key = TypeVar("Key", bound="SupportsRichComparison")
 
 
