f8d141fab7151f51da83ecacfa6628c34b65d341
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-08-04 13:21:42 +0100

    docs

--- kazu/modelling/language/string_similarity_scorers.py
+++ kazu/modelling/language/string_similarity_scorers.py
@@ -8,11 +8,22 @@ from strsimpy import NGram
 
 
 class StringSimilarityScorer(ABC):
+    """
+    calculates a NumericMetric based on a string match or a normalised string match and a normalised term
+    """
+
     @abstractmethod
     def calculate(self, match_norm: str, syn_norm: str) -> NumericMetric:
         raise NotImplementedError()
 
     def __call__(self, match: str, match_norm: str, term_norm: str) -> NumericMetric:
+        """
+
+        :param match: a string
+        :param match_norm: a normalised version of the string
+        :param term_norm: a normalised version of the string to compare to
+        :return:
+        """
         return self.calculate(match_norm=match_norm, syn_norm=term_norm)
 
 
@@ -33,6 +44,10 @@ class NGramStringSimilarityScorer(StringSimilarityScorer):
 
 
 class NumberMatchStringSimilarityScorer(BooleanStringSimilarityScorer):
+    """
+    checks all numbers in match_norm are represented in term_norm
+    """
+
     number_finder = re.compile("[0-9]+")
 
     def calculate(self, match_norm: str, syn_norm: str) -> bool:
@@ -42,6 +57,10 @@ class NumberMatchStringSimilarityScorer(BooleanStringSimilarityScorer):
 
 
 class EntitySubtypeStringSimilarityScorer(BooleanStringSimilarityScorer):
+    """
+    checks all TYPE x mentions in match norm are represented in term norm
+    """
+
     numeric_class_phrases = [re.compile(x) for x in ["TYPE [0-9]+"]]
 
     def calculate(self, match_norm: str, syn_norm: str) -> bool:
@@ -59,6 +78,10 @@ class EntitySubtypeStringSimilarityScorer(BooleanStringSimilarityScorer):
 
 
 class EntityNounModifierStringSimilarityScorer(BooleanStringSimilarityScorer):
+    """
+    checks all modifier phrased in match_norm are represented in term_norm
+    """
+
     noun_modifier_phrases = ["LIKE", "SUBUNIT", "PSEUDOGENE", "RECEPTOR"]
 
     def calculate(self, match_norm: str, syn_norm: str) -> bool:
@@ -79,6 +102,11 @@ class EntityNounModifierStringSimilarityScorer(BooleanStringSimilarityScorer):
 
 
 class RapidFuzzStringSimilarityScorer(StringSimilarityScorer):
+    """
+    uses rapid fuzz to calculate string similarity. Note, if the token count >4 and match_norm has
+    more than 10 chars, token_sort_ratio is used. Otherwise WRatio is used
+    """
+
     def calculate(self, match_norm: str, syn_norm: str) -> NumericMetric:
         if len(match_norm) > 10 and len(match_norm.split(" ")) > 4:
             return fuzz.token_sort_ratio(match_norm, syn_norm)
