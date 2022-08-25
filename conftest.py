a76183ff6a739fa4413054fab53b8e979eec8aef
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-08-25 15:01:01 +0100

    created mapping test fixutes

--- conftest.py
+++ conftest.py
@@ -1,9 +1,12 @@
-from typing import List
+from typing import List, Tuple, Set
 
 import pytest
 from hydra import compose, initialize_config_dir
 
-from kazu.tests.utils import CONFIG_DIR
+from kazu.data.data import SynonymTermWithMetrics
+from kazu.modelling.database.in_memory_db import SynonymDatabase
+from kazu.modelling.ontology_preprocessing.base import IDX, DEFAULT_LABEL, SYN, MAPPING_TYPE
+from kazu.tests.utils import CONFIG_DIR, DummyParser, make_dummy_parser
 
 
 @pytest.fixture(scope="session")
@@ -25,3 +28,56 @@ def override_kazu_test_config():
 @pytest.fixture(scope="session")
 def kazu_test_config(override_kazu_test_config):
     return override_kazu_test_config(overrides=[])
+
+
+@pytest.fixture(scope="session")
+def set_up_p27_test_case() -> Tuple[Set[SynonymTermWithMetrics], DummyParser]:
+
+    dummy_data = {
+        IDX: ["1", "1", "2", "2", "2", "3", "3", "3"],
+        DEFAULT_LABEL: ["CDKN1B", "CDKN1B", "PAK2", "PAK2", "PAK2", "ZNRD2", "ZNRD2", "ZNRD2"],
+        SYN: [
+            "cyclin-dependent kinase inhibitor 1B (p27, Kip1)",
+            "CDKN1B",
+            "PAK-2p27",
+            "p27",
+            "PAK2",
+            "Autoantigen p27",
+            "ZNRD2",
+            "p27",
+        ],
+        MAPPING_TYPE: ["", "", "", "", "", "", "", ""],
+    }
+    parser = make_dummy_parser(
+        in_path="", data=dummy_data, name="test_tfidf_parsr", source="test_tfidf_parsr"
+    )
+    parser.populate_databases()
+    terms_with_metrics = set(
+        SynonymTermWithMetrics.from_synonym_term(term)
+        for term in SynonymDatabase().get_all(parser.name).values()
+    )
+    return terms_with_metrics, parser
+
+
+@pytest.fixture(scope="session")
+def set_up_disease_mapping_test_case() -> Tuple[Set[SynonymTermWithMetrics], DummyParser]:
+
+    dummy_data = {
+        IDX: ["1", "1", "2"],
+        DEFAULT_LABEL: ["Heck's disease", "Heck's disease", "Neck Disease"],
+        SYN: [
+            "Heck's disease",
+            "Heck disease",
+            "Neck Disease",
+        ],
+        MAPPING_TYPE: ["", "", ""],
+    }
+    parser = make_dummy_parser(
+        in_path="", data=dummy_data, name="test_tfidf_parsr", source="test_tfidf_parsr"
+    )
+    parser.populate_databases()
+    terms_with_metrics = set(
+        SynonymTermWithMetrics.from_synonym_term(term)
+        for term in SynonymDatabase().get_all(parser.name).values()
+    )
+    return terms_with_metrics, parser
