435fea3cc58a5ed25d19c86ba195de6846427f56
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-08-31 10:39:53 +0100

    precommit newline fix

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import os
-from typing import List,Tuple, Set
+from typing import List, Tuple, Set
 import pytest
 from hydra import compose, initialize_config_dir
 
@@ -69,4 +69,3 @@ def acceptance_test_docs() -> List[Document]:
             docs.append(Document.from_json(item))
 
     return docs
-
