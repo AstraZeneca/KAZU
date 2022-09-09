3e8fd63103ee2873b390ab5d57568515450f792a
Elliot Ford <elliot.ford@astrazeneca.com>
2022-09-09 11:26:48 +0100

    show base classes in docs

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,7 @@ html_theme = "furo"
 autodoc_default_options = {
     "members": True,
     "undoc-members": True,
+    "show-inheritance": True,
 }
 
 # put type hints in the description rather than the function signature, where it all ends up on one line and looks very ugly
