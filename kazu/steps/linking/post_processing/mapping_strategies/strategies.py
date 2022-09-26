689050b0882ecb52ec923bf10cbe484db13d70bb
Richard Jackson <RichJackson@users.noreply.github.com>
2022-09-26 13:10:36 +0100

    refactor: consistency in getting a set item
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/steps/linking/post_processing/mapping_strategies/strategies.py
+++ kazu/steps/linking/post_processing/mapping_strategies/strategies.py
@@ -464,7 +464,7 @@ class StrongMatchWithEmbeddingConfirmationStringMatchingStrategy(StrongMatchMapp
         for term in synonym_term_sorted_by_score:
             if term.associated_id_sets not in selected_id_sets:
                 selected_id_sets.add(term.associated_id_sets)
-                if self.complex_string_scorer(ent_match, list(term.terms)[0]):
+                if self.complex_string_scorer(ent_match, next(iter(term.terms))):
                     selected_terms.add(term)
         return selected_terms
 
