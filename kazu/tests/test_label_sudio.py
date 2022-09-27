34d32433e2e1ef9f8850bb3359631706964a5322
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-09-27 14:10:45 +0100

    bug: fixed conversion call in test

--- kazu/tests/test_label_sudio.py
+++ kazu/tests/test_label_sudio.py
@@ -3,7 +3,7 @@ import os
 import pytest
 
 from kazu.data.data import Document, Entity, Mapping, LinkRanks
-from kazu.modelling.annotation.label_studio import LabelStudioManager
+from kazu.modelling.annotation.label_studio import LabelStudioManager, KazuToLabelStudioConverter
 from kazu.tests.utils import requires_label_studio
 from kazu.utils.grouping import sort_then_group
 
@@ -75,7 +75,7 @@ def test_kau_doc_to_label_studio():
 
     manager = create_test_manager()
     manager.delete_project_if_exists()
-    tasks = manager.convert_docs_to_tasks([doc_1])
+    tasks = KazuToLabelStudioConverter.convert_docs_to_tasks([doc_1])
     manager.create_linking_project(tasks)
 
     docs = manager.export_from_ls()
