daa9e9e80eb45c234ea8215c3024f70569d849c2
Fernandez <rolando.fernandez@astrazeneca.net>
2022-10-18 10:19:35 +0100

    simplified entity filtering

--- kazu/steps/other/cleanup.py
+++ kazu/steps/other/cleanup.py
@@ -1,11 +1,9 @@
-import itertools
 import traceback
-from typing import List, Tuple, Optional, Protocol, Callable, TypeVar
+from typing import List, Tuple, Optional, Protocol, Callable, Iterable
 from kazu.data.data import Document, Entity, PROCESSING_EXCEPTION
 from kazu.steps import BaseStep
 
 EntityFilterFn = Callable[[Entity], bool]
-T = TypeVar("T")
 
 
 class CleanupAction(Protocol):
@@ -20,44 +18,21 @@ class EntityFilterFnProvider(Protocol):
 
 class EntityFilterCleanupAction:
     def __init__(self, filter_fns: List[EntityFilterFn]):
-        self.combined_filter_fn = self.combine_filter_fns(filter_fns)
-
-    @staticmethod
-    def combine_filter_fns(filter_fns: List[EntityFilterFn]) -> EntityFilterFn:
-        def combined_filter_fn(ent: Entity) -> bool:
-            return any(filter_fn(ent) for filter_fn in filter_fns)
-
-        return combined_filter_fn
-
-    @staticmethod
-    def from_filter_fn_providers(
-        filter_providers: List[EntityFilterFnProvider],
-    ) -> "EntityFilterCleanupAction":
-        filter_fns = list(
-            itertools.chain.from_iterable(
-                (filter_provider.filter_fns() for filter_provider in filter_providers)
-            )
-        )
-        return EntityFilterCleanupAction(filter_fns)
+        self.filter_fns = filter_fns
 
     def cleanup(self, doc: Document):
         for section in doc.sections:
-            section_ents = set(section.entities)
-            filtered_ents = set(filter(self.combined_filter_fn, section_ents))
-
-            section_ents.difference_update(filtered_ents)
-            section.entities = list(section_ents)
+            section.entities = [
+                entity for entity in section.entities if not any(f(entity) for f in self.filter_fns)
+            ]
 
 
-class DropUnmappedEntsEntityFilters:
+class DropUnmappedEntityFilter:
     def __init__(self, from_ent_namespaces: Iterable[str]):
         self.from_ent_namespaces = set(from_ent_namespaces)
 
-    def filter_fns(self) -> List[EntityFilterFn]:
-        return [
-            (lambda ent: ent.namespace == ns and len(ent.mappings) == 0)
-            for ns in self.from_ent_namespaces
-        ]
+    def __call__(self, ent: Entity) -> bool:
+        return ent.namespace in self.from_ent_namespaces and len(ent.mappings) == 0
 
 
 class CleanupStep(BaseStep):
