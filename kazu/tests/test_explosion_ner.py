a4e87a622693886d56f9be11f62154c38a7609cd
Elliot Ford <elliot.ford@astrazeneca.com>
2022-10-24 17:44:39 +0100

    resolve synonymDB loading issue in test
    Bit of a hack, but not a problem at runtime, and this is good enough
    until we have resolved the relevant GitHub issue properly.

--- kazu/tests/test_explosion_ner.py
+++ kazu/tests/test_explosion_ner.py
@@ -7,6 +7,10 @@ from kazu.tests.utils import ner_simple_test_cases, requires_model_pack
 @requires_model_pack
 def test_ExplosionStringMatchingStep(kazu_test_config):
     # note, here we just test that the step is functional. Model performance is tested via an acceptance test
+
+    # note: loading the dictionary linking step is necessary as it caches the synonym db (and metadata db)
+    # we have a task to address this on GitHub as issue #312
+    instantiate(kazu_test_config.DictionaryEntityLinkingStep)
     step = instantiate(kazu_test_config.ExplosionStringMatchingStep)
     docs = [Document.create_simple_document(x[0]) for x in ner_simple_test_cases()]
     successes, failures = step(docs)
