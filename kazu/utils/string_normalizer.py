2b6a81916a5bb4a13c66353c98a0d06618fcc4fb
Fernandez <rolando.fernandez@astrazeneca.net>
2022-11-16 11:28:51 +0000

    type fixes

--- kazu/utils/string_normalizer.py
+++ kazu/utils/string_normalizer.py
@@ -1,7 +1,7 @@
 import re
 import regex
 from functools import lru_cache
-from typing import Optional, Dict, Protocol, Type
+from typing import Optional, Dict, Protocol, Type, Tuple
 
 from kazu.modelling.language.language_phenomena import GREEK_SUBS
 
