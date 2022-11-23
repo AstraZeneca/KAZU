08715a0d2c73feca0f11b9e8c2ddcc13357076ea
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-11-23 14:33:57 +0000

    updated assemble_pipeline.py to use new algorithm for matching terms to SynonymTerm

--- kazu/modelling/ontology_matching/assemble_pipeline.py
+++ kazu/modelling/ontology_matching/assemble_pipeline.py
@@ -1,7 +1,6 @@
-from typing import Dict, Iterable, Union, List
+from typing import List
 
 import spacy
-
 from kazu.modelling.ontology_matching.ontology_matcher import OntologyMatcher, SPAN_KEY
 from kazu.modelling.ontology_preprocessing.base import OntologyParser
 from kazu.utils.utils import PathLike
@@ -15,59 +14,50 @@ def custom_tokenizer(nlp):
 
 
 def main(
-    parser_name_to_entity_type: Dict[str, str],
-    labels: Union[Iterable[str], str],
     output_dir: PathLike,
-    parsers: List[OntologyParser] = None,
+    parsers: List[OntologyParser],
     curated_list: PathLike = None,
     span_key: str = SPAN_KEY,
 ) -> spacy.language.Language:
-    """Generates, serializes and returns a Spacy pipeline with an :class:`~kazu.modelling.ontology_matching.ontology_matcher.OntologyMatcher`.
+    """Generates, serializes and returns a Spacy pipeline with an
+    :class:`~kazu.modelling.ontology_matching.ontology_matcher.OntologyMatcher`.
 
     Generates an English Spacy pipeline with a tokenizer, a sentencizer with default
     config, and an OntologyMatcher based on the input parameters. The pipeline is
     written to disk, and also returned to the caller.
 
-    This is built with either a set of parsers in the case of a 'from scratch' build, for a new parser for example, or with a curated/annotated
-    list of synonyms.
+    If 'parsers' is provided without an argument for 'curated_list', the
+    :class:`~kazu.modelling.ontology_matching.ontology_matcher.OntologyMatcher` is built directly
+    from the configured synonyms from the parsers (with any associated generated synonyms). This is
+    useful for trying to understand which strings are 'noisy', but not recommended for production as raw ontology
+    data tends to need some curation before it can be applied.
 
-    :param parser_name_to_entity_type: a dictionary from parser names to the entity class we want
-        to associate them with.
-    :param labels: the entity class labels that the
-        :class:`kazu.steps.joint_ner_and_linking.explosion.ExplosionStringMatchingStep` can
-        recognise - this affects the matchers that are generated.
+    if a curated_list is also provided, the build process will attempt to match these to the
+    :class:`~kazu.data.data.SynonymTerm`\'s coming from each parser. If a curated item can't be matched
+    this will be logged as a warning.
+
     :param output_dir: the output directory to write the pipeline into.
-    :param parsers: parsers provided to build pipeline from using their associated synonyms.
-    :param curated_list: curated list to build pipeline from using the synonyms in the list.
+    :param parsers: build the pipeline using these parsers as a data source
+    :param curated_list: optional path to a jsonl file. Each line should be json with fields:
+        'term', 'case_sensitive', entity_class
     :param span_key: the key to use within the generated Spacy Docs'
         `span attribute <https://spacy.io/api/doc#spans>`_ to store and access recognised NER
         spans.
     """
-
-    if parsers is None and curated_list is None:
-        raise ValueError(
-            "Exactly one of parsers and curated_list needs to be provided (and not None) to get any synonyms to build the OntologyMatcher"
-        )
-    elif parsers is not None and curated_list is not None:
-        raise ValueError(
-            "Both parsers and curated_list passed - we build the pipeline from either one of these at a time, not both."
-        )
-
     nlp = spacy.blank("en")
     custom_tokenizer(nlp)
     nlp.add_pipe("sentencizer")
-    config = {"span_key": span_key, "parser_name_to_entity_type": parser_name_to_entity_type}
+    config = {
+        "span_key": span_key,
+        "parser_name_to_entity_type": {parser.name: parser.entity_class for parser in parsers},
+    }
     ontology_matcher = nlp.add_pipe("ontology_matcher", config=config)
     assert isinstance(ontology_matcher, OntologyMatcher)
-    if isinstance(labels, str):
-        labels = labels.split(",")
-    ontology_matcher.set_labels(labels)
 
-    if parsers is not None:
+    if curated_list is None:
         ontology_matcher.create_lowercase_phrasematcher_from_parsers(parsers)
     else:
-        assert curated_list is not None
-        ontology_matcher.create_phrasematchers_from_curated_list(curated_list)
+        ontology_matcher.create_phrasematchers_from_curated_list(curated_list, parsers)
 
     nlp.to_disk(output_dir)
     return nlp
