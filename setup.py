a15e32bba0d7b978ecd874caa402ee893e49b285
Kazu Version Bump <kazu.versionbump@users.noreply.github.com>
2022-11-25 11:06:16 +0000

    Bump version: 0.0.10 → 0.0.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kazu",
-    version="0.0.10",
+    version="0.0.11",
     license="Apache 2.0",
     author="AstraZeneca AI and Korea University",
     description="NER",
