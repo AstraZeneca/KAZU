a8cb2b8e333608155c389ba1cd98de54e63b719d
Richard Jackson <RichJackson@users.noreply.github.com>
2022-11-29 16:03:01 +0000

    test refactor
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/tests/test_ontology_parser.py
+++ kazu/tests/test_ontology_parser.py
@@ -1,8 +1,5 @@
 import dataclasses
 import json
-import tempfile
-from itertools import chain
-from pathlib import Path
 
 import pytest
 
@@ -12,70 +9,73 @@ from kazu.modelling.ontology_preprocessing.base import CuratedTermDataset
 from kazu.tests.utils import DummyParser
 
 
-def test_injected_synonyms():
-    with tempfile.TemporaryDirectory() as temp_dir:
-        path = Path(temp_dir).joinpath("injections.jsonl")
-        missing_parser_name = "missing_parser_1"
-        with open(path, "w") as f:
+def test_injected_synonyms(tmp_path):
+    path = tmp_path.joinpath("injections.jsonl")
+    missing_parser_name = "missing_parser_1"
 
-            first_injected_synonyms = CuratedTerm(
-                term="hello I'm injected",
-                action="keep",
-                case_sensitive=False,
-                entity_class="injection_test",
-                curated_id_mappings={
-                    "injection_test_parser_1": "first",
-                    "injection_test_parser_2": "second",
-                },
-            )
-            second_injected_synonyms = CuratedTerm(
-                term="hello I'm also injected",
-                action="keep",
-                case_sensitive=False,
-                entity_class="injection_test",
-                curated_id_mappings={
-                    "injection_test_parser_1": "third",
-                    "injection_test_parser_2": "alpha",
-                    missing_parser_name: "blah",
-                },
-            )
-            null_injected_synonyms = CuratedTerm(
-                term="I don't match anything",
-                action="keep",
-                case_sensitive=False,
-                entity_class="injection_test",
-                curated_id_mappings={missing_parser_name: "blah"},
-            )
-            f.write(json.dumps(dataclasses.asdict(first_injected_synonyms)) + "\n")
-            f.write(json.dumps(dataclasses.asdict(second_injected_synonyms)) + "\n")
-            f.write(json.dumps(dataclasses.asdict(null_injected_synonyms)) + "\n")
-
-        parser = DummyParser(
-            name="injection_test_parser_1",
-            in_path="",
+    injected_synonym_curated_terms = (
+        CuratedTerm(
+            term="hello I'm injected",
+            action="keep",
+            case_sensitive=False,
             entity_class="injection_test",
-            additional_synonyms_dataset=CuratedTermDataset(str(path)),
-        )
-        parser2 = DummyParser(
-            name="injection_test_parser_2",
-            in_path="",
+            curated_id_mappings={
+                "injection_test_parser_1": "first",
+                "injection_test_parser_2": "second",
+            },
+        ),
+        CuratedTerm(
+            term="hello I'm also injected",
+            action="keep",
+            case_sensitive=False,
             entity_class="injection_test",
-            additional_synonyms_dataset=CuratedTermDataset(str(path)),
-        )
-        parser.populate_databases()
-        parser2.populate_databases()
-        db = SynonymDatabase()
+            curated_id_mappings={
+                "injection_test_parser_1": "third",
+                "injection_test_parser_2": "alpha",
+                missing_parser_name: "blah",
+            },
+        ),
+        # won't match any parser
+        CuratedTerm(
+            term="I don't match anything",
+            action="keep",
+            case_sensitive=False,
+            entity_class="injection_test",
+            curated_id_mappings={missing_parser_name: "blah"},
+        ),
+    )
 
-        for term in [first_injected_synonyms, second_injected_synonyms, null_injected_synonyms]:
-            for parser_name, idx in term.curated_id_mappings.items():
-                if parser_name != missing_parser_name:
-                    term_norms = db.get_syns_for_id(name=parser_name, idx=idx)
-                    term_strings = set(
-                        chain.from_iterable(
-                            db.get(name=parser_name, synonym=x).terms for x in term_norms
-                        )
-                    )
-                    assert term.term in term_strings
-                else:
-                    with pytest.raises(KeyError):
-                        db.get_syns_for_id(name=parser_name, idx=idx)
+    with open(path, "w") as f:
+        f.writelines(
+            json.dumps(dataclasses.asdict(curated_term)) + "\n"
+            for curated_term in injected_synonym_curated_terms
+        )
+
+    parser = DummyParser(
+        name="injection_test_parser_1",
+        in_path="",
+        entity_class="injection_test",
+        additional_synonyms_dataset=CuratedTermDataset(path),
+    )
+    parser2 = DummyParser(
+        name="injection_test_parser_2",
+        in_path="",
+        entity_class="injection_test",
+        additional_synonyms_dataset=CuratedTermDataset(path),
+    )
+    parser.populate_databases()
+    parser2.populate_databases()
+    db = SynonymDatabase()
+
+    for term in injected_synonym_curated_terms:
+        for parser_name, idx in term.curated_id_mappings.items():
+            if parser_name != missing_parser_name:
+                term_norms = db.get_syns_for_id(name=parser_name, idx=idx)
+                term_strings = set(
+                    term for x in term_norms for term in db.get(name=parser_name, synonym=x).terms
+                )
+                assert term.term in term_strings
+            else:
+                with pytest.raises(KeyError):
+                    db.get_syns_for_id(name=parser_name, idx=idx)
+
