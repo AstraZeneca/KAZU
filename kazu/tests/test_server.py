37afec29a88375337425bd7ef38e23fea3fea442
Richard Jackson <richard.jackson4@astrazeneca.com>
2022-11-29 14:49:56 +0000

    improved test to ensure auth is actually working

--- kazu/tests/test_server.py
+++ kazu/tests/test_server.py
@@ -17,10 +17,23 @@ def test_api(ray_server, kazu_test_config):
 
 
 @requires_model_pack
-@pytest.mark.parametrize("server_type", ["ray_server", "ray_server_with_jwt_auth"])
-def test_batch_api(server_type, request, kazu_test_config):
+@pytest.mark.parametrize(
+    (
+        "server_type",
+        "should_fail_auth",
+    ),
+    [
+        pytest.param("ray_server", False),
+        pytest.param("ray_server_with_jwt_auth", False),
+        pytest.param("ray_server_with_jwt_auth", True),
+    ],
+)
+def test_batch_api(server_type, should_fail_auth, request, kazu_test_config):
     headers = request.getfixturevalue(server_type)
-    data = requests.post(
+    if should_fail_auth:
+        headers = {}
+
+    response = requests.post(
         f"http://127.0.0.1:{kazu_test_config.ray.serve.port}/api/{KAZU}/batch",
         headers=headers,
         json=[
@@ -32,10 +45,14 @@ def test_batch_api(server_type, request, kazu_test_config):
                 }
             },
         ],
-    ).json()
-    doc0_section0 = data[0]["sections"][0]
-    assert len(doc0_section0["entities"]) > 0
-    assert doc0_section0["entities"][0]["match"] == "EGFR"
+    )
+    if should_fail_auth:
+        assert response.status_code == 401
+    else:
+        data = response.json()
+        doc0_section0 = data[0]["sections"][0]
+        assert len(doc0_section0["entities"]) > 0
+        assert doc0_section0["entities"][0]["match"] == "EGFR"
 
-    doc1_section1 = data[1]["sections"][1]
-    assert doc1_section1["entities"][0]["match"] == "BRCA1"
+        doc1_section1 = data[1]["sections"][1]
+        assert doc1_section1["entities"][0]["match"] == "BRCA1"
