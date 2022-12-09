d0cbfb2fc9b298f576ca4afd4d1127ef9cb3c8c1
Kazu Version Bump <kazu.versionbump@users.noreply.github.com>
2022-12-09 17:42:02 +0000

    Bump version: 0.0.15 → 0.0.16

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ webserver_dependencies = [
 
 setup(
     name="kazu",
-    version="0.0.15",
+    version="0.0.16",
     license="Apache 2.0",
     author="AstraZeneca AI and Korea University",
     description="NER",
