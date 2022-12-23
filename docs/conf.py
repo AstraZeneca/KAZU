0dbb546cedf966989d431f9b8f7dccc003dba497
Elliot Ford <elliot.ford@astrazeneca.com>
2022-12-23 11:17:59 +0000

    mypy fix
    have to add explicit 'Any' as mypy 'knows' that obj starts as a module,
    so has that in a Union type for it otherwise, which makes mypy unhappy
    when you ass it to unwrap, which mypy wants a function for (this doesn't
    seem strictly right either, it's just that only a function will return
    something interesting).

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ import inspect
 import os
 import subprocess
 import sys
+from typing import Any
 
 import kazu
 
@@ -137,7 +138,7 @@ def linkcode_resolve(domain, info):
     if submod is None:
         return None
 
-    obj = submod
+    obj: Any = submod
     for part in fullname.split("."):
         try:
             # pandas has a warnings.catch_warnings context manager here
