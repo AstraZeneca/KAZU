24329187981925361cd0d50db38f28295ad72183
Elliot Ford <elliot.ford@astrazeneca.com>
2023-01-31 14:30:18 +0000

    make type hint more accurate and permissive
    spacy supports loading from pathlib.Paths, so we should type hint so our
    users known they can do that too if they want to.

--- kazu/utils/spacy_pipeline.py
+++ kazu/utils/spacy_pipeline.py
@@ -2,7 +2,9 @@ from functools import cache
 
 import spacy
 
+from kazu.utils.utils import PathLike
+
 
 @cache
-def cached_spacy_pipeline_load(path: str) -> spacy.Language:
+def cached_spacy_pipeline_load(path: PathLike) -> spacy.Language:
     return spacy.load(path)
