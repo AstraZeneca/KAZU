567cc02e2c441594e916f32183251e7e677aaeb5
Elliot Ford <elliot.ford@astrazeneca.com>
2023-02-09 15:46:03 +0000

    docs: more license details for scispacy abbreviation

--- kazu/utils/abbreviation_detector.py
+++ kazu/utils/abbreviation_detector.py
@@ -4,6 +4,33 @@ Original Credit:
 | https://github.com/allenai/scispacy
 | https://github.com/allenai/scispacy/blob/main/scispacy/abbreviation.py
 
+Licensed under Apache 2.0
+
+Copyright 2019 the Allen Institute for Artificial Intelligence (AI2)
+
+.. raw:: html
+
+    <details>
+    <summary>Full License Notice</summary>
+
+Copyright 2019 the Allen Institute for Artificial Intelligence (AI2)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+.. raw:: html
+
+    </details>
+
 Paper:
 
 Mark Neumann, Daniel King, Iz Beltagy, and Waleed Ammar. 2019.
