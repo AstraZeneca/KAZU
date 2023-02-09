03806f73f0f77625c4c11b66cfd92546a863c3bf
Elliot Ford <elliot.ford@astrazeneca.com>
2023-02-09 14:04:00 +0000

    docs: AutoNameEnum origins and license links
    Add links to the docs where this comes from, a link to the license and
    the license itself. We don't have to do this legally, but it seems nice
    to, and with a collapsible element for the license it's not too
    disruptive to the docs to do so.

--- kazu/data/data.py
+++ kazu/data/data.py
@@ -28,7 +28,30 @@ JsonDictType = Union[Dict[str, Any], List, int, float, bool, str, NoneType]
 class AutoNameEnum(Enum):
     """Subclass to create an Enum where values are the names when using enum.auto
 
-    Taken from the Python Enum Docs (licensed under Zero-Clause BSD)."""
+    Taken from the `Python Enum Docs <https://docs.python.org/3/howto/enum.html#using-automatic-values>`_.
+
+    This is `licensed under Zero-Clause BSD. <https://docs.python.org/3/license.html#zero-clause-bsd-license-for-code-in-the-python-release-documentation>`_
+
+    .. raw:: html
+
+        <details>
+        <summary>Full License</summary>
+
+    Permission to use, copy, modify, and/or distribute this software for any
+    purpose with or without fee is hereby granted.
+
+    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+    REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+    AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+    INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+    LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+    OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+    PERFORMANCE OF THIS SOFTWARE.
+
+    .. raw:: html
+
+        </details>
+    """
 
     def _generate_next_value_(name, start, count, last_values):
         return name
