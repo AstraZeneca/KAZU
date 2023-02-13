f7f814a21a364fb93a79560bdf508d7da15ce79e
Fernandez <rolando.fernandez@astrazeneca.net>
2023-02-13 13:31:26 +0000

    added title and blurb to the UI

--- frontend/src/components/app.tsx
+++ frontend/src/components/app.tsx
@@ -30,6 +30,8 @@ class App extends React.Component<AppProps, AppState> {
         }))
     }
 
+
+
     render() {
         const kazuResp = this.state.text_ner_result;
         let lsComponent;
@@ -42,9 +44,26 @@ class App extends React.Component<AppProps, AppState> {
             jsonViewComponent = undefined
         }
 
+        const appTitleText = (
+            <div className={"app-title-text"}>
+                <span>KAZU Named Entity Recognition</span>
+            </div>
+        )
+
+        const kazuBlurb = (
+            <div className={"kazu-blurb"}><span><strong>KAZU</strong> is our next gen NLP framework, specifically designed to deal with
+                the complexities of BioNER. It's designed to be production grade, resilient, fast, scalable, and easy to
+                extend, allowing you to quickly deploy emergent state-of-the-art NLP methods.
+
+                For more information and support, please visit <a href={"https://astrazeneca.github.com/kazu"}>https://astrazeneca.github.com/kazu</a>
+            </span>
+            </div>
+        )
         return (
             <div className="app-root">
+                {appTitleText}
                 <TextSubmit ner_response_callback={this.setKazuResponse.bind(this)} kazu_client={this.props.kazuApiClient} auth_enabled={this.props.authEnabled}/>
+                {kazuBlurb}
                 {lsComponent}
                 {jsonViewComponent}
             </div>
