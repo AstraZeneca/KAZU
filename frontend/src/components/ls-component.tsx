c3e879b678d98154a6348868aa83898194d6232b
Fernandez <rolando.fernandez@astrazeneca.net>
2023-02-16 11:56:07 +0000

    removed more unused code

--- frontend/src/components/ls-component.tsx
+++ frontend/src/components/ls-component.tsx
@@ -1,9 +1,7 @@
 // @ts-ignore -- this library doesn't provide type annotations
-
 import LabelStudio from "@heartexlabs/label-studio";
 import React, {useEffect, useRef} from 'react';
-import {Entity, KazuWebDocument, KazuLSResponse, Section} from "../types/types";
-import * as R from "rambda";
+import {KazuLSResponse} from "../types/types";
 
 
 type LabelStudioReactProps = {
@@ -45,36 +43,6 @@ const LabelStudioReact = (props: LabelStudioReactProps) => {
     );
 }
 
-
-type LSPrediction = {
-
-    "from_name": "tag",
-    "id": string,
-    "source": "$text",
-    "to_name": "text",
-    "ground_truth": true,
-    "type": "rectanglelabels",
-    "value": {
-        "start": number,
-        "end": number,
-        "labels": string[]
-    }
-}
-
-
-type LSTask = {
-    annotations: [],
-    predictions: {
-        result: LSPrediction[],
-        created_ago: string,
-        model_version: string
-    }[]
-    id: number,
-    data: {
-        text: string
-    }
-}
-
 type LSComponentProps = {
     kazuLSAnnotations?: KazuLSResponse;
 }
@@ -89,53 +57,6 @@ class LSComponent extends React.Component<LSComponentProps, LSComponentState> {
         this.state = {
 
         }
-    }
-
-    private static lsTaskFromKazuDocument(kazuDocument: KazuWebDocument): LSTask {
-        return {
-            annotations: [],
-            data: {text: kazuDocument.sections[0].text},
-            id: kazuDocument.sections[0].text.length,
-            predictions: [{
-                created_ago: "3 hours",
-                result: LSComponent.lsPredictionsFromKazuDocument(kazuDocument),
-                model_version: "model 1"
-            }]
-        }
-    }
-
-    private static lsPredictionsFromKazuDocument(kazuDocument: KazuWebDocument): LSPrediction[] {
-        const ents: Entity[] = R.chain((section: Section) => section.entities)(kazuDocument.sections);
-        return R.map((ent: Entity) => LSComponent.lsPredictionFromKazuEntity(ent))(ents);
-    }
-
-    private static lsPredictionFromKazuEntity(ent: Entity): LSPrediction {
-        return {
-            "from_name": "tag",
-            "id": `${ent.match}_${ent.start}-${ent.end}`,
-            "source": "$text",
-            "to_name": "text",
-            "ground_truth": true,
-            "type": "rectanglelabels",
-            "value": {
-                "start": ent.start,
-                "end": ent.end,
-                "labels": [ent.entity_class]
-            }
-        }
-    }
-
-    private static labelConfig(text: string) {
-        return `<View>
-                    <Labels name="tag" toName="text">
-                      <Label value="gene"></Label>
-                      <Label value="anatomy"></Label>
-                      <Label value="disease"></Label>
-                      <Label value="drug"></Label>
-                      <Label value="cell_line"></Label>
-                    </Labels>
-                    <Text name="text" value="$text"></Text>
-                </View>`
     }
 
     render() {
