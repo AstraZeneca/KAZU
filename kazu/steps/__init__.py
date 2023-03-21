17425d2de94994c8dbc5f5020c78d9a934916922
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-03-21 11:49:54 +0000

    removed unused SapBertForEntityLinkingStep step as is not worth refactoring for new cache model

--- kazu/steps/__init__.py
+++ kazu/steps/__init__.py
@@ -2,7 +2,6 @@ from kazu.steps.step import Step, document_iterating_step, document_batch_step
 from kazu.steps.document_post_processing.abbreviation_finder import AbbreviationFinderStep
 from kazu.steps.joint_ner_and_linking.explosion import ExplosionStringMatchingStep
 from kazu.steps.linking.dictionary import DictionaryEntityLinkingStep
-from kazu.steps.linking.sapbert import SapBertForEntityLinkingStep
 from kazu.steps.linking.mapping_step import MappingStep
 from kazu.steps.ner.hf_token_classification import TransformersModelForTokenClassificationNerStep
 from kazu.steps.ner.seth import SethStep
