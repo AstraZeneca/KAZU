15ff25ba40c32b0485b3665f8d8f89ac97e9785a
Elliot Ford <elliot.ford@astrazeneca.com>
2023-05-17 09:19:34 +0100

    parametrize similarity scorer tests

--- kazu/tests/test_string_similarity_scorers.py
+++ kazu/tests/test_string_similarity_scorers.py
@@ -1,5 +1,7 @@
 from typing import Sequence
 
+import pytest
+
 from kazu.data.data import EquivalentIdSet, EquivalentIdAggregationStrategy, SynonymTerm
 from kazu.language.string_similarity_scorers import (
     EntitySubtypeStringSimilarityScorer,
@@ -10,79 +12,53 @@ from kazu.language.string_similarity_scorers import (
 from kazu.utils.string_normalizer import StringNormalizer
 
 
-def test_EntitySubtypeStringSimilarityScorer():
-    syn_term_1 = make_term_for_scorer_test(["type II diabetes", "type 2 diabetes"])
-    syn_term_2 = make_term_for_scorer_test(["type I diabetes", "type 1 diabetes"])
-    scorer = EntitySubtypeStringSimilarityScorer()
-    ent_match = "diabetes, type 2"
-    assert scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_1.term_norm
-    )
-    assert not scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_2.term_norm
-    )
+@pytest.mark.parametrize(
+    ("scorer", "ent_match", "matching_synonyms", "not_matching_synonyms"),
+    (
+        (
+            EntitySubtypeStringSimilarityScorer(),
+            "diabetes, type 2",
+            ["type II diabetes", "type 2 diabetes"],
+            ["type I diabetes", "type 1 diabetes"],
+        ),
+        pytest.param(
+            EntitySubtypeStringSimilarityScorer(),
+            "PPP1R 14C",
+            [
+                "protein phosphatase 1 regulatory inhibitor subunit 14C",
+            ],
+            ["protein phosphatase 1 regulatory inhibitor subunit 14D"],
+            marks=pytest.mark.xfail,
+        ),
+        (NumberMatchStringSimilarityScorer(), "MAP1LC3A gene", ["MAP1LC3A"], ["MAP2LC3A"]),
+        (
+            EntityNounModifierStringSimilarityScorer(noun_modifier_phrases=["LIKE"]),
+            "CPI17 like",
+            ["CPI17-like"],
+            ["CPI17"],
+        ),
+        (
+            EntityNounModifierStringSimilarityScorer(noun_modifier_phrases=["PSEUDOGENE"]),
+            "CPI17",
+            ["CPI17"],
+            ["CPI17 pseudogene"],
+        ),
+        (
+            EntityNounModifierStringSimilarityScorer(["RECEPTOR"]),
+            "EGF receptor",
+            ["epidermal growth factor receptor"],
+            ["epidermal growth factor"],
+        ),
+    ),
+)
+def test_boolean_scorer(scorer, ent_match, matching_synonyms, not_matching_synonyms):
+    matching_syn_term = make_term_for_scorer_test(matching_synonyms)
+    not_matching_syn_term = make_term_for_scorer_test(not_matching_synonyms)
 
-    # TODO: currently this test fails due to inappropriate string normalisation. ideally, we want this scorer
-    # to also capture patters like 14C, 14D
-    # syn_term_1 = make_term_for_scorer_test(["protein phosphatase 1 regulatory inhibitor subunit 14C",])
-    # syn_term_2 = make_term_for_scorer_test(["protein phosphatase 1 regulatory inhibitor subunit 14D"])
-    # scorer = EntitySubtypeStringSimilarityScorer()
-    # ent_match = "PPP1R 14C"
-    # assert scorer(reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_1.term_norm)
-    # assert not scorer(reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_2.term_norm)
+    ent_match_norm = StringNormalizer.normalize(ent_match)
 
-
-def test_NumberMatchStringSimilarityScorer():
-    syn_term_1 = make_term_for_scorer_test(["MAP1LC3A"])
-    syn_term_2 = make_term_for_scorer_test(["MAP2LC3A"])
-    scorer = NumberMatchStringSimilarityScorer()
-    ent_match = "MAP1LC3A gene"
-    assert scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_1.term_norm
-    )
-    assert not scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_2.term_norm
-    )
-
-
-def test_EntityNounModifierStringSimilarityScorer():
-
-    syn_term_1 = make_term_for_scorer_test(["CPI17-like"])
-    syn_term_2 = make_term_for_scorer_test(["CPI17"])
-
-    scorer = EntityNounModifierStringSimilarityScorer(noun_modifier_phrases=["LIKE"])
-    ent_match = "CPI17 like"
-
-    assert scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_1.term_norm
-    )
-    assert not scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_2.term_norm
-    )
-
-    syn_term_1 = make_term_for_scorer_test(["CPI17"])
-    syn_term_2 = make_term_for_scorer_test(["CPI17 pseudogene"])
-
-    scorer = EntityNounModifierStringSimilarityScorer(noun_modifier_phrases=["PSEUDOGENE"])
-    ent_match = "CPI17"
-    assert scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_1.term_norm
-    )
-    assert not scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_2.term_norm
-    )
-
-    syn_term_1 = make_term_for_scorer_test(["epidermal growth factor receptor"])
-    syn_term_2 = make_term_for_scorer_test(["epidermal growth factor"])
-
-    scorer = EntityNounModifierStringSimilarityScorer(["RECEPTOR"])
-    ent_match = "EGF receptor"
-    assert scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_1.term_norm
-    )
-    assert not scorer(
-        reference_term=StringNormalizer.normalize(ent_match), query_term=syn_term_2.term_norm
-    )
+    assert scorer(reference_term=ent_match_norm, query_term=matching_syn_term.term_norm)
+    assert not scorer(reference_term=ent_match_norm, query_term=not_matching_syn_term.term_norm)
 
 
 def test_RapidFuzzStringSimilarityScorer():
