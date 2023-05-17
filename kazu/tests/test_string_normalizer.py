4e0a890fbf24b73ccb642d31d57950262455689b
Elliot Ford <elliot.ford@astrazeneca.com>
2023-05-17 08:50:02 +0100

    parametrize string normalizer tests

--- kazu/tests/test_string_normalizer.py
+++ kazu/tests/test_string_normalizer.py
@@ -1,56 +1,24 @@
+import pytest
+
 from kazu.utils.string_normalizer import StringNormalizer
 
 
-def check_case(original, expected, entity_class):
-    result = StringNormalizer.normalize(original_string=original, entity_class=entity_class)
+@pytest.mark.parametrize(
+    ("original", "expected"),
+    (
+        ("MOP-2", "MOP 2"),
+        ("y(+)L-type amino acid transporter 1", "Y (+) L TYPE AMINO ACID TRANSPORTER 1"),
+        ("mTOR", "MTOR"),
+        ("egfr", "EGFR"),
+        ("erbB2", "ERBB 2"),
+        ("egfr(-)", "ERGR (-)"),
+        ("C0D0C4J1X3", "C0D0C4J1X3"),
+        ("JAK-2", "JAK 2"),
+        ("JAK2", "JAK 2"),
+        ("MPNs", "MPN"),
+        ("TESTIN gene", "TESTIN GENE"),
+    ),
+)
+def test_normalizer(original, expected):
+    result = StringNormalizer.normalize(original_string=original, entity_class="gene")
     assert result == expected
-
-
-def test_normalizer():
-    original = "MOP-2"
-    expected = "MOP 2"
-    check_case(original, expected, "gene")
-
-    original = "y(+)L-type amino acid transporter 1"
-    expected = "Y (+) L TYPE AMINO ACID TRANSPORTER 1"
-    check_case(original, expected, "gene")
-
-    original = "mTOR"
-    expected = "MTOR"
-    check_case(original, expected, "gene")
-
-    original = "egfr"
-    expected = "EGFR"
-    check_case(original, expected, "gene")
-
-    original = "erbB2"
-    expected = "ERBB 2"
-    check_case(original, expected, "gene")
-
-    original = "egfr(-)"
-    expected = "EGFR (-)"
-    check_case(original, expected, "gene")
-
-    original = "C0D0C4J1X3"
-    expected = "C0D0C4J1X3"
-    check_case(original, expected, None)
-
-    original = "JAK-2"
-    expected = "JAK 2"
-    check_case(original, expected, "gene")
-
-    original = "JAK 2"
-    expected = "JAK 2"
-    check_case(original, expected, "gene")
-
-    original = "JAK2"
-    expected = "JAK 2"
-    check_case(original, expected, "gene")
-
-    original = "MPNs"
-    expected = "MPN"
-    check_case(original, expected, "gene")
-
-    original = "TESTIN gene"
-    expected = "TESTIN GENE"
-    check_case(original, expected, "gene")
