2cf4d1f2b7f730726557cf2fcd1bdb075d79debb
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-05-17 14:25:52 +0100

    upgraded mapping step to parser dependent version

--- kazu/steps/linking/mapping_step.py
+++ kazu/steps/linking/mapping_step.py
@@ -1,18 +1,24 @@
 import logging
+from typing import Iterable
 
 from kazu.data.data import Document
-from kazu.steps import Step, document_iterating_step
+from kazu.modelling.ontology_preprocessing.base import OntologyParser
+from kazu.steps import document_iterating_step
 from kazu.steps.linking.post_processing.strategy_runner import StrategyRunner
+from kazu.steps.step import ParserDependentStep
 
 logger = logging.getLogger(__name__)
 
 
-class MappingStep(Step):
-    """
-    A wrapper for :class:`.StrategyRunner`, so it can be used in a pipeline.
-    """
+class MappingStep(ParserDependentStep):
+    """A wrapper for :class:`.StrategyRunner`\\, so it can be used in a pipeline."""
 
-    def __init__(self, strategy_runner: StrategyRunner):
+    def __init__(
+        self,
+        parsers: Iterable[OntologyParser],
+        strategy_runner: StrategyRunner,
+    ):
+        super().__init__(parsers)
         self.strategy_runner = strategy_runner
 
     @document_iterating_step
