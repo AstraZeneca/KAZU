92bb658be46201db4b164095716bcea5fb946807
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-07-19 15:46:08 +0100

    fixed type in EFGR and entity class variabel missing

--- kazu/tests/test_string_normalizer.py
+++ kazu/tests/test_string_normalizer.py
@@ -4,21 +4,21 @@ from kazu.utils.string_normalizer import StringNormalizer
 
 
 @pytest.mark.parametrize(
-    ("original", "expected"),
+    ("original", "expected", "entity_class"),
     (
-        ("MOP-2", "MOP 2"),
-        ("y(+)L-type amino acid transporter 1", "Y (+) L TYPE AMINO ACID TRANSPORTER 1"),
-        ("mTOR", "MTOR"),
-        ("egfr", "EGFR"),
-        ("erbB2", "ERBB 2"),
-        ("egfr(-)", "ERGR (-)"),
-        ("C0D0C4J1X3", "C0D0C4J1X3"),
-        ("JAK-2", "JAK 2"),
-        ("JAK2", "JAK 2"),
-        ("MPNs", "MPN"),
-        ("TESTIN gene", "TESTIN GENE"),
+        ("MOP-2", "MOP 2", "gene"),
+        ("y(+)L-type amino acid transporter 1", "Y (+) L TYPE AMINO ACID TRANSPORTER 1", "gene"),
+        ("mTOR", "MTOR", "gene"),
+        ("egfr", "EGFR", "gene"),
+        ("erbB2", "ERBB 2", "gene"),
+        ("egfr(-)", "EGFR (-)", "gene"),
+        ("C0D0C4J1X3", "C0D0C4J1X3", None),
+        ("JAK-2", "JAK 2", "gene"),
+        ("JAK2", "JAK 2", "gene"),
+        ("MPNs", "MPN", "gene"),
+        ("TESTIN gene", "TESTIN GENE", "gene"),
     ),
 )
-def test_normalizer(original, expected):
-    result = StringNormalizer.normalize(original_string=original, entity_class="gene")
+def test_normalizer(original, expected, entity_class):
+    result = StringNormalizer.normalize(original_string=original, entity_class=entity_class)
     assert result == expected
