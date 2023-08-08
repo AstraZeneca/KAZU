e5b0db63b65a1a02314beafed37e62ef7b6d9412
Elliot Ford <elliot.ford@astrazeneca.com>
2023-08-08 14:37:08 +0100

    docs: intersphinx ignore spacy classes for custom tokenizer
    Otherwise we get a sphinx warning (which then gets turned into an error)
    complaining that intersphinx can't link to these classes. We can't do
    this because Spacy's docs have a custom build process with no apparent
    sphinx support.

--- docs/conf.py
+++ docs/conf.py
@@ -218,6 +218,8 @@ nitpick_ignore = [
     ("py:class", "spacy.matcher.phrasematcher.PhraseMatcher"),
     ("py:class", "spacy.tokens.doc.Doc"),
     ("py:class", "spacy.tokens.span.Span"),
+    ("py:class", "spacy.lang.en.English"),
+    ("py:class", "spacy.lang.en.EnglishDefaults"),
     # stanza doesn't appear to build API docs (and the build process is unclear - they may not even use Sphinx at all)
     ("py:class", "stanza.pipeline.core.Pipeline"),
     # starlette doesn't appear to build API docs, and doesn't seem to use Sphinx.
