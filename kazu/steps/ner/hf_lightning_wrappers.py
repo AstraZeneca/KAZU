bb443550a72729c9ceb9647ac805b9855622e5d7
Elliot Ford <elliot.ford@astrazeneca.com>
2023-08-09 09:17:11 +0100

    remove unused PLAutoModel class

--- kazu/steps/ner/hf_lightning_wrappers.py
+++ kazu/steps/ner/hf_lightning_wrappers.py
@@ -21,23 +21,3 @@ class PLAutoModelForTokenClassification(LightningModule):
         :external+pytorch_lightning:ref:`LightningModule.predict_step
         </common/lightning_module.rst#predict-step>`\\ ."""
         return self.model(**batch.data)
-
-
-class PLAutoModel(LightningModule):
-    def __init__(self, model: PreTrainedModel, *args: Any, **kwargs: Any):
-        """Very simple Lightning wrapper for AutoModel.
-
-        :param model: A pretrained model - usually created with AutoModel.from_pretrained
-        :param args:
-        :param kwargs:
-        """
-        super().__init__(*args, **kwargs)
-        self.model = model
-
-    def predict_step(self, batch: Any, batch_idx: int, dataloader_idx: Optional[int] = None) -> Any:
-        """Implementation of
-        :external+pytorch_lightning:ref:`LightningModule.predict_step
-        </common/lightning_module.rst#predict-step>`\\ ."""
-        result = self.model(input_ids=batch["input_ids"], attention_mask=batch["attention_mask"])
-
-        return result
