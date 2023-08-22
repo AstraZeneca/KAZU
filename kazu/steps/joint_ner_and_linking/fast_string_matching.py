59c952970053884bb86d0daca7c2e13cbe66e902
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-08-22 14:45:08 +0100

    added FastStringMatchingStep as a reimplementation of fast string matching. This uses a lot less memory than the spacy version

---
+++ kazu/steps/joint_ner_and_linking/fast_string_matching.py
@@ -0,0 +1,177 @@
+import logging
+from collections import defaultdict
+from collections.abc import Iterable
+
+import ahocorasick
+import spacy
+from kazu.data.data import Document, Entity, SynonymTermWithMetrics, MentionConfidence
+from kazu.database.in_memory_db import SynonymDatabase, ParserName, NormalisedSynonymStr
+from kazu.ontology_preprocessing.base import OntologyParser
+from kazu.steps import document_iterating_step
+from kazu.ontology_matching.assemble_pipeline import (  # noqa: F401 #we need this import to register the spacy component
+    KazuCustomEnglish,
+)
+from spacy.tokens import Span
+from kazu.steps.step import ParserDependentStep
+from kazu.utils.caching import kazu_disk_cache
+from kazu.utils.curated_term_tools import filter_curations_for_ner
+
+logger = logging.getLogger(__name__)
+
+EntityClass = str
+EntityInfoToOntologyInfoMapping = defaultdict[
+    tuple[EntityClass, MentionConfidence], set[tuple[ParserName, NormalisedSynonymStr]]
+]
+EntityStartIndex = int
+EntityEndIndex = int
+MatchedString = str
+
+
+class FastStringMatchingStep(ParserDependentStep):
+    """A wrapper for the ahocorasick algorithm.
+
+    In testing, this implementation is comparable in speed to a Spacy
+    `PhraseMatcher <https://spacy.io/api/phrasematcher>`_\\,
+    and uses a fraction of the memory. Since this implementation is unaware
+    of NLP concepts such as tokenisation, we backfill this capability by
+    checking for word boundaries with the spacy tokeniser.
+    """
+
+    def __init__(self, parsers: Iterable[OntologyParser], reload_spacy_at: int = 2000):
+        super().__init__(parsers)
+        self.reload_spacy_at = reload_spacy_at
+        self.parsers = parsers
+        self.automaton_strict, self.automaton_lc = self.create_automatons()
+        self.nlp = self.reload_spacy_pipeline()
+        self.call_count = 0
+        self.synonym_db = SynonymDatabase()
+
+    def reload_spacy_pipeline(self):
+        nlp = spacy.blank("kazu_custom_en")
+        nlp.add_pipe("sentencizer")
+        return nlp
+
+    @kazu_disk_cache.memoize(ignore={0})
+    def create_automatons(self):
+        """Create :class:`ahocorasick.Automaton`\\'s for parsers."""
+        key_to_ontology_info_lc: defaultdict[str, EntityInfoToOntologyInfoMapping] = defaultdict(
+            lambda: defaultdict(set)
+        )
+        key_to_ontology_info_cs: defaultdict[str, EntityInfoToOntologyInfoMapping] = defaultdict(
+            lambda: defaultdict(set)
+        )
+
+        logger.info("Creating ahocorasick Automatons")
+        for parser in self.parsers:
+            parser_curations = parser.populate_databases(return_curations=True)
+            if parser_curations is None:
+                logger.warning(
+                    "tried to create ahocorasick data for parser %s, but no curations are available",
+                    parser.name,
+                )
+                continue
+            if len(parser_curations) == 0:
+                logger.warning(
+                    "tried to create ahocorasick data for parser %s, but no curations were produced",
+                    parser.name,
+                )
+                continue
+
+            curations_for_ner = set(filter_curations_for_ner(parser_curations, parser))
+
+            for curation in curations_for_ner:
+                # a curation can have different term_norms for different parsers,
+                # since the string normalizer's output depends on the entity class.
+                # Also, a curation may exist in multiple SynonymTerm.terms
+                term_norm = curation.term_norm_for_linking(parser.entity_class)
+                entity_key = (parser.entity_class, curation.mention_confidence)
+                ontology_value = (parser.name, term_norm)
+                if curation.case_sensitive:
+                    match_string = curation.curated_synonym
+                    target_dict = key_to_ontology_info_cs
+                else:
+                    match_string = curation.curated_synonym.lower()
+                    target_dict = key_to_ontology_info_lc
+
+                target_dict[match_string][entity_key].add(ontology_value)
+
+        if len(key_to_ontology_info_cs) > 0:
+            automaton_strict = ahocorasick.Automaton()
+            for key, entity_to_ontology_info in key_to_ontology_info_cs.items():
+                automaton_strict.add_word(key, (key, entity_to_ontology_info))
+            automaton_strict.make_automaton()
+        else:
+            automaton_strict = None
+        if len(key_to_ontology_info_lc) > 0:
+            automaton_lc = ahocorasick.Automaton()
+            for key, entity_to_ontology_info in key_to_ontology_info_lc.items():
+                automaton_lc.add_word(key, (key, entity_to_ontology_info))
+            automaton_lc.make_automaton()
+        else:
+            automaton_lc = None
+
+        return automaton_strict, automaton_lc
+
+    def word_is_valid(
+        self, start_char: int, end_char: int, starts: set[int], ends: set[int]
+    ) -> bool:
+        return start_char in starts and end_char in ends
+
+    def _process_automation(
+        self,
+        automaton: ahocorasick.Automaton,
+        matchable_text: str,
+        original_text: str,
+        starts: set[int],
+        ends: set[int],
+    ) -> list[Entity]:
+        entities = []
+        for end_index, (match_key, ontology_dict) in automaton.iter(matchable_text):
+            start_index = end_index - len(match_key) + 1
+            original_match = original_text[start_index : end_index + 1]
+            if self.word_is_valid(start_index, end_index, starts, ends):
+                for (entity_class, confidence), parser_info_set in ontology_dict.items():
+                    terms = list()
+                    for parser_name, term_norm in parser_info_set:
+                        term_with_metrics = SynonymTermWithMetrics.from_synonym_term(
+                            self.synonym_db.get(parser_name, term_norm), exact_match=True
+                        )
+                        terms.append(term_with_metrics)
+                    e = Entity.load_contiguous_entity(
+                        start=start_index,
+                        end=end_index,
+                        match=original_match,
+                        entity_class=entity_class,
+                        namespace=self.namespace(),
+                        mention_confidence=confidence,
+                    )
+                    e.update_terms(terms)
+                    entities.append(e)
+        return entities
+
+    @document_iterating_step
+    def __call__(self, doc: Document) -> None:
+        self.call_count += 1
+        if self.call_count % self.reload_spacy_at == 0:
+            self.nlp = self.reload_spacy_pipeline()
+
+        for section in doc.sections:
+            spacy_doc = self.nlp(section.text)
+            starts, ends = set(), set()
+            for tok in spacy_doc:
+                span = Span(spacy_doc, tok.i, tok.i + 1)
+                starts.add(span.start_char)
+                ends.add(span.end_char - 1)
+
+            if self.automaton_lc is not None:
+                section.entities.extend(
+                    self._process_automation(
+                        self.automaton_lc, section.text.lower(), section.text, starts, ends
+                    )
+                )
+            if self.automaton_strict is not None:
+                section.entities.extend(
+                    self._process_automation(
+                        self.automaton_strict, section.text, section.text, starts, ends
+                    )
+                )
---
+++ kazu/tests/test_fast_matching.py
@@ -0,0 +1,497 @@
+import json
+from pathlib import Path
+
+import pytest
+from kazu.data.data import (
+    CuratedTerm,
+    DocumentJsonUtils,
+    MentionConfidence,
+    CuratedTermBehaviour,
+    EquivalentIdSet,
+    Document,
+)
+
+from kazu.ontology_preprocessing.base import (
+    IDX,
+    DEFAULT_LABEL,
+    SYN,
+    MAPPING_TYPE,
+)
+from kazu.tests.utils import DummyParser
+from kazu.utils.utils import Singleton
+from kazu.steps.joint_ner_and_linking.fast_string_matching import FastStringMatchingStep
+from kazu.ontology_preprocessing.synonym_generation import CombinatorialSynonymGenerator
+
+pytestmark = pytest.mark.usefixtures("mock_kazu_disk_cache_on_parsers")
+
+
+example_text = """There is a Q42_ID and Q42_syn in this sentence, as well as Q42_syn & Q8_syn synonyms.
+    This sentence is just to test when there are multiple synonyms for a single SynonymTerm,
+    like for complex 7 disease alpha a.k.a ComplexVII Disease\u03B1 amongst others."""
+
+
+def write_curations(path: Path, terms: list[CuratedTerm]):
+    with open(path, "w") as f:
+        for curation in terms:
+            f.write(json.dumps(DocumentJsonUtils.obj_to_dict_repr(curation)) + "\n")
+
+
+FIRST_MOCK_PARSER = "first_mock_parser"
+SECOND_MOCK_PARSER = "second_mock_parser"
+COMPLEX_7_DISEASE_ALPHA_NORM = "COMPLEX 7 DISEASE ALPHA"
+TARGET_IDX = "http://my.fake.ontology/complex_disease_123"
+CONFUSING_IDX = "http://my.fake.ontology/i_m_very_confused_123"
+CONFUSING_SYNONYM = "IM_A_SYMBOL_TO_CONFUSE_THE_pARSERL0L0L0"
+ENT_TYPE_1 = "ent_type_1"
+ENT_TYPE_2 = "ent_type_2"
+PARSER_1_DEFAULT_DATA = {
+    IDX: [
+        "http://my.fake.ontology/synonym_term_id_123",
+        TARGET_IDX,
+        TARGET_IDX,
+        "http://my.fake.ontology_amongst_id_123",
+        "http://my.fake.ontology_amongst_id_124",
+    ],
+    DEFAULT_LABEL: [
+        "SynonymTerm",
+        "SynonymTerm",
+        "Complex Disease Alpha VII",
+        "Amongst",
+        "Amongst Us",
+    ],
+    SYN: [
+        "SynonymTerm",
+        "SynonymTerm",
+        "complexVII disease\u03B1",
+        "amongst",
+        "amongst us",
+    ],
+    MAPPING_TYPE: ["test", "test", "test", "test", "test"],
+}
+
+PARSER_2_DEFAULT_DATA = {
+    IDX: [
+        "http://my.fake.ontology/synonym_term_id_123",
+        "http://my.fake.ontology/synonym_term_id_456",
+        TARGET_IDX,
+        "http://my.fake.ontology_amongst_id_123",
+    ],
+    DEFAULT_LABEL: [
+        "SynonymTerm",
+        "SynonymTerm",
+        "Complex Disease Alpha VII",
+        "Amongst",
+    ],
+    SYN: ["SynonymTerm", "SynonymTerm", "complexVII disease\u03B1", "amongst"],
+    MAPPING_TYPE: ["test", "test", "test", "test"],
+}
+
+
+@pytest.mark.parametrize(
+    (
+        "parser_1_curations",
+        "parser_2_curations",
+        "match_len",
+        "match_texts",
+        "match_ontology_dicts",
+        "parser_1_data",
+        "parser_2_data",
+    ),
+    [
+        pytest.param(
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="complexVII disease\u03B1",
+                    behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            FIRST_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=False,
+                ),
+            ],
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="complexVII disease\u03B1",
+                    behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            SECOND_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=False,
+                ),
+            ],
+            2,
+            {"ComplexVII Disease\u03B1"},
+            [
+                {
+                    ENT_TYPE_1: {
+                        (
+                            FIRST_MOCK_PARSER,
+                            COMPLEX_7_DISEASE_ALPHA_NORM,
+                            str(MentionConfidence.HIGHLY_LIKELY.value),
+                        )
+                    }
+                },
+                {
+                    ENT_TYPE_2: {
+                        (
+                            SECOND_MOCK_PARSER,
+                            COMPLEX_7_DISEASE_ALPHA_NORM,
+                            str(MentionConfidence.HIGHLY_LIKELY.value),
+                        )
+                    }
+                },
+            ],
+            PARSER_1_DEFAULT_DATA,
+            PARSER_2_DEFAULT_DATA,
+            id="Two curated case insensitive terms from two parsers Both should hit",
+        ),
+        pytest.param(
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="complexVII disease\u03B1",
+                    behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            FIRST_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=False,
+                )
+            ],
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="complexVII disease\u03B1",
+                    behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            SECOND_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=True,
+                )
+            ],
+            1,
+            {"ComplexVII Disease\u03B1"},
+            [
+                {
+                    ENT_TYPE_1: {
+                        (
+                            FIRST_MOCK_PARSER,
+                            COMPLEX_7_DISEASE_ALPHA_NORM,
+                            str(MentionConfidence.HIGHLY_LIKELY.value),
+                        )
+                    }
+                },
+            ],
+            PARSER_1_DEFAULT_DATA,
+            PARSER_2_DEFAULT_DATA,
+            id="Two curated terms from two parsers One should hit to test case sensitivity",
+        ),
+        pytest.param(
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="complexVII disease\u03B1",
+                    behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            FIRST_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=False,
+                ),
+            ],
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="complexVII disease\u03B1",
+                    behaviour=CuratedTermBehaviour.IGNORE,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            SECOND_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=False,
+                ),
+            ],
+            1,
+            {"ComplexVII Disease\u03B1"},
+            [
+                {
+                    ENT_TYPE_1: {
+                        (
+                            FIRST_MOCK_PARSER,
+                            COMPLEX_7_DISEASE_ALPHA_NORM,
+                            str(MentionConfidence.HIGHLY_LIKELY.value),
+                        )
+                    }
+                },
+            ],
+            PARSER_1_DEFAULT_DATA,
+            PARSER_2_DEFAULT_DATA,
+            id="Two curated terms from two parsers One should hit to test ignore logic",
+        ),
+        pytest.param(
+            [
+                CuratedTerm(
+                    mention_confidence=MentionConfidence.HIGHLY_LIKELY,
+                    curated_synonym="This sentence is just to test",
+                    behaviour=CuratedTermBehaviour.ADD_FOR_NER_AND_LINKING,
+                    associated_id_sets=frozenset(
+                        [
+                            EquivalentIdSet(
+                                ids_and_source=frozenset(
+                                    [
+                                        (
+                                            TARGET_IDX,
+                                            SECOND_MOCK_PARSER,
+                                        )
+                                    ]
+                                )
+                            )
+                        ]
+                    ),
+                    case_sensitive=False,
+                ),
+            ],
+            [],
+            1,
+            {"This sentence is just to test"},
+            [
+                {
+                    ENT_TYPE_1: {
+                        (
+                            FIRST_MOCK_PARSER,
+                            "THIS SENTENCE IS JUST TO TEST",
+                            str(MentionConfidence.HIGHLY_LIKELY.value),
+                        )
+                    }
+                },
+            ],
+            PARSER_1_DEFAULT_DATA,
+            PARSER_2_DEFAULT_DATA,
+            id="One curated term with a novel synonym This should be added to the synonym DB and hit",
+        ),
+    ],
+)
+def test_pipeline_build_from_parsers_and_curated_list(
+    tmp_path,
+    parser_1_curations,
+    parser_2_curations,
+    match_len,
+    match_texts,
+    match_ontology_dicts,
+    parser_1_data,
+    parser_2_data,
+    mock_build_fast_string_matcher_cache,
+):
+
+    Singleton.clear_all()
+    TEST_CURATIONS_PATH_PARSER_1 = tmp_path / "parser1_curated_terms.jsonl"
+    TEST_CURATIONS_PATH_PARSER_2 = tmp_path / "parser2_curated_terms.jsonl"
+    write_curations(path=TEST_CURATIONS_PATH_PARSER_1, terms=parser_1_curations)
+    write_curations(path=TEST_CURATIONS_PATH_PARSER_2, terms=parser_2_curations)
+    parser_1 = DummyParser(
+        name=FIRST_MOCK_PARSER,
+        entity_class="ent_type_1",
+        source=FIRST_MOCK_PARSER,
+        curations_path=str(TEST_CURATIONS_PATH_PARSER_1),
+        data=parser_1_data,
+    )
+    parser_2 = DummyParser(
+        name="second_mock_parser",
+        entity_class="ent_type_2",
+        source=SECOND_MOCK_PARSER,
+        curations_path=str(TEST_CURATIONS_PATH_PARSER_2),
+        data=parser_2_data,
+    )
+    step = FastStringMatchingStep(parsers=[parser_1, parser_2])
+    success, failed = step([Document.create_simple_document(example_text)])
+    entities = success[0].get_entities()
+    assert_matches(entities, match_len, match_texts, match_ontology_dicts)
+
+
+def test_pipeline_build_from_parsers_alone(tmp_path, mock_build_fast_string_matcher_cache):
+    Singleton.clear_all()
+    parser_1 = DummyParser(
+        name="first_mock_parser",
+        source="test",
+        synonym_generator=None,
+        entity_class="ent_type_1",
+        data={
+            IDX: [
+                "http://purl.obolibrary.org/obo/UBERON_042",
+                "http://purl.obolibrary.org/obo/UBERON_042",
+            ],
+            DEFAULT_LABEL: ["Q42", "Q42"],
+            SYN: ["Q42_label", "Q42_syn"],
+            MAPPING_TYPE: ["test", "test"],
+        },
+    )
+
+    parser_2 = DummyParser(
+        name="second_mock_parser",
+        entity_class="ent_type_2",
+        source="test",
+        synonym_generator=CombinatorialSynonymGenerator([]),
+        data={
+            IDX: [
+                "http://purl.obolibrary.org/obo/MONDO_08",
+                "http://purl.obolibrary.org/obo/MONDO_08",
+            ],
+            DEFAULT_LABEL: ["Q8", "Q8"],
+            SYN: ["Q8_label", "Q8_syn"],
+            MAPPING_TYPE: ["test", "test"],
+        },
+    )
+
+    parser_3 = DummyParser(
+        name="third_mock_parser",
+        entity_class="ent_type_3",
+        source="test",
+        data={
+            IDX: [
+                "http://my.fake.ontology/synonym_term_id_123",
+                "http://my.fake.ontology/complex_disease_123",
+                "http://my.fake.ontology/complex_disease_123",
+                "http://my.fake.ontology_amongst_id_123",
+            ],
+            DEFAULT_LABEL: [
+                "SynonymTerm",
+                "Complex Disease Alpha VII",
+                "Complex Disease Alpha VII",
+                "Amongst",
+            ],
+            SYN: ["SynonymTerm", "complex 7 disease alpha", "complexVII disease\u03B1", "amongst"],
+            MAPPING_TYPE: ["test", "test", "test", "test"],
+        },
+    )
+
+    step = FastStringMatchingStep(parsers=[parser_1, parser_2, parser_3])
+    success, failed = step([Document.create_simple_document(example_text)])
+    entities = success[0].get_entities()
+
+    match_len = 7
+    match_texts = {
+        "Q42_syn",
+        "Q8_syn",
+        "SynonymTerm",
+        "complex 7 disease alpha",
+        "ComplexVII Disease\u03B1",
+        "amongst",
+    }
+    match_ontology_dicts = [
+        {"ent_type_1": {("first_mock_parser", "Q42_SYN", str(MentionConfidence.POSSIBLE.value))}},
+        {"ent_type_1": {("first_mock_parser", "Q42_SYN", str(MentionConfidence.POSSIBLE.value))}},
+        {"ent_type_2": {("second_mock_parser", "Q8_SYN", str(MentionConfidence.POSSIBLE.value))}},
+        {
+            "ent_type_3": {
+                ("third_mock_parser", "SYNONYMTERM", str(MentionConfidence.POSSIBLE.value))
+            }
+        },
+        {
+            "ent_type_3": {
+                (
+                    "third_mock_parser",
+                    "COMPLEX 7 DISEASE ALPHA",
+                    str(MentionConfidence.PROBABLE.value),
+                )
+            }
+        },
+        {
+            "ent_type_3": {
+                (
+                    "third_mock_parser",
+                    "COMPLEX 7 DISEASE ALPHA",
+                    str(MentionConfidence.PROBABLE.value),
+                )
+            }
+        },
+        {"ent_type_3": {("third_mock_parser", "AMONGST", str(MentionConfidence.PROBABLE.value))}},
+    ]
+
+    assert_matches(entities, match_len, match_texts, match_ontology_dicts)
+
+
+def assert_matches(matches, match_len, match_texts, match_ontology_dicts):
+    assert len(matches) == match_len
+    assert set(m.match for m in matches) == match_texts
+    match_data = set()
+    parser_data = set()
+    for m in matches:
+        syn_term = next(iter(m.syn_term_to_synonym_terms))
+        match_data.add(
+            (
+                m.entity_class,
+                syn_term.parser_name,
+                syn_term.term_norm,
+                str(m.mention_confidence.value),
+            )
+        )
+    for item in match_ontology_dicts:
+        for k, v in item.items():
+            parser_data.add((k,) + next(iter(v)))
+
+    assert len(match_data.symmetric_difference(parser_data)) == 0
