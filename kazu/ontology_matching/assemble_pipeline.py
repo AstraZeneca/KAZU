470c704721919311eb2cac615eb5e3d9cb133fdc
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-09-12 11:03:20 +0100

    added SpacyPipelines Singleton. Moved the kazu spacy basic tokenisation component to spacy_pipeline.py, so it can be referenced in multiple locations. Also removed the cached_spacy_pipeline_load function as we don't want this behaviour anymore

--- kazu/ontology_matching/assemble_pipeline.py
+++ kazu/ontology_matching/assemble_pipeline.py
@@ -1,71 +1,9 @@
-from copy import deepcopy
-from string import ascii_lowercase
-
 import spacy
-from spacy.lang.en import English, EnglishDefaults
-from spacy.lang.en.tokenizer_exceptions import TOKENIZER_EXCEPTIONS
-from spacy.lang.char_classes import (
-    LIST_ELLIPSES,
-    LIST_ICONS,
-    CONCAT_QUOTES,
-    ALPHA_LOWER,
-    ALPHA_UPPER,
-    ALPHA,
-    HYPHENS,
-)
 
 from kazu.ontology_matching.ontology_matcher import OntologyMatcher, SPAN_KEY
 from kazu.ontology_preprocessing.base import OntologyParser
 from kazu.utils.utils import PathLike
-
-
-SPACY_DEFAULT_INFIXES = (
-    LIST_ELLIPSES
-    + LIST_ICONS
-    + [
-        r"(?<=[0-9])[+\-\*^](?=[0-9-])",
-        r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES),
-        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
-        # note: this will get removed below
-        r"(?<=[{a}0-9])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
-        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
-    ]
-)
-
-# We remove the hypen infix pattern because splitting by hyphens causes problems like
-# splitting the company name 'ALK-Abello' into ['ALK', '-', 'Abello']
-# and then potentially recognising ALK as a gene.
-
-# A spacy PR that modified this hyphen pattern specifically mentions biomedical texts
-# as being potentially problematic for this rule:
-# https://github.com/explosion/spaCy/pull/5770#issuecomment-659389160
-DEFAULT_INFIXES_MINUS_HYPHENS = SPACY_DEFAULT_INFIXES[:-2] + SPACY_DEFAULT_INFIXES[-1:]
-
-# There are exceptions saying not to tokenize things like 'a.' and 'b.' - presumably
-# to handle lists elements with letters like this. That isn't relevant for how we're
-# using spacy to do NER + linking, and it breaks some cases for us - when an entity
-# like 'Haemophilia A' is at the end of a sentence, so we get a token like 'A.', which
-# then doesn't get split, so we don't recognise it, unless we remove these exceptions.
-TOKENIZER_EXCEPTIONS_MINUS_SINGLE_LETTER = deepcopy(TOKENIZER_EXCEPTIONS)
-for letter in ascii_lowercase:
-    TOKENIZER_EXCEPTIONS_MINUS_SINGLE_LETTER.pop(letter + ".")
-
-
-class KazuCustomEnglishDefaults(EnglishDefaults):
-    tokenizer_exceptions = TOKENIZER_EXCEPTIONS_MINUS_SINGLE_LETTER
-    infixes = [r"\(", "/"] + DEFAULT_INFIXES_MINUS_HYPHENS
-    # related to the above, '.' isn't picked up as a suffix when preceded by a
-    # single uppercase character
-    suffixes = EnglishDefaults.suffixes + [
-        r"(?<=\b[{au}])\.".format(au=ALPHA_UPPER)
-    ]  # type:ignore[operator] # because mypy doesn't know that EnglishDefaults.suffixes is
-    # always a list[str]
-
-
-@spacy.registry.languages("kazu_custom_en")
-class KazuCustomEnglish(English):
-    lang = "kazu_custom_en"
-    Defaults = KazuCustomEnglishDefaults
+from kazu.utils.spacy_pipeline import basic_spacy_pipeline
 
 
 def main(
@@ -94,9 +32,7 @@ def main(
         `span attribute <https://spacy.io/api/doc#spans>`_ to store and access recognised NER
         spans.
     """
-    nlp = spacy.blank("kazu_custom_en")
-
-    nlp.add_pipe("sentencizer")
+    nlp = basic_spacy_pipeline()
     config = {
         "span_key": span_key,
         "parser_name_to_entity_type": {parser.name: parser.entity_class for parser in parsers},
--- kazu/utils/spacy_pipeline.py
+++ kazu/utils/spacy_pipeline.py
@@ -1,10 +1,180 @@
-from functools import cache
+import logging
+from collections import defaultdict
+from collections.abc import Callable, Iterable
+from copy import deepcopy
+from string import ascii_lowercase
+from typing import Union, Any
 
 import spacy
+from kazu.utils.utils import Singleton
+from spacy import Language
+from spacy.lang.char_classes import (
+    LIST_ELLIPSES,
+    LIST_ICONS,
+    CONCAT_QUOTES,
+    ALPHA_LOWER,
+    ALPHA_UPPER,
+    ALPHA,
+    HYPHENS,
+)
+from spacy.lang.en import English, EnglishDefaults
+from spacy.lang.en.tokenizer_exceptions import TOKENIZER_EXCEPTIONS
+from spacy.tokens import Doc
 
-from kazu.utils.utils import PathLike
+logger = logging.getLogger(__name__)
+
+SPACY_DEFAULT_INFIXES = (
+    LIST_ELLIPSES
+    + LIST_ICONS
+    + [
+        r"(?<=[0-9])[+\-\*^](?=[0-9-])",
+        r"(?<=[{al}{q}])\.(?=[{au}{q}])".format(al=ALPHA_LOWER, au=ALPHA_UPPER, q=CONCAT_QUOTES),
+        r"(?<=[{a}]),(?=[{a}])".format(a=ALPHA),
+        # note: this will get removed below
+        r"(?<=[{a}0-9])(?:{h})(?=[{a}])".format(a=ALPHA, h=HYPHENS),
+        r"(?<=[{a}0-9])[:<>=/](?=[{a}])".format(a=ALPHA),
+    ]
+)
+
+# We remove the hypen infix pattern because splitting by hyphens causes problems like
+# splitting the company name 'ALK-Abello' into ['ALK', '-', 'Abello']
+# and then potentially recognising ALK as a gene.
+
+# A spacy PR that modified this hyphen pattern specifically mentions biomedical texts
+# as being potentially problematic for this rule:
+# https://github.com/explosion/spaCy/pull/5770#issuecomment-659389160
+DEFAULT_INFIXES_MINUS_HYPHENS = SPACY_DEFAULT_INFIXES[:-2] + SPACY_DEFAULT_INFIXES[-1:]
+
+# There are exceptions saying not to tokenize things like 'a.' and 'b.' - presumably
+# to handle lists elements with letters like this. That isn't relevant for how we're
+# using spacy to do NER + linking, and it breaks some cases for us - when an entity
+# like 'Haemophilia A' is at the end of a sentence, so we get a token like 'A.', which
+# then doesn't get split, so we don't recognise it, unless we remove these exceptions.
+TOKENIZER_EXCEPTIONS_MINUS_SINGLE_LETTER = deepcopy(TOKENIZER_EXCEPTIONS)
+for letter in ascii_lowercase:
+    TOKENIZER_EXCEPTIONS_MINUS_SINGLE_LETTER.pop(letter + ".")
 
 
-@cache
-def cached_spacy_pipeline_load(path: PathLike) -> spacy.language.Language:
-    return spacy.load(path)
+class KazuCustomEnglishDefaults(EnglishDefaults):
+    tokenizer_exceptions = TOKENIZER_EXCEPTIONS_MINUS_SINGLE_LETTER
+    infixes = [r"\(", "/"] + DEFAULT_INFIXES_MINUS_HYPHENS
+    # related to the above, '.' isn't picked up as a suffix when preceded by a
+    # single uppercase character
+    suffixes = EnglishDefaults.suffixes + [  # type:ignore[operator] # because mypy
+        r"(?<=\b[{au}])\.".format(
+            au=ALPHA_UPPER
+        )  # doesn't know that EnglishDefaults.suffixes is always a list[str]
+    ]
+
+
+@spacy.registry.languages("kazu_custom_en")
+class KazuCustomEnglish(English):
+    lang = "kazu_custom_en"
+    Defaults = KazuCustomEnglishDefaults
+
+
+BASIC_PIPELINE_NAME = "basic"
+
+
+def basic_spacy_pipeline() -> Language:
+    nlp = spacy.blank("kazu_custom_en")
+    nlp.add_pipe("sentencizer")
+    return nlp
+
+
+class SpacyPipelines(metaclass=Singleton):
+    """Wraps spacy pipelines into a singleton, so multiple can be accessed from
+    different locations without additional memory overhead.
+
+    In addition, due to a
+    `known memory issue <https://github.com/explosion/spaCy/discussions/9362>`_
+    , we reload each pipeline after a certain number of calls
+    """
+
+    def __init__(self):
+        # because this is a singleton, we can't parameterise the reload variable ins the constructor
+        self._reload_at = 1000
+        self.name_to_path_or_build_func: dict[str, Union[str, Callable[[], Language]]] = {}
+        self.name_to_reload_callbacks: defaultdict[str, list[Callable[[], None]]] = defaultdict(
+            list
+        )
+        self.call_counter: defaultdict[str, int] = defaultdict(lambda: 0)
+        self.name_to_model: dict[str, Language] = {}
+
+    @property
+    def reload_at(self) -> int:
+        instance = SpacyPipelines()
+        return instance._reload_at
+
+    @reload_at.setter
+    def reload_at(self, value: int) -> None:
+        instance = SpacyPipelines()
+        instance._reload_at = value
+
+    @staticmethod
+    def add_from_path(name: str, path: str) -> None:
+        instance = SpacyPipelines()
+        if name in instance.name_to_path_or_build_func:
+            logger.info("The spacy pipeline key %s is already loaded.", name)
+        else:
+            instance.name_to_path_or_build_func[name] = path
+        instance.name_to_model[name] = spacy.load(path)
+
+    @staticmethod
+    def add_from_func(name: str, func: Callable[[], Language]) -> None:
+        instance = SpacyPipelines()
+        if name in instance.name_to_path_or_build_func:
+            logger.info("The spacy pipeline key %s is already loaded.", name)
+        else:
+            instance.name_to_path_or_build_func[name] = func
+        instance.name_to_model[name] = func()
+
+    @staticmethod
+    def add_reload_callback_func(name: str, func: Callable[[], None]) -> None:
+        instance = SpacyPipelines()
+        instance.name_to_reload_callbacks[name].append(func)
+
+    @staticmethod
+    def get_model(name: str) -> Language:
+        return SpacyPipelines().name_to_model[name]
+
+    def process_batch(
+        self,
+        texts: Union[Iterable[Union[str, Doc]], Iterable[tuple[Union[str, Doc], Any]]],
+        model_name: str,
+        **kwargs: Any,
+    ) -> Union[Iterable[Doc], Iterable[tuple[Doc, Any]]]:
+        for result in self.name_to_model[model_name].pipe(texts=texts, **kwargs):
+            yield result
+            self.call_counter[model_name] += 1
+
+        self._reload_if_required(model_name)
+
+    def process_single(self, text: Union[str, Doc], model_name: str, **kwargs: Any) -> Doc:
+        self.call_counter[model_name] += 1
+        self._reload_if_required(model_name)
+        return self.name_to_model[model_name](text, **kwargs)
+
+    def _reload_if_required(
+        self,
+        model_name: str,
+    ) -> None:
+        if self.call_counter[model_name] >= self.reload_at:
+            logger.info(
+                "max spacy calls exceeded for %s, see https://github.com/explosion/spaCy/discussions/9362 for more info",
+                model_name,
+            )
+            self.reload_model(model_name)
+            self.call_counter[model_name] = 0
+
+    def reload_model(self, model_name: str) -> None:
+        """Reload a model, clearing the spacy vocab."""
+
+        path_or_func = self.name_to_path_or_build_func[model_name]
+        logger.info("The model will be reloaded from %s.", path_or_func)
+        if isinstance(path_or_func, str):
+            self.name_to_model[model_name] = spacy.load(path_or_func)
+        else:
+            self.name_to_model[model_name] = path_or_func()
+        for call_back in self.name_to_reload_callbacks[model_name]:
+            call_back()
