b075d30f715b422179b939a4c87646ade759ddbf
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-09-19 13:22:43 +0100

    rewritten RulesBasedEntityClassDisambiguationFilterStep to take account of when rules are required to fire. Also improved docs
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/steps/linking/rule_based_disambiguator.py
+++ kazu/steps/linking/rule_based_disambiguator.py
@@ -1,25 +1,31 @@
 import logging
 from collections import defaultdict
-from collections.abc import Iterable
-from typing import Optional, Any
+from enum import Enum, auto
+from typing import Any, Literal, Optional
 
 import spacy
-from kazu.data.data import Document, Entity, Section
-from kazu.utils.grouping import sort_then_group
-from spacy.matcher import Matcher
-from spacy.tokens import Doc, Span, Token
-from kazu.steps import Step, document_iterating_step
+from kazu.data.data import Document, Entity
 from kazu.ontology_matching.assemble_pipeline import (  # noqa: F401 # we need this import to register the spacy component
     KazuCustomEnglish,
 )
-
+from kazu.steps import Step, document_iterating_step
+from spacy.matcher import Matcher
+from spacy.tokens import Token, Span
+from kazu.utils.spacy_pipeline import SpacyToKazuObjectMapper
 
 logger = logging.getLogger(__name__)
 TPOrFP = Literal["tp", "fp"]
-CooccurrenceListData = dict[str, dict[str, dict[TPOrFP, Optional[list[str]]]]]
-CooccurrenceSetData = dict[str, dict[str, dict[str, set[str]]]]
-ClassMatchers = dict[str, dict[str, Matcher]]
+TPOrFPMatcher = dict[TPOrFP, Matcher]
+MatcherMentionRules = dict[str, dict[str, dict[TPOrFP, list[list[dict[str, Any]]]]]]
 MatcherClassRules = dict[str, dict[TPOrFP, list[list[dict[str, Any]]]]]
+MentionMatchers = dict[str, dict[str, TPOrFPMatcher]]
+ClassMatchers = dict[str, TPOrFPMatcher]
+
+
+class MatcherResult(Enum):
+    HIT = auto()
+    MISS = auto()
+    NOT_CONFIGURED = auto()
 
 
 class RulesBasedEntityClassDisambiguationFilterStep(Step):
@@ -27,39 +33,50 @@ class RulesBasedEntityClassDisambiguationFilterStep(Step):
     :class:`.Section` that don't meet rules based
     disambiguation requirements in at least one location in the document.
 
-    This step can utilise both Spacy `Matcher <https://spacy.io/api/matcher>`_
-    rules and sentence based keyword co-occurrence information to determine whether
-    entities are valid or not.
+    This step utilises Spacy `Matcher <https://spacy.io/api/matcher>`_
+    rules to determine whether an entity class and or/mention entities are valid or not.
+    These Matcher rules operate on the sentence in which each under consideration
+    is located.
 
-    Rules can have both true positive and false positive aspects.
+    Rules can have both true positive and false positive aspects. If defined, that
+    aspect MUST be correct at least once in the document for all entities with the same
+    key (composed of the matched string and entity class) to be valid.
 
-    `Matcher <https://spacy.io/api/matcher>`_ rules operate on the entity class level
-    (i.e. apply to whole classes of an entity), whereas co-occurrence rules operate
-    on the mention level (i.e. does a specific mention of an entity co-occur with some expected words,
-    in the same sentence).
+    Non-contiguous entities are evaluated on the full span of the text they cover, rather
+    than the specific tokens.
 
     """
 
+    _tp_allowed_values = {MatcherResult.HIT, MatcherResult.NOT_CONFIGURED}
+
     def __init__(
-        self, class_matcher_rules: MatcherClassRules, cooccurrence_rules: CooccurrenceListData
+        self, class_matcher_rules: MatcherClassRules, mention_matcher_rules: MatcherMentionRules
     ):
         """
 
         :param class_matcher_rules: these should follow the format:
-            .. code-block::
 
-                {<entity class>:
-                    {<tp or fp (for true positive or false positive rules respectively>:
-                        [a list of rules according to the spacy pattern matcher syntax]
+            .. code-block:: python
+
+                {
+                    "<entity class>": {
+                        "<tp or fp (for true positive or false positive rules respectively>": [
+                            "<a list of rules>",
+                            "<according to the spacy pattern matcher syntax>",
+                        ]
                     }
                 }
-        :param cooccurrence_rules: these should follow the format:
-            .. code-block::
+        :param mention_matcher_rules: these should follow the format:
 
-                {<entity class>:
-                    {<mention to disambiguate>:
-                        {<tp or fp (for true positive or false positive rules respectively>:
-                            [<a list of keywords to disambiguate the context>]
+            .. code-block:: python
+
+                {
+                    "<entity class>": {
+                        "<mention to disambiguate>": {
+                            "<tp or fp>": [
+                                "<a list of rules>",
+                                "<according to the spacy pattern matcher syntax>",
+                            ]
                         }
                     }
                 }
@@ -67,179 +84,129 @@ class RulesBasedEntityClassDisambiguationFilterStep(Step):
         self.nlp = spacy.blank("kazu_custom_en")
         self.nlp.add_pipe("sentencizer")
         self.class_matchers: ClassMatchers = self._build_class_matchers(class_matcher_rules)
-        self.cooccurrence_rules: CooccurrenceSetData = self._build_cooccurrence_rules(
-            cooccurrence_rules
-        )
-
-        for entity_class in self.class_matchers:
-            Token.set_extension(entity_class, default=False, force=True)
-
-        for entity_class in self.cooccurrence_rules:
-            Token.set_extension(entity_class, default=False, force=True)
-
-    @staticmethod
-    def _build_cooccurrence_rules(coocurance_data: CooccurrenceListData) -> CooccurrenceSetData:
-        result: dict = coocurance_data.copy()
-        for class_name, term_data in coocurance_data.items():
-            for term, rule_type_and_terms in term_data.items():
-                for rule_type, terms_list in rule_type_and_terms.items():
-                    if terms_list is not None:
-                        result[class_name][term][rule_type] = set(terms_list)
-        return result
+        self.mention_matchers: MentionMatchers = self._build_mention_matchers(mention_matcher_rules)
 
     def _build_class_matchers(self, class_rules: MatcherClassRules) -> ClassMatchers:
-        result: defaultdict[str, dict[str, Matcher]] = defaultdict(dict)
+        result: ClassMatchers = {}
         for class_name, rules in class_rules.items():
             for rule_type, rule_instances in rules.items():
                 if rule_instances is not None:
                     matcher = Matcher(self.nlp.vocab)
                     matcher.add(f"{class_name}_{rule_type}", rule_instances)
-                    result[class_name][rule_type] = matcher
-        return dict(result)
+                    result.setdefault(class_name, {})[rule_type] = matcher
+        return result
 
-    def _cooccurrence_found(self, context: str, cooc_strings: set) -> bool:
-        return any(x in context for x in cooc_strings)
-
-    def _run_class_matchers(
-        self,
-        entity_class: str,
-        doc: Doc,
-    ) -> Iterable[bool]:
-        maybe_class_matchers = self.class_matchers.get(entity_class)
-        if maybe_class_matchers is None:
-            yield True
-        else:
-            tp_matcher = maybe_class_matchers.get("tp")
-            fp_matcher = maybe_class_matchers.get("fp")
-            if tp_matcher is None:
-                yield True
-            else:
-                if tp_matcher(doc):
-                    yield True
-                else:
-                    yield False
-
-            if fp_matcher is None:
-                yield True
-            else:
-                if fp_matcher(doc):
-                    yield False
-                else:
-                    yield True
-
-    def _run_cooccurrence_rules(
-        self,
-        entity_class: str,
-        entity_match: str,
-        span: Span,
-    ) -> Iterable[bool]:
-        maybe_cooccurrence_rules = self.cooccurrence_rules.get(entity_class, {}).get(entity_match)
-        if maybe_cooccurrence_rules is None:
-            yield True
-        else:
-            context = span.sent.text
-            tp_rules = maybe_cooccurrence_rules.get("tp")
-            fp_rules = maybe_cooccurrence_rules.get("fp")
-            if tp_rules is None:
-                yield True
-            else:
-                if self._cooccurrence_found(context, tp_rules):
-                    yield True
-                else:
-                    yield False
-
-            if fp_rules is None:
-                yield True
-            else:
-                if self._cooccurrence_found(context, fp_rules):
-                    return False
-                else:
-                    yield True
+    def _build_mention_matchers(self, mention_rules: MatcherMentionRules) -> MentionMatchers:
+        result: MentionMatchers = {}
+        rule_type: TPOrFP
+        for class_name, target_term_dict in mention_rules.items():
+            for target_term, rules in target_term_dict.items():
+                for rule_type, rule_instances in rules.items():
+                    if rule_instances is not None:
+                        matcher = Matcher(self.nlp.vocab)
+                        matcher.add(f"{class_name}_{rule_type}", rule_instances)
+                        result.setdefault(class_name, {})
+                        result[class_name].setdefault(target_term, {})
+                        matcher = Matcher(self.nlp.vocab)
+                        matcher.add(f"{class_name}_{target_term}_{rule_type}", rule_instances)
+                        result[class_name][target_term][rule_type] = matcher
+        return result
 
     @document_iterating_step
     def __call__(self, doc: Document) -> None:
-
-        entity_mentions_needing_disambiguation: set[Entity] = set()
-        entity_classes_needing_disambiguation: defaultdict[
-            Section, set[tuple[Doc, str]]
-        ] = defaultdict(set)
-        ent_to_span: dict[Entity, Span] = {}
-
+        ent_tp_class_results: defaultdict[tuple[str, str], set[bool]] = defaultdict(set)
+        ent_fp_class_results: defaultdict[tuple[str, str], set[bool]] = defaultdict(set)
+        ent_tp_mention_results: defaultdict[tuple[str, str], set[bool]] = defaultdict(set)
+        ent_fp_mention_results: defaultdict[tuple[str, str], set[bool]] = defaultdict(set)
+        key_requires_class_tp = {}
+        key_requires_class_fp = {}
+        key_requires_mention_tp = {}
+        key_requires_mention_fp = {}
         for section in doc.sections:
-            spacy_doc: Doc = self.nlp(section.text)
+            mapper = SpacyToKazuObjectMapper(self.nlp, section)
             for entity in section.entities:
                 entity_class = entity.entity_class
-                match = entity.match
-                span = spacy_doc.char_span(
-                    start_idx=entity.start, end_idx=entity.end, label=entity.entity_class
+
+                entity_match = entity.match
+                key = (
+                    entity_match,
+                    entity_class,
                 )
-                ent_to_span[entity] = span
-                for token in span:
-                    token._.set(entity.entity_class, True)
-                if entity_class in self.class_matchers or match in self.cooccurrence_rules.get(
-                    entity_class, {}
-                ):
-                    entity_mentions_needing_disambiguation.add(entity)
-                    entity_classes_needing_disambiguation[section].add(
-                        (
-                            spacy_doc,
-                            entity_class,
-                        )
-                    )
+                maybe_class_matchers = self.class_matchers.get(entity_class)
+                maybe_mention_matchers = self.mention_matchers.get(entity_class, {}).get(
+                    entity.match
+                )
+                if maybe_class_matchers is None and maybe_mention_matchers is None:
+                    continue
 
-        class_keys_to_keep, maybe_class_keys_to_drop = self._check_class_matcher_rules(
-            entity_classes_needing_disambiguation
-        )
+                tp_class_result, fp_class_result = self._check_tp_fp_matcher_rules(
+                    entity, mapper, maybe_class_matchers
+                )
+                if tp_class_result is MatcherResult.NOT_CONFIGURED:
+                    key_requires_class_tp[key] = False
+                else:
+                    key_requires_class_tp[key] = True
+                if fp_class_result is MatcherResult.NOT_CONFIGURED:
+                    key_requires_class_fp[key] = False
+                else:
+                    key_requires_class_fp[key] = True
 
-        maybe_mention_keys_to_drop, mention_keys_to_keep = self._check_cooccurrence_rules(
-            ent_to_span, entity_mentions_needing_disambiguation
-        )
+                ent_tp_class_results[key].add(tp_class_result in self._tp_allowed_values)
+                ent_fp_class_results[key].add(fp_class_result is MatcherResult.HIT)
 
-        maybe_class_keys_to_drop.difference_update(class_keys_to_keep)
-        maybe_mention_keys_to_drop.difference_update(mention_keys_to_keep)
+                tp_mention_result, fp_mention_result = self._check_tp_fp_matcher_rules(
+                    entity, mapper, maybe_mention_matchers
+                )
+                if tp_mention_result is MatcherResult.NOT_CONFIGURED:
+                    key_requires_mention_tp[key] = False
+                else:
+                    key_requires_mention_tp[key] = True
+                if fp_mention_result is MatcherResult.NOT_CONFIGURED:
+                    key_requires_mention_fp[key] = False
+                else:
+                    key_requires_mention_fp[key] = True
+
+                ent_tp_mention_results[key].add(tp_mention_result in self._tp_allowed_values)
+                ent_fp_mention_results[key].add(fp_class_result is MatcherResult.HIT)
+
         for section in doc.sections:
             for ent in list(section.entities):
-                if ent.entity_class in maybe_class_keys_to_drop:
-                    section.entities.remove(ent)
-                elif (ent.match, ent.entity_class) in maybe_mention_keys_to_drop:
-                    section.entities.remove(ent)
-
-    def _check_cooccurrence_rules(
-        self, ent_to_span: dict[Entity, Span], entity_mentions_needing_disambiguation: set[Entity]
-    ) -> tuple[set[tuple[str, str]], set[tuple[str, str]]]:
-        maybe_mention_keys_to_drop = set()
-        mention_keys_to_keep = set()
-        for key, ents_iter in sort_then_group(
-            entity_mentions_needing_disambiguation, key_func=lambda x: (x.match, x.entity_class)
-        ):
-            match, entity_class = key
-
-            for entity in ents_iter:
-                cooccurrence_results: set[bool] = set()
-                cooccurrence_results.update(
-                    self._run_cooccurrence_rules(
-                        entity_class=entity_class,
-                        entity_match=match,
-                        span=ent_to_span[entity],
-                    )
+                key = (
+                    ent.match,
+                    ent.entity_class,
                 )
-                key = entity.match, entity.entity_class
-                if False not in cooccurrence_results and True in cooccurrence_results:
-                    mention_keys_to_keep.add(key)
-                else:
-                    maybe_mention_keys_to_drop.add(key)
-        return maybe_mention_keys_to_drop, mention_keys_to_keep
+                if (
+                    (key_requires_class_fp[key] and True in ent_fp_class_results[key])
+                    or (key_requires_class_tp[key] and True not in ent_tp_class_results[key])
+                    or (key_requires_mention_fp[key] and True in ent_fp_mention_results[key])
+                    or (key_requires_mention_tp[key] and True not in ent_tp_mention_results[key])
+                ):
+                    section.entities.remove(ent)
 
-    def _check_class_matcher_rules(
-        self, entity_classes_needing_disambiguation: defaultdict[Section, set[tuple[Doc, str]]]
-    ) -> tuple[set[str], set[str]]:
-        maybe_class_keys_to_drop = set()
-        class_keys_to_keep = set()
-        for section, spacy_doc_and_class_set in entity_classes_needing_disambiguation.items():
-            for spacy_doc, entity_class in spacy_doc_and_class_set:
-                class_match_results = set(self._run_class_matchers(entity_class, spacy_doc))
-                if False not in class_match_results and True in class_match_results:
-                    class_keys_to_keep.add(entity_class)
-                else:
-                    maybe_class_keys_to_drop.add(entity_class)
-        return class_keys_to_keep, maybe_class_keys_to_drop
+    @staticmethod
+    def _check_matcher(context: Span, maybe_matcher: Optional[Matcher]) -> MatcherResult:
+        if maybe_matcher is None:
+            return MatcherResult.NOT_CONFIGURED
+        if bool(maybe_matcher(context)):
+            return MatcherResult.HIT
+        else:
+            return MatcherResult.MISS
+
+    @staticmethod
+    def _check_tp_fp_matcher_rules(
+        entity: Entity, mapper: SpacyToKazuObjectMapper, matchers: Optional[TPOrFPMatcher]
+    ) -> tuple[MatcherResult, MatcherResult]:
+        if matchers is None:
+            return MatcherResult.NOT_CONFIGURED, MatcherResult.NOT_CONFIGURED
+        span = mapper.ent_to_span[entity]
+        context = span.sent
+
+        tp_matcher = matchers.get("tp")
+        tp_result = RulesBasedEntityClassDisambiguationFilterStep._check_matcher(
+            context, tp_matcher
+        )
+        fp_matcher = matchers.get("fp")
+        fp_result = RulesBasedEntityClassDisambiguationFilterStep._check_matcher(
+            context, fp_matcher
+        )
+        return tp_result, fp_result
