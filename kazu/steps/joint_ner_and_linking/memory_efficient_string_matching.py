c270c94200e50b35d136802aa043cd77f21cd866
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-10-05 13:09:23 +0100

    refactored MemoryEfficientStringMatchingStep to use a single Automaton for speed and efficiency. Also ent starts and ends are calculated from Spacy Token idx directly
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/steps/joint_ner_and_linking/memory_efficient_string_matching.py
+++ kazu/steps/joint_ner_and_linking/memory_efficient_string_matching.py
@@ -3,7 +3,6 @@ from collections import defaultdict
 from collections.abc import Iterable
 
 import ahocorasick
-from spacy.tokens import Span
 
 from kazu.data.data import Document, Entity, SynonymTermWithMetrics, MentionConfidence
 from kazu.database.in_memory_db import SynonymDatabase, ParserName, NormalisedSynonymStr
@@ -17,12 +16,10 @@ from kazu.utils.spacy_pipeline import SpacyPipelines, basic_spacy_pipeline, BASI
 logger = logging.getLogger(__name__)
 
 EntityClass = str
+CaseSensitive = bool
 EntityInfoToOntologyInfoMapping = defaultdict[
-    tuple[EntityClass, MentionConfidence], set[tuple[ParserName, NormalisedSynonymStr]]
+    tuple[EntityClass, MentionConfidence, CaseSensitive, NormalisedSynonymStr], set[ParserName]
 ]
-EntityStartIndex = int
-EntityEndIndex = int
-MatchedString = str
 
 
 class MemoryEfficientStringMatchingStep(ParserDependentStep):
@@ -38,22 +35,21 @@ class MemoryEfficientStringMatchingStep(ParserDependentStep):
     def __init__(self, parsers: Iterable[OntologyParser]):
         super().__init__(parsers)
         self.parsers = parsers
-        self.automaton_strict, self.automaton_lc = self._create_automatons()
+        self.automaton = self._create_automaton()
         self.spacy_pipelines = SpacyPipelines()
         self.spacy_pipelines.add_from_func(BASIC_PIPELINE_NAME, basic_spacy_pipeline)
         self.synonym_db = SynonymDatabase()
 
     @kazu_disk_cache.memoize(ignore={0})
-    def _create_automatons(self):
-        """Create :class:`ahocorasick.Automaton`\\'s for parsers."""
-        key_to_ontology_info_lc: defaultdict[str, EntityInfoToOntologyInfoMapping] = defaultdict(
-            lambda: defaultdict(set)
-        )
-        key_to_ontology_info_cs: defaultdict[str, EntityInfoToOntologyInfoMapping] = defaultdict(
+    def _create_automaton(self) -> ahocorasick.Automaton:
+        """Create `ahocorasick.Automaton
+        <https://pyahocorasick.readthedocs.io/en/latest/#automaton-class>`_\\ s
+        for parsers."""
+        key_to_ontology_info: defaultdict[str, EntityInfoToOntologyInfoMapping] = defaultdict(
             lambda: defaultdict(set)
         )
 
-        logger.info("Creating ahocorasick Automatons")
+        logger.info("Creating ahocorasick Automaton")
         for parser in self.parsers:
             parser_curations = parser.populate_databases(return_curations=True)
             if parser_curations is None:
@@ -76,38 +72,34 @@ class MemoryEfficientStringMatchingStep(ParserDependentStep):
                 # since the string normalizer's output depends on the entity class.
                 # Also, a curation may exist in multiple SynonymTerm.terms
                 term_norm = curation.term_norm_for_linking(parser.entity_class)
-                entity_key = (parser.entity_class, curation.mention_confidence)
-                ontology_value = (parser.name, term_norm)
-                if curation.case_sensitive:
-                    match_string = curation.curated_synonym
-                    target_dict = key_to_ontology_info_cs
-                else:
-                    match_string = curation.curated_synonym.lower()
-                    target_dict = key_to_ontology_info_lc
+                entity_key = (
+                    parser.entity_class,
+                    curation.mention_confidence,
+                    curation.case_sensitive,
+                    term_norm,
+                )
+                key_to_ontology_info[curation.curated_synonym][entity_key].add(parser.name)
 
-                target_dict[match_string][entity_key].add(ontology_value)
-
-        if len(key_to_ontology_info_cs) > 0:
-            automaton_strict = ahocorasick.Automaton()
-            for key, entity_to_ontology_info in key_to_ontology_info_cs.items():
-                automaton_strict.add_word(key, (key, entity_to_ontology_info))
-            automaton_strict.make_automaton()
+        if len(key_to_ontology_info) > 0:
+            case_insensitive_automaton = ahocorasick.Automaton()
+            for key, entity_to_ontology_info in key_to_ontology_info.items():
+                case_insensitive_automaton.add_word(key.lower(), (key, entity_to_ontology_info))
+            case_insensitive_automaton.make_automaton()
         else:
-            automaton_strict = None
-        if len(key_to_ontology_info_lc) > 0:
-            automaton_lc = ahocorasick.Automaton()
-            for key, entity_to_ontology_info in key_to_ontology_info_lc.items():
-                automaton_lc.add_word(key, (key, entity_to_ontology_info))
-            automaton_lc.make_automaton()
-        else:
-            automaton_lc = None
+            case_insensitive_automaton = None
 
-        return automaton_strict, automaton_lc
+        return case_insensitive_automaton
 
     def _word_is_valid(
         self, start_char: int, end_char: int, starts: set[int], ends: set[int]
     ) -> bool:
         return start_char in starts and end_char in ends
+
+    def _case_matches(self, actual_match: str, original_casing: str, case_sensitive: bool) -> bool:
+        # no need to check the strings actually match if it's not case sensitive
+        # as this is already done as part of the string matching, so we know that
+        # actual_match.lower() == original_casing.lower()
+        return (not case_sensitive) or actual_match == original_casing
 
     def _process_automaton(
         self,
@@ -122,13 +114,26 @@ class MemoryEfficientStringMatchingStep(ParserDependentStep):
             start_index = end_index - len(match_key) + 1
             original_match = original_text[start_index : end_index + 1]
             if self._word_is_valid(start_index, end_index, starts, ends):
-                for (entity_class, confidence), parser_info_set in ontology_dict.items():
-                    terms = list()
-                    for parser_name, term_norm in parser_info_set:
-                        term_with_metrics = SynonymTermWithMetrics.from_synonym_term(
+                for entity_info, parser_name_set in ontology_dict.items():
+                    (
+                        entity_class,
+                        confidence,
+                        case_sensitive,
+                        term_norm,
+                    ) = entity_info
+                    # filter out cases where we've specified we only want exact matches, but we only have a lowercase match
+                    if not self._case_matches(
+                        actual_match=original_match,
+                        original_casing=match_key,
+                        case_sensitive=case_sensitive,
+                    ):
+                        continue
+                    terms = [
+                        SynonymTermWithMetrics.from_synonym_term(
                             self.synonym_db.get(parser_name, term_norm), exact_match=True
                         )
-                        terms.append(term_with_metrics)
+                        for parser_name in parser_name_set
+                    ]
                     e = Entity.load_contiguous_entity(
                         start=start_index,
                         end=end_index,
@@ -148,19 +153,11 @@ class MemoryEfficientStringMatchingStep(ParserDependentStep):
             spacy_doc = self.spacy_pipelines.process_single(section.text, BASIC_PIPELINE_NAME)
             starts, ends = set(), set()
             for tok in spacy_doc:
-                span = Span(spacy_doc, tok.i, tok.i + 1)
-                starts.add(span.start_char)
-                ends.add(span.end_char - 1)
+                starts.add(tok.idx)
+                ends.add(tok.idx + len(tok) - 1)
 
-            if self.automaton_lc is not None:
-                section.entities.extend(
-                    self._process_automaton(
-                        self.automaton_lc, section.text.lower(), section.text, starts, ends
-                    )
+            section.entities.extend(
+                self._process_automaton(
+                    self.automaton, section.text.lower(), section.text, starts, ends
                 )
-            if self.automaton_strict is not None:
-                section.entities.extend(
-                    self._process_automaton(
-                        self.automaton_strict, section.text, section.text, starts, ends
-                    )
-                )
+            )
