bbcca3b0bd0e9226d71b29971f9586af848e62df
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-10-05 13:46:12 +0100

    added MemoryEfficientStringMatchingStep to steps __init__.py
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/steps/__init__.py
+++ kazu/steps/__init__.py
@@ -1,6 +1,9 @@
 from kazu.steps.step import Step, ParserDependentStep, document_iterating_step, document_batch_step
 from kazu.steps.document_post_processing.abbreviation_finder import AbbreviationFinderStep
 from kazu.steps.joint_ner_and_linking.explosion import ExplosionStringMatchingStep
+from kazu.steps.joint_ner_and_linking.memory_efficient_string_matching import (
+    MemoryEfficientStringMatchingStep,
+)
 from kazu.steps.linking.dictionary import DictionaryEntityLinkingStep
 from kazu.steps.linking.post_processing.mapping_step import MappingStep
 from kazu.steps.linking.rules_based_disambiguation import (
