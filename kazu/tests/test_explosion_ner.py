c44b76b93e584dbfcc127963a1710febef1fd252
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-10-10 14:24:54 +0100

    skipping explosion test for now

--- kazu/tests/test_explosion_ner.py
+++ kazu/tests/test_explosion_ner.py
@@ -1,8 +1,10 @@
+import pytest
 from hydra.utils import instantiate
 
 from kazu.tests.utils import requires_model_pack
 
 
+@pytest.mark.skip(reason="semi deprecated - not in default model pack and is slow")
 @requires_model_pack
 def test_ExplosionStringMatchingStep(kazu_test_config, ner_simple_test_cases):
     # note, here we just test that the step is functional. Model performance is tested via an acceptance test
