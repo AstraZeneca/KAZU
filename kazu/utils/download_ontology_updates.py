e211b3ac1e541b97efe699eb5253630c6eefb36e
Richard Jackson <richard.jackson4@astrazeneca.com>
2023-11-09 14:13:20 +0000

    added resources_to_download optional param
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/utils/download_ontology_updates.py
+++ kazu/utils/download_ontology_updates.py
@@ -9,6 +9,52 @@ import requests
 
 _ONTOLOGY_DIR_NAME = "ontologies"
 
+ONTOLOGY_NAMES_TO_PATH = {
+    "CL": (
+        "http://purl.obolibrary.org/obo/cl.owl",
+        "cl.owl",
+    ),
+    "CLO": (
+        "http://purl.obolibrary.org/obo/clo.owl",
+        "clo.owl",
+    ),
+    "GO": (
+        "http://purl.obolibrary.org/obo/go.owl",
+        "go.owl",
+    ),
+    "MONDO": (
+        "http://purl.obolibrary.org/obo/mondo.json",
+        "mondo.json",
+    ),
+    "STATO": (
+        "http://purl.obolibrary.org/obo/stato.owl",
+        "stato.owl",
+    ),
+    "UBERON": (
+        "http://purl.obolibrary.org/obo/uberon.owl",
+        "uberon.owl",
+    ),
+    "HGNC_GENE_FAMILY": (
+        "http://biomart.genenames.org/martservice/results?query="
+        '<!DOCTYPE Query><Query client="biomartclient" processor="TSV" limit="-1" header="1">'
+        '<Dataset name="hgnc_family_mart" config="family_config">'
+        '<Attribute name="family__family_id_103"/><Attribute name="family__name_103"/>'
+        '<Attribute name="family__alias__alias_101"/><Attribute name="family__root_symbol_103"/><'
+        "/Dataset></Query>",
+        "gene_family.tsv",
+    ),
+    "CELLOSAURUS": (
+        "https://ftp.expasy.org/databases/cellosaurus/cellosaurus.obo",
+        "cellosaurus.obo",
+    ),
+    "HPO": (
+        "https://github.com/obophenotype/human-phenotype-ontology/releases/latest/download/hp-full.owl",
+        "hp-full.owl",
+    ),
+}
+
+DOWNLOAD_KEYS = list(ONTOLOGY_NAMES_TO_PATH.keys()) + ["OPENTARGETS", "CHEMBL"]
+
 
 def download_and_process_chembl(output_dir: Path, chembl_version: str) -> None:
     target_dir = output_dir.joinpath(_ONTOLOGY_DIR_NAME).absolute()
@@ -50,33 +96,27 @@ def download_and_process_chembl(output_dir: Path, chembl_version: str) -> None:
             conn.execute(f"DROP TABLE {table_name}")
     cur.close()
     print("running sqllite VACUUM")
-    conn = sqlite3.connect(chembl_db_path,isolation_level=None)
+    conn = sqlite3.connect(chembl_db_path, isolation_level=None)
     conn.execute("VACUUM")
     conn.close()
 
 
-def download_single_file_resources(output_dir: Path) -> None:
-    ontologies_to_path = {
-        "http://purl.obolibrary.org/obo/cl.owl": "cl.owl",
-        "http://purl.obolibrary.org/obo/clo.owl": "clo.owl",
-        "http://purl.obolibrary.org/obo/go.owl": "go.owl",
-        "http://purl.obolibrary.org/obo/mondo.json": "mondo.json",
-        "http://purl.obolibrary.org/obo/stato.owl": "stato.owl",
-        "http://purl.obolibrary.org/obo/uberon.owl": "uberon.owl",
-        'http://biomart.genenames.org/martservice/results?query=<!DOCTYPE Query><Query client="biomartclient" processor="TSV" limit="-1" header="1"><Dataset name="hgnc_family_mart" config="family_config"><Attribute name="family__family_id_103"/><Attribute name="family__name_103"/><Attribute name="family__alias__alias_101"/><Attribute name="family__root_symbol_103"/></Dataset></Query>': "gene_family.tsv",
-        "https://ftp.expasy.org/databases/cellosaurus/cellosaurus.obo": "cellosaurus.obo",
-        "https://github.com/obophenotype/human-phenotype-ontology/releases/latest/download/hp-full.owl": "hp-full.owl",
-    }
-    for url, local_path in ontologies_to_path.items():
-        response = requests.get(url, stream=True)
-        if response.status_code > 300:
-            print(f"request failed: {url}")
-            print(response)
-        else:
+def download_single_file_resources(output_dir: Path, resources_to_download: list[str]) -> None:
+    for resource_to_download in resources_to_download:
+        maybe_url_path = ONTOLOGY_NAMES_TO_PATH.get(resource_to_download)
+        if maybe_url_path is not None:
+            url, local_path = maybe_url_path
             print(f"downloading data from {url}")
-            with output_dir.joinpath(_ONTOLOGY_DIR_NAME).joinpath(local_path).open(mode="w") as f:
-                for chunk in response.iter_content(chunk_size=None):
-                    f.write(chunk)
+            response = requests.get(url, stream=True)
+            if response.status_code > 300:
+                print(f"request failed: {url}")
+                print(response)
+            else:
+                with output_dir.joinpath(_ONTOLOGY_DIR_NAME).joinpath(local_path).open(
+                    mode="wb"
+                ) as f:
+                    for chunk in response.iter_content(chunk_size=None):
+                        f.write(chunk)
 
 
 def download_ftp_resources(output_dir: Path, open_targets_version: str) -> None:
@@ -121,28 +161,36 @@ if __name__ == "__main__":
         "--chembl_version",
         type=str,
         required=False,
-        help="Version of Chembl to use, e.g. '33' ",
+        help="Version of Chembl to use, e.g. '33'",
     )
     parser.add_argument(
         "--output_dir",
         type=Path,
         required=True,
-        help="""path to download resources to""",
+        help="path to download resources to",
+    )
+    parser.add_argument(
+        "--resources_to_download",
+        nargs="+",
+        required=False,
+        default=DOWNLOAD_KEYS,
+        help=f"resources to download. Choose from {DOWNLOAD_KEYS}. If blank, download all",
     )
     args = parser.parse_args()
 
     output_dir: Path = args.output_dir
-    output_dir.mkdir(parents=True, exist_ok=True)
-    download_single_file_resources(output_dir)
+    output_dir.joinpath(_ONTOLOGY_DIR_NAME).mkdir(parents=True, exist_ok=True)
+    download_single_file_resources(output_dir, args.resources_to_download)
 
-    if args.open_targets_release_version:
-        download_ftp_resources(output_dir, args.open_targets_release_version)
-    else:
-        print(
-            "skipping open targets as no version was provided with --open_targets_release_version"
-        )
-
-    if args.chembl_version:
-        download_and_process_chembl(output_dir, args.chembl_version)
-    else:
-        print("skipping chembl as no version was provided with --chembl_version")
+    if "OPENTARGETS" in args.resources_to_download:
+        if args.open_targets_release_version:
+            download_ftp_resources(output_dir, args.open_targets_release_version)
+        else:
+            print(
+                "skipping open targets as no version was provided with --open_targets_release_version"
+            )
+    if "CHEMBL" in args.resources_to_download:
+        if args.chembl_version:
+            download_and_process_chembl(output_dir, args.chembl_version)
+        else:
+            print("skipping chembl as no version was provided with --chembl_version")
