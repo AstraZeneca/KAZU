63a1bdbf168da4d383ae2da4579c3f3afe478ba6
Richard Jackson <RichJackson@users.noreply.github.com>
2024-03-05 11:42:00 +0000

    various minor refactors and docstring changes for autocuration
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/ontology_preprocessing/autocuration.py
+++ kazu/ontology_preprocessing/autocuration.py
@@ -83,17 +83,17 @@ class MaxLength(AutoCurationAction):
         self.max_len = max_len
 
     def __call__(self, curated_term: CuratedTerm) -> CuratedTerm:
-        for form in curated_term.original_forms:
-            if len(form.string) > self.max_len:
-                return dataclasses.replace(
-                    curated_term, behaviour=CuratedTermBehaviour.DROP_SYNONYM_TERM_FOR_LINKING
-                )
-
-        return curated_term
+        if any(len(form.string) > self.max_len for form in curated_term.original_forms):
+            return dataclasses.replace(
+                curated_term, behaviour=CuratedTermBehaviour.DROP_SYNONYM_TERM_FOR_LINKING
+            )
+        else:
+            return curated_term
 
 
 def is_upper_case_word_to_case_insensitive(curated_term: CuratedTerm) -> CuratedTerm:
-    """Make strings composed of all alphabetical characters case-insensitive.
+    """Make curations where all original forms are all uppercase alphabetical characters
+    case-insensitive.
 
     Some data sources use all-caps strings for nouns that can be considered case-
     insensitive (e.g. Chembl).
@@ -120,31 +120,32 @@ def is_upper_case_word_to_case_insensitive(curated_term: CuratedTerm) -> Curated
     )
 
 
-def reverse_camel_case_to_case_sensitive(curated_term: CuratedTerm) -> CuratedTerm:
-    """If a form is reverse camel case, then it should be case-sensitive.
+def initial_lowercase_then_upper_to_case_sensitive(curated_term: CuratedTerm) -> CuratedTerm:
+    """If a form starts with a lowercase character followed by an uppercase character,
+    then all forms should be case-sensitive.
 
     E.g. "eGFR" vs "EGFR".
 
     :param curated_term:
     :return:
     """
-    for form in curated_term.original_forms:
-        try:
-            if form.string[0].islower() and form.string[1].isupper():
-                return dataclasses.replace(
-                    curated_term,
-                    original_forms=frozenset(
-                        dataclasses.replace(form, case_sensitive=True)
-                        for form in curated_term.original_forms
-                    ),
-                    alternative_forms=frozenset(
-                        dataclasses.replace(form, case_sensitive=True)
-                        for form in curated_term.alternative_forms
-                    ),
-                )
-        except IndexError:
-            pass
-    return curated_term
+    if any(
+        len(form.string) >= 2 and form.string[0].islower() and form.string[1].isupper()
+        for form in curated_term.original_forms
+    ):
+        return dataclasses.replace(
+            curated_term,
+            original_forms=frozenset(
+                dataclasses.replace(form, case_sensitive=True)
+                for form in curated_term.original_forms
+            ),
+            alternative_forms=frozenset(
+                dataclasses.replace(form, case_sensitive=True)
+                for form in curated_term.alternative_forms
+            ),
+        )
+    else:
+        return curated_term
 
 
 class AutoCurator:
