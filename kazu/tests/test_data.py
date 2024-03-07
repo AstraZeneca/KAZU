705bd01306ef293ef6e19a30e1482ed71b4c8af7
Elliot Ford <elliot.ford@astrazeneca.com>
2024-03-07 10:07:29 +0000

    tests: delete old serialization test
    This was essentially a hand-crafted 'roundtrip' test, which is now
    covered by the hypothesis tests.

--- kazu/tests/test_data.py
+++ kazu/tests/test_data.py
@@ -1,75 +1,12 @@
-import json
-
 import pytest
+
 from kazu.data.data import (
     CharSpan,
     Entity,
     Document,
     SynonymTermWithMetrics,
-    Mapping,
-    StringMatchConfidence,
-    DisambiguationConfidence,
 )
 from kazu.tests.utils import make_dummy_synonym_term
-
-
-def test_serialisation():
-    original_doc = Document.create_simple_document("Hello")
-    term = make_dummy_synonym_term(ids=["1", "2", "3"], parser_name="test", embed_score=0.99)
-    original_doc.sections[0].entities = [
-        Entity(
-            namespace="test",
-            match="metastatic liver cancer",
-            entity_class="test",
-            spans=frozenset([CharSpan(start=16, end=39)]),
-            syn_term_to_synonym_terms={term: term},
-            mappings={
-                Mapping(
-                    default_label="a",
-                    source="b",
-                    parser_name="c",
-                    idx="d",
-                    string_match_strategy="e",
-                    string_match_confidence=StringMatchConfidence.PROBABLE,
-                    disambiguation_confidence=DisambiguationConfidence.PROBABLE,
-                    disambiguation_strategy="f",
-                    xref_source_parser_name="g",
-                    metadata={"some": "thing"},
-                ),
-                Mapping(
-                    default_label="aa",
-                    source="bb",
-                    parser_name="cc",
-                    idx="dd",
-                    string_match_strategy="ee",
-                    string_match_confidence=StringMatchConfidence.PROBABLE,
-                    disambiguation_confidence=None,
-                    disambiguation_strategy=None,
-                    xref_source_parser_name=None,
-                    metadata={},
-                ),
-            },
-        )
-    ]
-    original_doc.sections[0].sentence_spans = [
-        CharSpan(start=0, end=28),
-        CharSpan(start=29, end=50),
-    ]
-    json_str = original_doc.to_json()
-    # ensure this is valid json
-    json_doc = json.loads(json_str)
-    assert type(json_doc["sections"][0]["sentence_spans"]) is list
-
-    # since entities are compared by id(self), they cannot be compared directly. We therefore need to compare their dictionary representations
-    deserialised_doc = Document.from_json(json_str)
-    original_entities = original_doc.sections[0].entities
-    deserialised_entities = deserialised_doc.sections[0].entities
-    # remove entities, since they will never be equal
-    original_doc.sections[0].entities = []
-    deserialised_doc.sections[0].entities = []
-    assert deserialised_doc == original_doc
-    for original_entity, deserialised_entity in zip(original_entities, deserialised_entities):
-        assert original_entity.__dict__ == deserialised_entity.__dict__
 
 
 def test_overlap_logic():
