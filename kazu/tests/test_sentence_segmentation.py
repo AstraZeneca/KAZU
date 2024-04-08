8f804e47ebb1906df475b8acffe0ee4b235b8ecc
Elliot Ford <elliot.ford@astrazeneca.com>
2024-04-08 17:10:14 +0100

    tests: remove unnecessary test
    We were skipping this test by default, and it only does work over and
    above what test_StanzaStep does in terms of testing the explosion step
    for sentence spans, when that step is semi deprecated. At this point,
    it's preferable to delete this test that have it permanently skipped.

--- kazu/tests/test_sentence_segmentation.py
+++ kazu/tests/test_sentence_segmentation.py
@@ -1,4 +1,3 @@
-import pytest
 from hydra.utils import instantiate
 
 from kazu.data import Document, CharSpan
@@ -68,40 +67,3 @@ def test_multiple_sentence_splitters_causes_error(kazu_test_config):
     dummy_processed, dummy_failures = dummy_step(docs)
 
     assert len(dummy_failures) == 1
-
-
-@pytest.mark.skip(
-    reason="ExplosionStringMatchingStep is semi deprecated - not in default model pack so not built, making this test extrememly slow."
-)
-@requires_model_pack
-def test_equivalent_to_explosion_for_simple_sents(kazu_test_config):
-    st_step: Step = instantiate(kazu_test_config.StanzaStep)
-    ex_step: Step = instantiate(
-        kazu_test_config.ExplosionStringMatchingStep, include_sentence_offsets=True
-    )
-
-    docs: list[Document] = [
-        Document.create_simple_document(
-            "This spans from char 0 to 29. This sentence spans from 30 to 64."
-        )
-        for _ in range(2)
-    ]
-
-    st_processed, st_failures = st_step([docs[0]])
-    ex_processed, ex_failures = ex_step([docs[1]])
-
-    assert len(st_processed) == len(ex_processed) == 1 and len(st_failures) == len(ex_failures) == 0
-
-    st_sent_spans = list(st_processed[0].sections[0].sentence_spans)
-    ex_sent_spans = list(ex_processed[0].sections[0].sentence_spans)
-
-    assert len(st_sent_spans) == len(ex_sent_spans) == 2
-
-    assert (
-        st_sent_spans[0].start == ex_sent_spans[0].start == 0
-        and st_sent_spans[0].end == ex_sent_spans[0].end == 29
-    )
-    assert (
-        st_sent_spans[1].start == ex_sent_spans[1].start == 30
-        and st_sent_spans[1].end == ex_sent_spans[1].end == 64
-    )
