50d4f53d6f3d0260feb4cdd799cd524d968356ca
Richard Jackson <richard.jackson4@astrazeneca.com>
2024-05-10 14:58:11 +0100

    changed const name to SKIP_MESSAGE_EXPERIMENTAL for abstraction
    Co-authored-by: Elliot Ford <elliot.ford@astrazeneca.com>

--- kazu/tests/utils.py
+++ kazu/tests/utils.py
@@ -51,12 +51,12 @@ maybe_skip_server_tests = pytest.mark.skipif(
     getenv("SKIP_KAZU_SERVER_TESTS") is not None, reason=SKIP_MESSAGE_SERVER_TESTS
 )
 
-SKIP_MESSAGE_EXPERIMENTAL_MODEL = """Some models are deemed to be experimental, and are therefore
+SKIP_MESSAGE_EXPERIMENTAL = """Some models are deemed to be experimental, and are therefore
 not tested in our standard test battery.
 """
 
 maybe_skip_experimental_tests = pytest.mark.skipif(
-    getenv("SKIP_KAZU_EXPERIMENTAL_TESTS") is not None, reason=SKIP_MESSAGE_EXPERIMENTAL_MODEL
+    getenv("SKIP_KAZU_EXPERIMENTAL_TESTS") is not None, reason=SKIP_MESSAGE_EXPERIMENTAL
 )
 
 SKIP_MESSAGE_NO_LABEL_STUDIO = """Skipping acceptance test as either LS_PROJECT_NAME, LS_URL_PORT \
