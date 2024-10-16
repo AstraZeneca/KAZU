bb586990e803b3699a928e3a70a9bde70b7442b2
Szymon Palucha <paluchasz@googlemail.com>
2024-10-16 14:09:18 +0100

    fix: remove button reserved for other pages

--- krt/pages/1_update_ontology_version.py
+++ krt/pages/1_update_ontology_version.py
@@ -1,13 +1,7 @@
 import streamlit as st
-from kazu.krt.components import show_save_button, show_reset_button
-from kazu.krt.string_editor.components import (
-    reset,
-)
 from kazu.krt.ontology_update_editor.components import OntologyUpdateForm
 
 st.markdown("# Update Ontology Version")
-show_save_button()
-show_reset_button(reset)
 st.write("""This page allows you to update various public ontologies to their latest version. """)
 st.write("choose a parser to update")
 
